4541013f8e0e6f799401a470323cb21916578895
mgiulio <giulio.mainardi@gmail.com>
2011-06-29 10:45:48 +0200

    Split feed routines in standalone files.

---
+++ src/core.js
@@ -0,0 +1,168 @@
+/*!
+ * jQuery Lifestream Plug-in
+ * @version 0.1.1
+ * Show a stream of your online activity
+ *
+ * Copyright 2011, Christian Vuerings - http://denbuzze.com
+ */
+/*globals jQuery, $ */
+(function( $ ){
+  /**
+   * Initialize the lifestream plug-in
+   * @param {Object} config Configuration object
+   */
+  $.fn.lifestream = function( config ) {
+
+    // Make the plug-in chainable
+    return this.each(function() {
+
+      // The element where the lifestream is linked to
+      var outputElement = $(this),
+
+      // Extend the default settings with the values passed
+      settings = jQuery.extend({
+        // The name of the main lifestream class
+        // We use this for the main ul class e.g. lifestream
+        // and for the specific feeds e.g. lifestream-twitter
+        classname: "lifestream",
+        // Callback function which will be triggered when a feed is loaded
+        feedloaded: null,
+        // The amount of feed items you want to show
+        limit: 10,
+        // An array of feed items which you want to use
+        list: []
+      }, config),
+
+      // The data object contains all the feed items
+      data = {
+        count: settings.list.length,
+        items: []
+      },
+
+      // We use the item settings to pass the global settings variable to
+      // every feed
+      itemsettings = jQuery.extend( true, {}, settings ),
+
+      /**
+       * This method will be called every time a feed is loaded. This means
+       * that several DOM changes will occur. We did this because otherwise it
+       * takes to look before anything shows up.
+       * We allow 1 request per feed - so 1 DOM change per feed
+       * @private
+       * @param {Array} inputdata an array containing all the feeditems for a
+       * specific feed.
+       */
+      finished = function( inputdata ) {
+
+        // Merge the feed items we have from other feeds, with the feeditems
+        // from the new feed
+        $.merge( data.items, inputdata );
+
+        // Sort the feeditems by date - we want the most recent one first
+        data.items.sort( function( a, b ) {
+            return ( b.date - a.date );
+        });
+
+        var items = data.items,
+
+            // We need to check whether the amount of current feed items is
+            // smaller than the main limit. This parameter will be used in the
+            // for loop
+            length = ( items.length < settings.limit ) ?
+              items.length :
+              settings.limit,
+            i = 0, item,
+
+            // We create an unordered list which will create all the feed
+            // items
+            ul = $('<ul class="' + settings.classname + '"/>');
+
+        // Run over all the feed items + add them as list items to the
+        // unordered list
+        for ( ; i < length; i++ ) {
+          item = items[i];
+          if ( item.html ) {
+            $('<li class="'+ settings.classname + '-'
+              + item.config.service + '">').append( item.html )
+                                           .appendTo( ul );
+          }
+        }
+
+        // Change the innerHTML with a list of all the feeditems in
+        // chronological order
+        outputElement.html( ul );
+
+        // Trigger the feedloaded callback, if it is a function
+        if ( $.isFunction( settings.feedloaded ) ) {
+          settings.feedloaded();
+        }
+
+      },
+
+      /**
+       * Fire up all the feeds and pass them the right arugments.
+       * @private
+       */
+      load = function() {
+
+        var i = 0, j = settings.list.length;
+
+        // We don't pass the list array to each feed  because this will create
+        // a recursive JavaScript object
+        delete itemsettings.list;
+
+        // Run over all the items in the list
+        for( ; i < j; i++ ) {
+
+          var config = settings.list[i];
+
+          // Check whether the feed exists, if the feed is a function and if a
+          // user has been filled in
+          if ( $.fn.lifestream.feeds[config.service] &&
+               $.isFunction( $.fn.lifestream.feeds[config.service] )
+               && config.user) {
+
+            // You'll be able to get the global settings by using
+            // config._settings in your feed
+            config._settings = itemsettings;
+
+            // Call the feed with a config object and finished callback
+            $.fn.lifestream.feeds[config.service]( config, finished );
+          }
+
+        }
+
+      };
+
+      // Load the jQuery templates plug-in if it wasn't included in the page.
+      // At then end we call the load method.
+      if( !jQuery.tmpl ) {
+        jQuery.getScript(
+          "https://raw.github.com/jquery/jquery-tmpl/master/"
+            + "jquery.tmpl.min.js",
+          load);
+      } else {
+        load();
+      }
+
+    });
+
+  };
+  
+  /**
+   * Create a valid YQL URL by passing in a query
+   * @param {String} query The query you want to convert into a valid yql url
+   * @return {String} A valid YQL URL
+   */
+  $.fn.lifestream.createYqlFromUrl = function( query ) {
+    return ( "http://query.yahooapis.com/v1/public/yql?q=__QUERY__&env=" +
+      "store://datatables.org/alltableswithkeys&format=json")
+      .replace( "__QUERY__" , encodeURIComponent( query ) );
+  };
+
+  /**
+   * A big container which contains all available feeds
+   */
+  $.fn.lifestream.feeds = $.fn.lifestream.feeds || {};
+  
+}( jQuery ));
---
+++ src/feeds/twitter.js
@@ -0,0 +1,87 @@
+$.fn.lifestream.feeds.twitter = function( config, callback ) {
+
+    var template = $.extend({},
+      {
+        posted: '{{html tweet}}'
+      },
+      config.template),
+
+    /**
+     * Add links to the twitter feed.
+     * Hashes, @ and regular links are supported.
+     * @private
+     * @param {String} tweet A string of a tweet
+     * @return {String} A linkified tweet
+     */
+    linkify = function( tweet ) {
+
+      var link = function( t ) {
+        return t.replace(
+          /[a-z]+:\/\/[a-z0-9-_]+\.[a-z0-9-_:~%&\?\/.=]+[^:\.,\)\s*$]/ig,
+          function( m ) {
+            return '<a href="' + m + '">'
+              + ( ( m.length > 25 ) ? m.substr( 0, 24 ) + '...' : m )
+              + '</a>';
+          }
+        );
+      },
+      at = function( t ) {
+        return t.replace(
+          /(^|[^\w]+)\@([a-zA-Z0-9_]{1,15})/g,
+          function( m, m1, m2 ) {
+            return m1 + '<a href="http://twitter.com/' + m2 + '">@'
+              + m2 + '</a>';
+          }
+        );
+      },
+      hash = function( t ) {
+        return t.replace(
+          /(^|[^\w'"]+)\#([a-zA-Z0-9_]+)/g,
+          function( m, m1, m2 ) {
+            return m1 + '<a href="http://search.twitter.com/search?q=%23'
+            + m2 + '">#' + m2 + '</a>';
+          }
+        );
+      };
+
+      return hash(at(link(tweet)));
+
+    },
+    /**
+     * Parse the input from twitter
+     */
+    parseTwitter = function( input ) {
+      var output = [], i = 0, j;
+
+      if(input.query && input.query.count && input.query.count >0) {
+        j = input.query.count;
+        for( ; i<j; i++) {
+          var status = input.query.results.statuses[i].status;
+          output.push({
+            date: new Date(status.created_at),
+            config: config,
+            html: $.tmpl( template.posted, {
+              tweet: linkify(status.text)
+            } )
+          });
+        }
+      }
+      return output;
+    };
+
+    $.ajax({
+      url: $.fn.lifestream.createYqlUrl('select status.id, status.created_at, status.text'
+        + ' from twitter.user.timeline where screen_name="'+ config.user +'"'),
+      dataType: 'jsonp',
+      success: function( data ) {
+        callback(parseTwitter(data));
+      }
+    });
+
+    // Expose the template.
+    // We use this to check which templates are available
+    return {
+      "template" : template
+    };
+
+  };
