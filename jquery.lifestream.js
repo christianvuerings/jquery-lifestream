44013afd4dc0d10b834139c980beaa0f449e8f05
Christian Vuerings <vueringschristian@gmail.com>
2011-06-29 07:53:12 +0200

    Fixes gh-38 - Make the plug-in chainable
    I had to re-intend all of the core code though.

--- jquery.lifestream.js
+++ jquery.lifestream.js
@@ -25,122 +25,130 @@
    */
   $.fn.lifestream = function( config ) {
 
-    // The element where the lifestream is linked to
-    var outputElement = this,
+    // Make the plug-in chainable
+    return this.each(function() {
 
-    // Extend the default settings with the values passed
-    settings = jQuery.extend({
-      // The name of the main lifestream class
-      // We use this for the main ul class e.g. lifestream
-      // and for the specific feeds e.g. lifestream-twitter
-      classname: "lifestream",
-      // The amount of feed items you want to show
-      limit: 10
-    }, config),
+      // The element where the lifestream is linked to
+      var outputElement = $(this),
 
-    // The data object contains all the feed items
-    data = {
-      count: settings.list.length,
-      items: []
-    },
+      // Extend the default settings with the values passed
+      settings = jQuery.extend({
+        // The name of the main lifestream class
+        // We use this for the main ul class e.g. lifestream
+        // and for the specific feeds e.g. lifestream-twitter
+        classname: "lifestream",
+        // The amount of feed items you want to show
+        limit: 10
+      }, config),
 
-    // We use the item settings to pass the global settings variable to
-    // every feed
-    itemsettings = jQuery.extend( true, {}, settings ),
+      // The data object contains all the feed items
+      data = {
+        count: settings.list.length,
+        items: []
+      },
 
-    /**
-     * This method will be called every time a feed is loaded. This means
-     * that several DOM changes will occur. We did this because otherwise it
-     * takes to look before anything shows up.
-     * We allow 1 request per feed - so 1 DOM change per feed
-     * @private
-     * @param {Array} inputdata an array containing all the feeditems for a
-     * specific feed.
-     */
-    finished = function( inputdata ) {
+      // We use the item settings to pass the global settings variable to
+      // every feed
+      itemsettings = jQuery.extend( true, {}, settings ),
 
-      // Merge the feed items we have from other feeds, with the feeditems
-      // from the new feed
-      $.merge( data.items, inputdata );
+      /**
+       * This method will be called every time a feed is loaded. This means
+       * that several DOM changes will occur. We did this because otherwise it
+       * takes to look before anything shows up.
+       * We allow 1 request per feed - so 1 DOM change per feed
+       * @private
+       * @param {Array} inputdata an array containing all the feeditems for a
+       * specific feed.
+       */
+      finished = function( inputdata ) {
 
-      // Sort the feeditems by date - we want the most recent one first
-      data.items.sort( function( a, b ) {
-          return ( b.date - a.date );
-      });
+        // Merge the feed items we have from other feeds, with the feeditems
+        // from the new feed
+        $.merge( data.items, inputdata );
 
-      var items = data.items,
+        // Sort the feeditems by date - we want the most recent one first
+        data.items.sort( function( a, b ) {
+            return ( b.date - a.date );
+        });
 
-          // We need to check whether the amount of current feed items is
-          // smaller than the main limit. This parameter will be used in the
-          // for loop
-          length = ( items.length < settings.limit ) ?
-            items.length :
-            settings.limit,
-          i = 0, item,
+        var items = data.items,
 
-          // We create an unordered list which will create all the feed items
-          ul = $('<ul class="' + settings.classname + '"/>');
+            // We need to check whether the amount of current feed items is
+            // smaller than the main limit. This parameter will be used in the
+            // for loop
+            length = ( items.length < settings.limit ) ?
+              items.length :
+              settings.limit,
+            i = 0, item,
 
-      // Run over all the feed items + add them as list items to the unordered
-      // list
-      for ( ; i < length; i++ ) {
-        item = items[i];
-        if ( item.html ) {
-          $('<li class="'+ settings.classname + '-'
-            + item.config.service + '">').append( item.html ).appendTo( ul );
-        }
-      }
+            // We create an unordered list which will create all the feed
+            // items
+            ul = $('<ul class="' + settings.classname + '"/>');
 
-      // Change the innerHTML with a list of all the feeditems in
-      // chronological order
-      outputElement.html( ul );
-
-    },
-
-    /**
-     * Fire up all the feeds and pass them the right arugments.
-     * @private
-     */
-    load = function() {
-
-      var i = 0, j = settings.list.length;
-
-      // We don't pass the list array to each feed  because this will create
-      // a recursive JavaScript object
-      delete itemsettings.list;
-
-      // Run over all the items in the list
-      for( ; i < j; i++ ) {
-
-        var config = settings.list[i];
-
-        // Check whether the feed exists, if the feed is a function and if a
-        // user has been filled in
-        if ( $.fn.lifestream.feeds[config.service] &&
-             $.isFunction( $.fn.lifestream.feeds[config.service] )
-             && config.user) {
-
-          // You'll be able to get the global settings by using
-          // config._settings in your feed
-          config._settings = itemsettings;
-
-          // Call the feed with a config object and finished callback
-          $.fn.lifestream.feeds[config.service]( config, finished );
+        // Run over all the feed items + add them as list items to the
+        // unordered list
+        for ( ; i < length; i++ ) {
+          item = items[i];
+          if ( item.html ) {
+            $('<li class="'+ settings.classname + '-'
+              + item.config.service + '">').append( item.html )
+                                           .appendTo( ul );
+          }
         }
 
+        // Change the innerHTML with a list of all the feeditems in
+        // chronological order
+        outputElement.html( ul );
+
+      },
+
+      /**
+       * Fire up all the feeds and pass them the right arugments.
+       * @private
+       */
+      load = function() {
+
+        var i = 0, j = settings.list.length;
+
+        // We don't pass the list array to each feed  because this will create
+        // a recursive JavaScript object
+        delete itemsettings.list;
+
+        // Run over all the items in the list
+        for( ; i < j; i++ ) {
+
+          var config = settings.list[i];
+
+          // Check whether the feed exists, if the feed is a function and if a
+          // user has been filled in
+          if ( $.fn.lifestream.feeds[config.service] &&
+               $.isFunction( $.fn.lifestream.feeds[config.service] )
+               && config.user) {
+
+            // You'll be able to get the global settings by using
+            // config._settings in your feed
+            config._settings = itemsettings;
+
+            // Call the feed with a config object and finished callback
+            $.fn.lifestream.feeds[config.service]( config, finished );
+          }
+
+        }
+
+      };
+
+      // Load the jQuery templates plug-in if it wasn't included in the page.
+      // At then end we call the load method.
+      if( !jQuery.tmpl ) {
+        jQuery.getScript(
+          "https://raw.github.com/jquery/jquery-tmpl/master/"
+            + "jquery.tmpl.min.js",
+          load);
+      } else {
+        load();
       }
 
-    };
-
-    // Load the jQuery templates plug-in if it wasn't included in the page.
-    // At then end we call the load method.
-    if( !jQuery.tmpl ) {
-      jQuery.getScript(
-        "https://raw.github.com/jquery/jquery-tmpl/master/jquery.tmpl.min.js",
-        load);
-    } else {
-      load();
-    }
+    });
 
   };
 
