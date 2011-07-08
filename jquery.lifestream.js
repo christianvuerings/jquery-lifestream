333b11b1b89374e79b135d6399bc38ae6f5b210b
Joshua Hawxwell <m@hawx.me>
2011-07-08 15:09:46 +0100

    Added instapaper support

--- jquery.lifestream.js
+++ jquery.lifestream.js
@@ -779,6 +779,53 @@
     };
 
   };
+  
+  $.fn.lifestream.feeds.instapaper = function( config, callback ) {
+    var template = $.extend({},
+      {
+        loved: 'loved <a href="${link}">${title}</a>'
+      },
+      config.template),
+      
+    parseInstapaper = function( input ) {
+      var output = [], list, i = 0, j;
+      
+      console.log(input.query);
+      
+      if(input.query && input.query.count && input.query.count > 0 
+          && input.query.results.rss.channel.item) {
+
+        list = input.query.results.rss.channel.item;
+        j = list.length;
+        for( ; i<j; i++) {
+          var item = list[i];
+          output.push({
+            date: new Date( item.pubDate ),
+            config: config,
+            html: $.tmpl( template.loved, item )
+          });
+        }
+      }
+      return output;
+    };
+    
+    $.ajax({
+      url: createYqlUrl('select * from xml where url='
+        + '"http://www.instapaper.com/starred/rss/'
+        + config.user + '"'),
+      dataType: 'jsonp',
+      success: function( data ) {
+        callback(parseInstapaper(data));
+      }
+    });
+    
+    // Expose the template.
+    // We use this to check which templates are available
+    return {
+      "template" : template
+    };
+    
+  };
 
   $.fn.lifestream.feeds.iusethis = function( config, callback ) {
 
