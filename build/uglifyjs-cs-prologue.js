398fdbe73835c8bbbbec45ad5774f92c1d41ab81
mgiulio <giulio.mainardi@gmail.com>
2011-07-15 17:46:03 +0200

    BS: Update makefile and .reduce() fix
    - Modify deps for uglifyjs-cs build
    - Add an implementation of Array.prototype.reduce() for
      browsers that does not support it(like, for example, IE8).
      It is commented because UglifyJS complain about a TypeError
      problem...fixing it

--- build/uglifyjs-cs-prologue.js
+++ build/uglifyjs-cs-prologue.js
@@ -10,3 +10,33 @@ var
     return modules[module];
   }
 ;
+
+/* if (!Array.prototype.reduce) {
+  Array.prototype.reduce = function reduce(accumlator){
+        var i, l = this.length, curr;
+        
+        if(typeof accumlator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+          throw new TypeError("First argument is not callable");
+
+        if((l == 0 || l === null) && (arguments.length <= 1))// == on purpose to test 0 and false.
+          throw new TypeError("Array length is 0 and no second argument");
+        
+        if(arguments.length <= 1){
+          for(i=0 ; i=l) // empty array
+            throw new TypeError("Empty array and no second argument");
+            
+          curr = this[i++]; // Increase i to start searching the secondly defined element in the array
+        }
+        else{
+          curr = arguments[1];
+        }
+        
+        for(i = i || 0 ; i < l ; i++){
+          if(i in this)
+            curr = accumlator.call(undefined, curr, this[i], i, this);
+        }
+        
+        return curr;
+      };
+  } */
+
