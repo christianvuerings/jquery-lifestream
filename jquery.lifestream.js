d40661e3cfb36c9fdc51d940c4b83c20193b0379
mgiulio <giulio.mainardi@gmail.com>
2011-07-22 14:51:30 +0200

    Update built files

--- jquery.lifestream.js
+++ jquery.lifestream.js
@@ -387,45 +387,43 @@
     "template" : template
   };
 
-};$.fn.lifestream.feeds.flickr = function( config, callback ) {
+};;$.fn.lifestream.feeds.dribbble = function( config, callback ) {
 
-  var template = $.extend({},
-    {
-      posted: 'posted a photo <a href="${link}">${title}</a>'
-    },
-    config.template);
+    var template = $.extend({},
+      {
+        posted: 'posted a shot <a href="${url}">${title}</a>'
+      },
+      config.template);
 
-  $.ajax({
-    url: "http://api.flickr.com/services/feeds/photos_public.gne?id="
-      + config.user + "&lang=en-us&format=json",
-    dataType: "jsonp",
-    jsonp: 'jsoncallback',
-    success: function( data ) {
-      var output = [], i = 0, j;
+    $.ajax({
+      url: "http://api.dribbble.com/players/" + config.user + "/shots",
+      dataType: "jsonp",
+      success: function( data ) {
+        var output = [], i = 0, j;
 
-      if(data && data.items && data.items.length > 0) {
-        j = data.items.length;
-        for( ; i<j; i++) {
-          var item = data.items[i];
-          output.push({
-            date: new Date( item.published ),
-            config: config,
-            html: $.tmpl( template.posted, item )
-          });
+        if(data && data.total) {
+          j = data.shots.length;
+          for( ; i<j; i++) {
+            var item = data.shots[i];
+            output.push({
+              date: new Date(item.created_at),
+              config: config,
+              html: $.tmpl( template.posted, item )
+            });
+          }
         }
+
+        callback(output);
       }
+    });
 
-      callback(output);
-    }
-  });
+    // Expose the template.
+    // We use this to check which templates are available
+    return {
+      "template" : template
+    };
 
-  // Expose the template.
-  // We use this to check which templates are available
-  return {
-    "template" : template
-  };
-
-};$.fn.lifestream.feeds.flickr = function( config, callback ) {
+  };$.fn.lifestream.feeds.flickr = function( config, callback ) {
 
   var template = $.extend({},
     {
