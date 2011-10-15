7a2e84756cca7b9d6b43b395dd3104afa27d704a
Armin Rosu <armin.rosu@gmail.com>
2011-10-15 20:56:20 +0300

    +wikipedia (English)
    Added support for wikipedia contributions, english language only.
    [Wikipedia Api Reference](http://www.mediawiki.org/wiki/API:Query)
    
    By default 10 contributions are returned, but that number can be
    configured. Thinking of setting it to the lifestream count.
    
    The API doesn't return a url for the contribution. It would be possible
    to get it, but that would require another API call, so I chose to
    simply replace spaces with underscores in the title, since this is the
    way wikipedia generates urls anyway (afaik).
    
    The username used in the example has the highest number of edits on
    wikipedia.
    
    Maybe should add a setting for the chosen language …

---
+++ src/services/wikipedia.js
@@ -0,0 +1,47 @@
+(function($) {
+$.fn.lifestream.feeds.wikipedia = function( config, callback ) {
+
+  var template = $.extend({},
+    {
+      contribution: 'contributed to <a href="${url}">${title}</a>'
+    },
+    config.template);
+
+  $.ajax({
+    url: "http://en.wikipedia.org/w/api.php?action=query&ucuser="
+    + config.user + "&list=usercontribs&ucdir=older&format=json",
+    dataType: "jsonp",
+    success: function( data ) {
+      var output = [], i = 0, j;
+
+      if(data && data.query.usercontribs) {
+        j = data.query.usercontribs.length;
+        for( ; i<j; i++) {
+
+          var item = data.query.usercontribs[i];
+          
+          // Fastest way to get the URL. 
+          // Alternatively, we'd have to poll wikipedia for the pageid's link
+          item.url = 'http://en.wikipedia.org/wiki/' 
+          + item.title.replace(' ', '_');
+
+          output.push({
+            date: new Date( item.timestamp ),
+            config: config,
+            html: $.tmpl( template.contribution, item )
+          });
+        }
+      }
+
+      callback(output);
+    }
+  });
+
+  // Expose the template.
+  // We use this to check which templates are available
+  return {
+    "template" : template
+  };
+
+};
+})(jQuery);
