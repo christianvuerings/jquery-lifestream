6b3af6571c13c893a1c34a46594c13ad094b3da8
Ethan Brown <ethan.w.brown@gmail.com>
2011-12-20 11:15:38 -0600

    added zotero

---
+++ src/services/zotero.js
@@ -0,0 +1,46 @@
+(function($) {
+$.fn.lifestream.feeds.zotero = function( config, callback ) {
+
+  var template = $.extend({},
+    {
+      flagged: 'flagged <a href="${id}">${title}</a> by ${creatorSummary}'
+    },
+    config.template),
+
+  parseZotero = function( input ) {
+    var output = [], list, i = 0, j;
+
+    if(input.query && input.query.count && input.query.count > 0) {
+      list = input.query.results.feed.entry;
+      j = list.length;
+      for( ; i<j; i++) {
+        var item = list[i];
+        output.push({
+          date: new Date(item.updated),
+          config: config,
+          url: 'http://zotero.com/users/' + config.user,
+          html: $.tmpl( template.flagged, item ),
+        });
+      }
+    }
+    return output;
+  };
+
+  $.ajax({
+    url: $.fn.lifestream.createYqlUrl('select * from xml where url='
+      + '"https://api.zotero.org/users/'
+      + config.user + '/items"'),
+    dataType: 'jsonp',
+    success: function( data ) {
+      callback(parseZotero(data));
+    }
+  });
+
+  // Expose the template.
+  // We use this to check which templates are available
+  return {
+    "template" : template
+  };
+
+};
+})(jQuery);
