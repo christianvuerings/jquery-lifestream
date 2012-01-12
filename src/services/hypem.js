8ca1b71609801d8e9f4dd6350fa5b9628341b836
Blake Embrey <developer@blakeembrey.com>
2012-01-12 18:15:44 +1000

    Various fixes as described by @christianv

--- src/services/hypem.js
+++ src/services/hypem.js
@@ -1,39 +1,33 @@
 (function($) {
 $.fn.lifestream.feeds.hypem = function( config, callback ) {
 
-  if( !config.type || config.type != "history" || config.type != "loved" )
-    config.type = "loved";
+  if( !config.type || config.type !== "history" || config.type !== "loved" ) { config.type = "loved"; }
 
   var template = $.extend({},
-    {
-      loved: 'loved <a href="http://hypem.com/item/${mediaid}">${title}</a> by <a href="http://hypem.com/artist/${artist}">${artist}</a>',
-      history: 'listened to <a href="http://hypem.com/item/${mediaid}">${title}</a> by <a href="http://hypem.com/artist/${artist}">${artist}</a>'
-    },
-    config.template);
+  {
+    loved: 'loved <a href="http://hypem.com/item/${mediaid}">${title}</a> by <a href="http://hypem.com/artist/${artist}">${artist}</a>',
+    history: 'listened to <a href="http://hypem.com/item/${mediaid}">${title}</a> by <a href="http://hypem.com/artist/${artist}">${artist}</a>'
+  },
+  config.template);
 
   $.ajax({
     url: "http://hypem.com/playlist/" + config.type + "/" + config.user + "/json/1/data.js",
     dataType: "json",
     success: function( data ) {
-      // Minus one for the additional version variable
-      var count = Object.keys(data).length - 1;
-      var output = [], i = 0, j;
-      if (data && count > 0) {
-        for( ; i < count; i++) {
+      var output = [], i = 0, j = -1;
+      for (var k in data) {
+        if (data.hasOwnProperty(k)) {
+          j++;
+        }
+      }
+      if (data && j > 0) {
+        for( ; i < j; i++) {
           var item = data[i];
-          if( config.type == "history" ) {
-            output.push({
-              date: new Date(item.dateplayed * 1000),
-              config: config,
-              html: $.tmpl( template.history, item )
-            });
-          } else {
-            output.push({
-              date: new Date(item.dateloved * 1000),
-              config: config,
-              html: $.tmpl( template.loved, item )
-            });
-          }
+          output.push({
+            date: new Date( (config.type === "history" ? item.dateplayed : item.dateloved) * 1000 ),
+            config: config,
+            html: $.tmpl( (config.type === "history" ? template.history : template.loved) , item )
+          });
         }
       }
       callback(output);
