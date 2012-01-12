12bddd5de1e22f673563b022e89b09987e265fda
Christian Vuerings <vueringschristian@gmail.com>
2012-01-11 20:52:36 -0800

    Fix the template page

--- src/services/wordpress.js
+++ src/services/wordpress.js
@@ -28,19 +28,23 @@ $.fn.lifestream.feeds.wordpress = function( config, callback ) {
     return output;
   };
 
-  // If the config.user property starts with http:// we assume that is the
-  // full url to the user his blog. We append the /feed to the url.
-  var url = (config.user.indexOf('http://') === 0
-             ? config.user + '/feed'
-             : 'http://' + config.user + '.wordpress.com/feed');
-  $.ajax({
-    url: $.fn.lifestream.createYqlUrl('select * from xml where '
-      + 'url="' + url + '"'),
-    dataType: "jsonp",
-    success: function ( data ) {
-      callback(parseWordpress(data));
-    }
-  });
+  var url = "";
+
+  if ( config.user ){
+    // If the config.user property starts with http:// we assume that is the
+    // full url to the user his blog. We append the /feed to the url.
+    url = (config.user.indexOf('http://') === 0
+               ? config.user + '/feed'
+               : 'http://' + config.user + '.wordpress.com/feed');
+    $.ajax({
+      url: $.fn.lifestream.createYqlUrl('select * from xml where '
+        + 'url="' + url + '"'),
+      dataType: "jsonp",
+      success: function ( data ) {
+        callback(parseWordpress(data));
+      }
+    });
+  }
 
   // Expose the template.
   // We use this to check which templates are available
