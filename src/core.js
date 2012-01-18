5541bb2142fad1a30203808a237ba5a8634932a8
Christian Vuerings <vueringschristian@gmail.com>
2012-01-17 20:34:51 -0800

    Add support for Object.keys on many browsers

--- src/core.js
+++ src/core.js
@@ -168,4 +168,17 @@
    */
   $.fn.lifestream.feeds = $.fn.lifestream.feeds || {};
 
+  /**
+   * Add compatible Object.keys support in older environments that do not natively support it
+   * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/keys#section_6
+   */
+  if(!Object.keys) Object.keys = function(o){
+    if (o !== Object(o)){
+      throw new TypeError('Object.keys called on non-object');
+    }
+    var ret=[],p;
+    for(p in o) if(Object.prototype.hasOwnProperty.call(o,p)) ret.push(p);
+    return ret;
+  }
+
 }( jQuery ));
