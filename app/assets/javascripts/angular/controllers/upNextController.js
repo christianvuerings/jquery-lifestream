266ebe2a7bbefad44172f3d944b5438721a7d2e6
Scot Hacker <shacker@birdhouse.org>
2012-11-17 00:32:53 -0800

    CLC-675 Added calendar filters to prevent unneeded DOM rendering

--- app/assets/javascripts/angular/controllers/upNextController.js
+++ app/assets/javascripts/angular/controllers/upNextController.js
@@ -11,6 +11,16 @@
 
       $scope.items = data.items;
 
+      $scope.isAllDay = function(item) {
+        // Filter for all-day calendar items, which have a "date" property
+        return item.start.date ? true : false;
+      }
+
+      $scope.notAllDay = function(item) {
+        // Filter for timed calendar items, which have a "dateTime" property
+        return item.start.dateTime ? true : false;
+      }
+
     });
 
   }]);
