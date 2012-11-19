93ff8d2bca81aa253d27dd27b94d1c7e98bb7124
Scot Hacker <shacker@birdhouse.org>
2012-11-19 14:29:35 -0800

    CLC-675 Simplify and snake_case dummy JSON in UpNext

--- app/assets/javascripts/angular/controllers/upNextController.js
+++ app/assets/javascripts/angular/controllers/upNextController.js
@@ -13,13 +13,13 @@
 
       $scope.isAllDay = function(item) {
         // Filter for all-day calendar items, which have a "date" property
-        return item.start.date ? true : false;
-      }
+        return !!item.start.date;
+      };
 
       $scope.notAllDay = function(item) {
         // Filter for timed calendar items, which have a "dateTime" property
-        return item.start.dateTime ? true : false;
-      }
+        return !!item.start.date_time;
+      };
 
     });
 
