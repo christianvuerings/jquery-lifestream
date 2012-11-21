35229db93f500406fd69035b933f659778f52932
Chris Tweney <chris@media.berkeley.edu>
2012-11-21 13:22:25 -0800

    CLC-727 Fix failing tests by adding query-string matching to VCR recording

--- lib/fakeable_proxy.rb
+++ lib/fakeable_proxy.rb
@@ -4,17 +4,25 @@ module FakeableProxy
     c.hook_into :fakeweb, :faraday
     c.allow_http_connections_when_no_cassette = true
     c.debug_logger = File.open(Rails.root.join("log", "vcr-debug.log"), 'w')
-    c.after_http_request do |request,response|
+    c.after_http_request do |request, response|
       # so we don't record sensitive auth headers
-      response.headers['Authorization'], request.headers['Authorization'], response.headers['x-sakai-token'], request.headers['x-sakai-token'] = ''
+      if !response.blank?
+        response.headers['Authorization'], response.headers['x-sakai-token'] = ''
+      end
+      if !request.blank?
+        request.headers['Authorization'], request.headers['x-sakai-token'] = ''
+      end
     end
   end
 
   def FakeableProxy.wrap_request(proxy_id, force_fake = nil)
     if force_fake || Settings.freshen_vcr
+      query_string_matcher = lambda do |a, b|
+        URI(a.uri).query == URI(b.uri).query
+      end
       VCR.use_cassette(proxy_id,
                        :allow_playback_repeats => true,
-                       :match_requests_on => [:uri, :path],
+                       :match_requests_on => [:method, :path, query_string_matcher],
                        :record => :new_episodes,
                        :serialize_with => :json,
                        :preserve_exact_body_bytes => false) do
