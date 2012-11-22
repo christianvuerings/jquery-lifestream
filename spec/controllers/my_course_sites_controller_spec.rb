ba8930974be30175a6916d22c04c743a7a619a26
Yu-Hung Lin <yuhung@yuhunglin.com>
2012-11-22 02:10:35 -0800

    [CLC-658] First stab at the upnext/tasks task

---
+++ spec/controllers/my_course_sites_controller_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe MyCourseSitesController do
+
+  before(:each) do
+    @user_id = rand(99999).to_s
+  end
+
+  it "should be an empty course sites feed on non-authenticated user" do
+    get :get_feed
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    json_response.should == {}
+  end
+
+  it "should be an non-empty course feed on authenticated user" do
+    MyCourseSites.stub(:get_feed).and_return(
+      [{course_code: "PLEO 22",
+      id: "750027",
+      emitter: "Canvas"}])
+    session[:user_id] = @user_id
+    get :get_feed
+    json_response = JSON.parse(response.body)
+    json_response.size.should == 1
+    json_response[0]["course_code"].should == "PLEO 22"
+  end
+
+end
---
+++ spec/controllers/my_tasks_controller_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe MyTasksController do
+
+  before(:each) do
+    @user_id = rand(99999).to_s
+  end
+
+  it "should be an empty task feed on non-authenticated user" do
+    get :get_feed
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    json_response.should == {}
+  end
+
+  it "should be an non-empty task feed on authenticated user" do
+    session[:user_id] = @user_id
+    get :get_feed
+    json_response = JSON.parse(response.body)
+    json_response.should_not == {}
+    json_response["sections"].length.should == 4
+    default_sections = ["Due", "Due Tomorrow", "Upcoming", "Unscheduled"]
+    json_response["sections"].each do |section|
+      default_sections.include?(section["title"]).should be true
+    end
+
+  end
+end
