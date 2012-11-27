972666453e30d3c648f34e3b02f76c3b1ee2418b
Chris Tweney <chris@media.berkeley.edu>
2012-11-27 06:38:48 -0800

    CLC-526 Handle final_redirect url via state param

--- app/controllers/canvas_oauth2_controller.rb
+++ app/controllers/canvas_oauth2_controller.rb
@@ -2,37 +2,47 @@ require 'signet/oauth_2/client'
 
 class CanvasOauth2Controller < ApplicationController
 
-  def get_client
+  def get_client(final_redirect)
     Signet::OAuth2::Client.new(
         :authorization_uri => Settings.canvas_proxy.authorization_uri,
         :token_credential_uri => Settings.canvas_proxy.token_credential_uri,
         :client_id => Settings.canvas_proxy.client_id,
         :client_secret => Settings.canvas_proxy.client_secret,
-        :redirect_uri => url_for(:only_path => false, :controller => 'canvasOauth2', :action => "handle_callback")
+        :redirect_uri => url_for(
+            :only_path => false,
+            :controller => 'canvasOauth2',
+            :action => "handle_callback",
+            :state => Base64.encode64(final_redirect))
     )
   end
 
   def request_authorization
-    client = get_client
+    state = params[:final_redirect] || "/profile"
+    client = get_client state
     url = client.authorization_uri.to_s
     Rails.logger.debug "Initiating Oauth2 authorization request for user #{session[:user_id]} - redirecting to #{url}"
     redirect_to url
   end
 
   def handle_callback
-    client = get_client
+    client = get_client ""
     Rails.logger.debug "Handling Oauth2 authorization callback for user #{session[:user_id]}, fetching token from #{client.token_credential_uri}"
-    if request.parameters[:code]
-      client.code = request.parameters[:code]
+    if params[:code]
+      client.code = params[:code]
       token_response = client.fetch_access_token
+      Rails.logger.debug "handle_callback params = #{params}"
       oauth2 = Oauth2Data.where(
           uid: session[:user_id],
           app_id: "canvas").first_or_initialize
       oauth2.access_token = token_response["access_token"]
       oauth2.save
-      MyCourseSites.expire session[:user_id]
+    else
+      # TODO delete tokens
     end
-    redirect_to "/profile"
+    MyCourseSites.expire session[:user_id]
+    final_redirect = params[:state] || "/profile"
+    final_redirect = Base64.decode64 final_redirect
+    redirect_to final_redirect
   end
 
 end
