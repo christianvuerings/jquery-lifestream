aa0ff03938271344d166942f6dfd8a5593f1b4d6
Christian Vuerings <vueringschristian@gmail.com>
2012-11-27 10:25:01 -0800

    CLC-745 - Front-end - Implement the oauth API exposed by the back-end https://jira.media.berkeley.edu/jira/browse/CLC-745
    * Use the real API from the back-end
    * Redirect to the profile page instead of the dashboard
    * Show something when there are no classes available
    * Remove more references to preferences
    * Remove the preferences controller
    * Fix link to the profile page in the launcher bar
    
    We'll be hiding the Google button for now since this hasn't been merged yet.
    
    **This doesn't close the jira ticket yet, it's just one of the steps**

--- app/assets/javascripts/angular/controllers/myClassesController.js
+++ app/assets/javascripts/angular/controllers/myClassesController.js
@@ -7,7 +7,7 @@
    */
   calcentral.controller('MyClassesController', ['$http', '$scope', function($http, $scope) {
 
-    $http.get('/dummy/myclasses.json').success(function(data) {
+    $http.get('/api/my/course_sites').success(function(data) {
 
       $scope.myclasses = data;
 
