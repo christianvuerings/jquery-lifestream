cae000c8975ffe15b1ed36021a6f10eac46d8ea4
Chris Tweney <chris@media.berkeley.edu>
2012-11-27 14:04:45 -0800

    CLC-526 Rewrite get_client to be more clear per Ray feedback; constantize the Canvas app ID

--- app/controllers/canvas_oauth2_controller.rb
+++ app/controllers/canvas_oauth2_controller.rb
@@ -2,7 +2,7 @@ require 'signet/oauth_2/client'
 
 class CanvasOauth2Controller < ApplicationController
 
-  def get_client(final_redirect)
+  def get_client(final_redirect = "")
     Signet::OAuth2::Client.new(
         :authorization_uri => Settings.canvas_proxy.authorization_uri,
         :token_credential_uri => Settings.canvas_proxy.token_credential_uri,
@@ -17,15 +17,15 @@ class CanvasOauth2Controller < ApplicationController
   end
 
   def request_authorization
-    state = params[:final_redirect] || "/profile"
-    client = get_client state
+    final_redirect = params[:final_redirect] || "/profile"
+    client = get_client final_redirect
     url = client.authorization_uri.to_s
     Rails.logger.debug "Initiating Oauth2 authorization request for user #{session[:user_id]} - redirecting to #{url}"
     redirect_to url
   end
 
   def handle_callback
-    client = get_client ""
+    client = get_client
     Rails.logger.debug "Handling Oauth2 authorization callback for user #{session[:user_id]}, fetching token from #{client.token_credential_uri}"
     if params[:code]
       client.code = params[:code]
@@ -33,11 +33,13 @@ class CanvasOauth2Controller < ApplicationController
       Rails.logger.debug "handle_callback params = #{params}"
       oauth2 = Oauth2Data.where(
           uid: session[:user_id],
-          app_id: "canvas").first_or_initialize
+          app_id: CanvasProxy::APP_ID).first_or_initialize
       oauth2.access_token = token_response["access_token"]
+      Rails.logger.debug "Saving Canvas token for user #{session[:user_id]}"
       oauth2.save
     else
-      # TODO delete tokens
+      Rails.logger.debug "Deleting Canvas token for user #{session[:user_id]}"
+      Oauth2Data.delete_all(:uid => session[:user_id], :app_id => CanvasProxy::APP_ID)
     end
     MyCourseSites.expire session[:user_id]
     final_redirect = params[:state] || "/profile"
