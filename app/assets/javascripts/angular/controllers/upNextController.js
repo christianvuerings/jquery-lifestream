1b7631f5727109e6b5d51c41ff6209bb29a1b6f8
Scot Hacker <shacker@birdhouse.org>
2012-11-29 17:18:32 -0800

    CLC-747 Dashboard redesign WIP

--- app/assets/javascripts/angular/controllers/upNextController.js
+++ app/assets/javascripts/angular/controllers/upNextController.js
@@ -12,13 +12,13 @@
       $scope.items = data.items;
 
       $scope.isAllDay = function(item) {
-        // Filter for all-day calendar items, which have a "date" property
-        return !!item.start.date;
+        // Filter for all-day calendar items
+        return !!item.is_all_day;
       };
 
       $scope.notAllDay = function(item) {
-        // Filter for timed calendar items, which have a "dateTime" property
-        return !!item.start.date_time;
+        // Filter for timed calendar items
+        return !item.is_all_day;
       };
 
     });
