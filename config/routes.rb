c7f1d4313aea30278899427520f29d3de4629473
Yu-Hung Lin <yuhung@yuhunglin.com>
2012-12-03 16:45:43 -0800

    [CLC-766] Backend API endpoints for my groups

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,7 @@ Calcentral::Application.routes.draw do
   match '/api/my/course_sites' => 'my_course_sites#get_feed', :as => :my_course_sites, :defaults => { :format => 'json' }
   match '/api/my/up_next' => 'my_up_next#get_feed', :as => :my_up_next, :defaults => { :format => 'json' }
   match '/api/my/tasks' => 'my_tasks#get_feed', :as => :my_tasks, :defaults => { :format => 'json' }
+  match '/api/my/groups' => 'my_group_sites#get_feed', :as => :my_groups, :defaults => { :format => 'json' }
 
   match '/api/canvas/request_authorization' => 'canvas_auth#request_authorization'
   match '/api/canvas/handle_callback' => 'canvas_auth#handle_callback'
