e17e9d468f7af2bf7ab770d9a052614362afd21d
Chris Tweney <chris@media.berkeley.edu>
2012-12-05 10:15:56 -0800

    Squashed commit of the following:
    commit 7921b36217d8c0d1ebaa058aefef537ec9651b63
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Wed Dec 5 09:34:26 2012 -0800
    
        CLC-696 Tweak log levels, set default log level back to WARN
    
    commit b423ffde7b7d606fc2ee7a8a8d73fc1fb6d984c6
    Merge: 1645939 88478ab
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Wed Dec 5 09:04:11 2012 -0800
    
        Merge pull request #2 from yuhunglin/CLC-696
    
        Timezone aware fixes
    
    commit 88478ab537ec280f390177c70d8a755d1658f302
    Author: Yu-Hung Lin <yuhung@yuhunglin.com>
    Date:   Wed Dec 5 05:44:32 2012 -0800
    
        fixes on element on edge case of day_after_tomorrow, not so wonderful fudging of google task dates
    
    commit 6478bb578d226ebdfa089b6a76ebab8a243b22dc
    Author: Yu-Hung Lin <yuhung@yuhunglin.com>
    Date:   Wed Dec 5 04:08:29 2012 -0800
    
        fixes
    
    commit 16459390708e5562227b5870fb6207ab3d421a3f
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 18:32:09 2012 -0800
    
        CLC-696 In test, instantiating starting_date as a Time object should make the test pass even when the machine is in UTC
    
    commit 0d88d4737fcd9567b9a158d7447ab06d2557f3c0
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 18:00:33 2012 -0800
    
        CLC-696 Using Date.civil instead of Date.new may give us the right GMT timezone
    
    commit 3f5f4c00bdb92f75733bf03d51ec83383f4370eb
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 17:42:00 2012 -0800
    
        CLC-696 Add more log to determine_bucket method
    
    commit 567fe4c6ca102ec4f2b399dd9b5526097adef9d6
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 17:23:08 2012 -0800
    
        CLC-696 Increase log level another way
    
    commit 48fe04addd4be8bc458ba67ed6fbb53219329d82
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 16:53:34 2012 -0800
    
        CLC-696 Log more chattily so I can see what's going on
    
    commit 5cc14999c4974a95fd5a33c4e41e74382e955b92
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 16:37:04 2012 -0800
    
        CLC-696 Attempt to fix Travis test
    
    commit 3762aeb4318772a589220cbf89e7c1c6c6a30580
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 16:22:54 2012 -0800
    
        CLC-696 Fix test
    
    commit 90644627c5dd4588ecd8200a9d79a5349f877c9b
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 16:15:39 2012 -0800
    
        CLC-696 Get all the Canvas data into task feed, and test it
    
    commit 25e30c347638cc2725fcdc40e3600b69c50bc70a
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 11:36:15 2012 -0800
    
        CLC-696 Make some CanvasProxy tests part of testext
    
    commit 53aaf91e37aadb94c0b639320a4eb8bb08663398
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 09:46:38 2012 -0800
    
        CLC-696 Make MyTasks model more object oriented
    
    commit 83262918dd26b7a4c3716c80ae3556e5291a2a72
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Tue Dec 4 09:20:08 2012 -0800
    
        CLC-696 Add coming_up feed to Canvas proxy

--- spec/models/my_tasks_spec.rb
+++ spec/models/my_tasks_spec.rb
@@ -5,31 +5,70 @@ describe "MyTasks" do
     @user_id = rand(99999).to_s
     @fake_google_proxy = GoogleProxy.new({fake: true})
     @fake_google_tasks_array = @fake_google_proxy.tasks_list()
+    @fake_canvas_proxy = CanvasProxy.new({fake: true})
   end
 
-  it "should load nicely with the pre-recorded fake Google proxy feed for tasks#list" do
+  it "should load nicely with the pre-recorded fake Google and Canvas proxy feeds using the server's timezone" do
     GoogleProxy.stub(:access_granted?).and_return(true)
+    CanvasProxy.stub(:access_granted?).and_return(true)
     GoogleProxy.stub(:new).and_return(@fake_google_proxy)
-    GoogleProxy.any_instance.stub(:events_list).and_return(@fake_google_tasks_array)
-    valid_feed = MyTasks.get_feed(@user_id, Date.new(2012, 11, 27))
+    CanvasProxy.stub(:new).and_return(@fake_canvas_proxy)
+    my_tasks_model = MyTasks.new(@user_id, Date.new(2012, 11, 27).to_time_in_current_zone)
+    valid_feed = my_tasks_model.get_feed
     valid_feed["sections"].length.should == 4
     valid_feed["sections"][0]["title"].should == "Due"
-    valid_feed["sections"][0]["tasks"].size.should == 1
+    valid_feed["sections"][0]["tasks"].size.should == 3
+    valid_feed["sections"][1]["title"].should == "Due Tomorrow"
+    valid_feed["sections"][1]["tasks"].size.should == 0
     valid_feed["sections"][2]["title"].should == "Upcoming"
-    valid_feed["sections"][2]["tasks"].size.should == 3
+    valid_feed["sections"][2]["tasks"].size.should == 4
+    valid_feed["sections"][3]["title"].should == "Unscheduled"
+    valid_feed["sections"][3]["tasks"].size.should == 1
     valid_feed["sections"].each do |section|
       section["tasks"].each do |task|
         task["title"].blank?.should == false
-        task["link_url"].should == "https://mail.google.com/tasks/canvas?pli=1"
         task["source_url"].blank?.should == false
-        if task["due_date"]
-          task["due_date"]["date_string"].length.should <= 5 #cheating with a "xx/xx" length check
-        end
+
         if task["emitter"] == "Google Tasks"
-          task["class"] == "class2"
+          task["link_url"].should == "https://mail.google.com/tasks/canvas?pli=1"
+          task["class"].should == "class2"
+          if task["due_date"]
+            task["due_date"]["date_string"] =~ /\d\d\/\d\d/
+            task["due_date"]["epoch"].should >= 1351641600
+          end
+        end
+        if task["emitter"] == CanvasProxy::APP_ID
+          task["link_url"].should =~ /https:\/\/ucberkeley.instructure.com\/courses/
+          task["link_url"].should == task["source_url"]
+          task["color_class"].should == "class1"
+          task["due_date"]["date_string"] =~ /\d\d\/\d\d/
+          task["due_date"]["epoch"].should >= 1354060800
         end
       end
     end
   end
 
+  it "should shift tasks into different buckets with a different timezone " do
+    #(since Berkeley assignments are probably always due at -0800, irregardless of where the student is)
+    old_time_zone = Time.zone
+    Time.zone = 13.hours
+
+    GoogleProxy.stub(:access_granted?).and_return(true)
+    CanvasProxy.stub(:access_granted?).and_return(true)
+    GoogleProxy.stub(:new).and_return(@fake_google_proxy)
+    CanvasProxy.stub(:new).and_return(@fake_canvas_proxy)
+    my_tasks_model = MyTasks.new(@user_id, Date.new(2012, 11, 27).to_time_in_current_zone)
+    valid_feed = my_tasks_model.get_feed
+    valid_feed["sections"][0]["title"].should == "Due"
+    valid_feed["sections"][0]["tasks"].size.should == 1
+    valid_feed["sections"][1]["title"].should == "Due Tomorrow"
+    valid_feed["sections"][1]["tasks"].size.should == 2
+    valid_feed["sections"][2]["title"].should == "Upcoming"
+    valid_feed["sections"][2]["tasks"].size.should == 4
+    valid_feed["sections"][3]["title"].should == "Unscheduled"
+    valid_feed["sections"][3]["tasks"].size.should == 1
+
+    Time.zone = old_time_zone
+
+  end
 end
