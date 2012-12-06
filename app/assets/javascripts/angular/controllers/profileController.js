6eb4ebb1d7466f5af88cfb88778cdba0fb8b0e21
Christian Vuerings <vueringschristian@gmail.com>
2012-12-06 11:59:52 -0800

    CLC-829 - remove request to /dummy/my_status.json on the profile page

--- app/assets/javascripts/angular/controllers/profileController.js
+++ app/assets/javascripts/angular/controllers/profileController.js
@@ -7,10 +7,6 @@
    */
   calcentral.controller('ProfileController', ['$rootScope', '$http', '$scope', function($rootScope, $http, $scope) {
 
-    $http.get('/dummy/mystatus.json').success(function(data) {
-      $scope.status = data;
-    });
-
     $rootScope.title = 'Profile | CalCentral';
 
   }]);
