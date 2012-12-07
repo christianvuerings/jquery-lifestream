71011a40827fbf26eb61fc613caff04026907aa6
Christian Vuerings <vueringschristian@gmail.com>
2012-12-07 15:58:14 -0800

    CLC-838 - Attach the csrf token to every post/put/delete request in Angular https://jira.media.berkeley.edu/jira/browse/CLC-838

--- app/assets/javascripts/angular/calcentral.js
+++ app/assets/javascripts/angular/calcentral.js
@@ -9,7 +9,7 @@
   var calcentral = angular.module('calcentral', []);
 
   // Set the configuration
-  calcentral.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  calcentral.config(['$httpProvider', '$locationProvider', '$routeProvider', function($httpProvider, $locationProvider, $routeProvider) {
 
     // We set it to html5 mode so we don't have hash bang URLs
     $locationProvider.html5Mode(true).hashPrefix('!');
@@ -35,6 +35,11 @@
       controller: 'ErrorController',
       isPublic: true
     });
+
+    // Setting up CSRF tokens for POST, PUT and DELETE requests
+    var token = document.querySelector('meta[name=csrf-token]').content;
+    $httpProvider.defaults.headers.post['X-CSRF-Token'] = token;
+    $httpProvider.defaults.headers.put['X-CSRF-Token'] = token;
 
   }]);
 
