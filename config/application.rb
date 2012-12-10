7a03e5141a4378096e02f46b0a8e53fcf2d8daa2
Ray Davis <ray@media.berkeley.edu>
2012-12-10 13:09:19 -0800

    NOJIRA Ensure that configuration code includes Rails extensions This was a hidden requirement that only showed up in production env. For other envs, it had been taken of by the zurb-foundation gem.

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'log4r'
 include Log4r
+require 'active_support/core_ext'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
