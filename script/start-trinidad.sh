ff942d426b31cea8ceae67bdd9ff4bd395652e41
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-01-09 12:38:10 -0800

    [CLC-917] Calling stop-trinidad before attempting to start

--- script/start-trinidad.sh
+++ script/start-trinidad.sh
@@ -9,6 +9,12 @@ LOGIT="tee -a $LOG"
 # TODO Make sure memcached is running.
 # /usr/local/bin/memcached -d
 
+# Kill all instances of trinidad if there are any running.
+echo | $LOGIT
+echo "------------------------------------------" | $LOGIT
+echo "`date`: Stopping running instances of CalCentral..." | $LOGIT
+./script/stop-trinidad.sh
+
 # Enable rvm and use the correct Ruby version and gem set.
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 source .rvmrc
