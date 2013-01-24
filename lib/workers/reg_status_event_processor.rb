58c4675ce5d9a8be7e49666f8e1835fe17ceb8f9
Chris Tweney <chris@media.berkeley.edu>
2013-01-24 14:50:32 -0800

    Squashed commit of the following:
    commit d4533a526e1df9b27c2e2a998cb321ec6dee719a
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Thu Jan 24 14:49:47 2013 -0800
    
        CLC-1050 Fill in rest of the translations
    
    commit 0a349b14f1874f0ecfe1067f7facd75092a18175
    Author: Chris Tweney <chris@media.berkeley.edu>
    Date:   Thu Jan 24 14:28:59 2013 -0800
    
        CLC-1050 Changes per review: fix whitespace, make current_term system codes a setting

--- lib/workers/reg_status_event_processor.rb
+++ lib/workers/reg_status_event_processor.rb
@@ -35,10 +35,40 @@ class RegStatusEventProcessor
   end
 
   def translate_status(reg_status)
-    # TODO fill in other status code translations.
+    # TODO resolve gaps and ??? marks - CLC-1069
     case
+      when ""
+        '"admitted / not registered." To complete your registration you must pay all registration fees, have no outstanding "blocks" and be enrolled in at least one course.'
+      when "A"
+        '"applied / not registered for Summer Session."  Check with summer session on you application status and if you have any outstanding fees owed.'
       when "C"
         '"registered, continuing." You are an active registered student in your second or subsequent semester.'
+      when "L"
+        '"registered, limited." You have all the privileges of student except ???'
+      when "N"
+        '"registered, new." Congratulations you have completed the admissions and registration steps and are now a newly admitted student.'
+      when "R"
+        '"registered, re-admitted." Readmission usually occurs after you have taken one or more semesters off, and following a withdrawal application, or if you took a semester to study abroad.'
+      when "V"
+        '"registered, visitor." You have been provided limited student privileges.'
+      when "D"
+        '"not registered, academic dismissed." You no longer have the privileges of a registered student. For more information, please contact the Office of Student Conduct.'
+      when "F"
+        '"not registered, incomplete (withdrawal)." Your request to withdraw from the current semester has been processed.'
+      when "U"
+        '"not registered, admin cancellation." You have completed your program or graduated.'
+      when "I"
+        '"registered, potential deletes." ?????'
+      when "X"
+        '"not registered, registration cancelled." Your cancellation request has been processed.'
+      when "Y"
+        '"registered, roster number changed." ?????'
+      when "Z"
+        '"not registered, deceased." The Office of the Registrar has been notified of the death of this student ID.'
+      when "W"
+        '"not registered, withdrawn." You have been withdrawn by a university official for one of the following reasons ????'
+      when "S"
+        '"registered for summer session." Congratulations you are registered for Summer Session classes.'
       else
         'Unknown Status.'
     end
