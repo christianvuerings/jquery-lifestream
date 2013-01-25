3d14e4b36b5fd40cde62b04f1f3e4cf3938e6cea
Chris Tweney <chris@media.berkeley.edu>
2013-01-25 15:59:55 -0800

    CLC-1075 Connecting /api/my/notifications feed to the Notification backend storage

--- app/models/my_notifications.rb
+++ app/models/my_notifications.rb
@@ -2,9 +2,8 @@ require 'json'
 
 class MyNotifications < MyMergedModel
 
-  def get_feed_internal(opts={})
-    data = {}
-    data
+  def get_feed_internal
+    {:notifications => Notification.where(:uid => @uid)}
   end
 
 end
