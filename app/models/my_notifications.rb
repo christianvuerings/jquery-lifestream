63e3ccb3b820571347c7d16a61701657ce884ddb
Chris Tweney <chris@media.berkeley.edu>
2013-01-28 15:08:26 -0800

    CLC-1075 Be more efficient instantiating translators

--- app/models/my_notifications.rb
+++ app/models/my_notifications.rb
@@ -2,10 +2,14 @@ require 'json'
 
 class MyNotifications < MyMergedModel
 
+  def self.translators
+    @translators ||= {}
+  end
+
   def get_feed_internal
     notifications = []
     Notification.where(:uid => @uid).each do |notification|
-      translator = notification.translator.constantize.new
+      translator = (MyNotifications.translators[notification.translator] ||= notification.translator.constantize.new)
       notifications.push translator.translate notification
     end
     {:notifications => notifications}
