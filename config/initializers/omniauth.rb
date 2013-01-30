4341fc2a2d143c7671f5dc956491a470cb270e33
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-01-29 16:21:05 -0800

    [NOJIRA] Need more ways of bypassing cas

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cas, :host => Settings.cas_server
 end
+
+if Settings.application.fake_cas && Settings.application.fake_cas_id
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:cas] = OmniAuth::AuthHash.new(
+    {
+      :provider => 'cas',
+      :uid => Settings.application.fake_cas_id
+    })
+end
