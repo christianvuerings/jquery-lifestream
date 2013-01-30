75694e72d09de460222519e888039e8425f5e3b5
Chris Tweney <chris@media.berkeley.edu>
2013-01-30 14:42:21 -0800

    CLC-1075 Update the reg-status translations

--- lib/notifications/reg_status_translator.rb
+++ lib/notifications/reg_status_translator.rb
@@ -42,39 +42,42 @@ class RegStatusTranslator
 
   def translate_status(reg_status)
     # TODO resolve gaps and ??? marks - CLC-1069
+    unregistered = "NOT REGISTERED. In order to be officially registered, you must pay at least 20% of your registration fees, have no outstanding blocks, and be enrolled in at least one class."
+    registered = "REGISTERED. You are officially registered for this term and are entitled to access campus services."
+
     case reg_status.upcase
       when ""
-        '"admitted / not registered." To complete your registration you must pay all registration fees, have no outstanding "blocks" and be enrolled in at least one course.'
+        unregistered
       when "A"
-        '"applied / not registered for Summer Session."  Check with summer session on you application status and if you have any outstanding fees owed.'
+        unregistered
       when "C"
-        '"registered, continuing." You are an active registered student in your second or subsequent semester.'
+        registered
       when "L"
-        '"registered, limited." You have all the privileges of student except ???'
+        registered
       when "N"
-        '"registered, new." Congratulations you have completed the admissions and registration steps and are now a newly admitted student.'
+        registered
       when "R"
-        '"registered, re-admitted." Readmission usually occurs after you have taken one or more semesters off, and following a withdrawal application, or if you took a semester to study abroad.'
+        registered
       when "V"
-        '"registered, visitor." You have been provided limited student privileges.'
+        registered
       when "D"
-        '"not registered, academic dismissed." You no longer have the privileges of a registered student. For more information, please contact the Office of Student Conduct.'
+        'DISMISSED. You have been academically dismissed for this term.'
       when "F"
         '"not registered, incomplete (withdrawal)." Your request to withdraw from the current semester has been processed.'
       when "U"
-        '"not registered, admin cancellation." You have completed your program or graduated.'
+        'ADMIN CANCELLED. You have been administratively cancelled for this term.'
       when "I"
         '"registered, potential deletes." ?????'
       when "X"
-        '"not registered, registration cancelled." Your cancellation request has been processed.'
+        'CANCELLED. Your registration has been canceled for this term.'
       when "Y"
         '"registered, roster number changed." ?????'
       when "Z"
         '"not registered, deceased." The Office of the Registrar has been notified of the death of this student ID.'
       when "W"
-        '"not registered, withdrawn." You have been withdrawn by a university official for one of the following reasons ????'
+        'WITHDRAWN. You are withdrawn for this term and may owe fees depending on your date of withdrawal'
       when "S"
-        '"registered for summer session." Congratulations you are registered for Summer Session classes.'
+        registered
       else
         'Unknown Status.'
     end
