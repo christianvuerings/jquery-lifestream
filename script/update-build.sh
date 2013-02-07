05c30ae273c2dadfd318667e0a3c49652299e9da
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-02-07 14:19:26 -0800

    [CLC-1224] Update-build script fixes

--- script/update-build.sh
+++ script/update-build.sh
@@ -2,19 +2,20 @@
 # Script to update and build a shared deployment of CalCentral.
 
 cd $( dirname "${BASH_SOURCE[0]}" )/..
-
+TARGET_REMOTE=${REMOTE:-origin}
+TARGET_BRANCH=${BRANCH:-master}
 LOG=`date +"log/start-stop_%Y-%m-%d.log"`
 LOGIT="tee -a $LOG"
 
 echo "=========================================" | $LOGIT
-echo "`date`: Updating CalCentral source code" | $LOGIT
-git fetch origin >> $LOG 2>&1
-git checkout -qf #{branch} >> $LOG 2>&1
-git reset --hard HEAD >>$LOG 2>&1
-git pull >>$LOG 2>&1
+echo "`date`: Updating CalCentral source code from:" $TARGET_REMOTE ", branch:" $TARGET_BRANCH | $LOGIT
+git fetch $TARGET_REMOTE 2>&1 | $LOGIT
+git checkout -qf $TARGET_BRANCH 2>&1 | $LOGIT
+git reset --hard HEAD 2>&1 | $LOGIT
+git pull 2>&1 | $LOGIT
 echo "Last commit:" | $LOGIT
 git log -1 | $LOGIT
-echo | $LOGIT\
+echo | $LOGIT
 
 echo | $LOGIT
 echo "------------------------------------------" | $LOGIT
