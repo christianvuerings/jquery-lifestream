d0502679ef14ffb3773d63869ee55ea438e139cd
Scot Hacker <shacker@birdhouse.org>
2013-02-08 13:35:46 -0800

    CLC-1205 Canvas assignments missing details text https://jira.media.berkeley.edu/jira/browse/CLC-1205

--- app/models/my_tasks/canvas_tasks.rb
+++ app/models/my_tasks/canvas_tasks.rb
@@ -12,39 +12,11 @@ class MyTasks::CanvasTasks
   def fetch_tasks!(tasks)
     # Track assignment IDs to filter duplicates.
     assignments = Set.new
-    fetch_canvas_coming_up!(CanvasComingUpProxy.new(:user_id => @uid), tasks, assignments)
     fetch_canvas_todo!(CanvasTodoProxy.new(:user_id => @uid), tasks, assignments)
+    fetch_canvas_coming_up!(CanvasComingUpProxy.new(:user_id => @uid), tasks, assignments)
   end
 
   private
-
-  def fetch_canvas_coming_up!(canvas_proxy, tasks, assignments)
-    response = canvas_proxy.coming_up
-    if response && (response.status == 200)
-      results = JSON.parse response.body
-      Rails.logger.info "#{self.class.name} Sorting Canvas coming_up feed into buckets with starting_date #{@starting_date}"
-      results.each do |result|
-        if (result["type"] != "Assignment") || new_assignment?(result, assignments)
-          formatted_entry = {
-            "type" => result["type"].downcase,
-            "title" => result["title"],
-            "emitter" => CanvasProxy::APP_ID,
-            "link_url" => result["html_url"],
-            "source_url" => result["html_url"],
-            "color_class" => "canvas-class",
-            "status" => "inprogress"
-          }
-          due_date = result["start_at"]
-          due_date = convert_due_date(due_date)
-          format_date_into_entry!(due_date, formatted_entry, "due_date")
-          bucket = determine_bucket(due_date, formatted_entry, @now_time, @starting_date)
-          formatted_entry["bucket"] = bucket
-          Rails.logger.info "#{self.class.name} Putting Canvas coming_up event with due_date #{formatted_entry["due_date"]} in #{bucket} bucket: #{formatted_entry}"
-          tasks.push(formatted_entry)
-        end
-      end
-    end
-  end
 
   def fetch_canvas_todo!(canvas_proxy, tasks, assignments)
     response = canvas_proxy.todo
@@ -64,10 +36,44 @@ class MyTasks::CanvasTasks
             "color_class" => "canvas-class",
             "status" => "inprogress"
           }
+          if result["assignment"]["description"] != ""
+            formatted_entry["note"] = ActionView::Base.full_sanitizer.sanitize(result["assignment"]["description"])
+          end
           format_date_into_entry!(due_date, formatted_entry, "due_date")
           bucket = determine_bucket(due_date, formatted_entry, @now_time, @starting_date)
           formatted_entry["bucket"] = bucket
           Rails.logger.info "#{self.class.name} Putting Canvas todo with due_date #{formatted_entry["due_date"]} in #{bucket} bucket: #{formatted_entry}"
+          tasks.push(formatted_entry)
+        end
+      end
+    end
+  end
+
+  def fetch_canvas_coming_up!(canvas_proxy, tasks, assignments)
+    response = canvas_proxy.coming_up
+    if response && (response.status == 200)
+      results = JSON.parse response.body
+      Rails.logger.info "#{self.class.name} Sorting Canvas coming_up feed into buckets with starting_date #{@starting_date}"
+      results.each do |result|
+
+        Rails.logger.debug "Coming up assignment = #{result["html_url"]}"
+
+        if (result["type"] != "Assignment") || new_assignment?(result, assignments)
+          formatted_entry = {
+            "type" => result["type"].downcase,
+            "title" => result["title"],
+            "emitter" => CanvasProxy::APP_ID,
+            "link_url" => result["html_url"],
+            "source_url" => result["html_url"],
+            "color_class" => "canvas-class",
+            "status" => "inprogress"
+          }
+          due_date = result["start_at"]
+          due_date = convert_due_date(due_date)
+          format_date_into_entry!(due_date, formatted_entry, "due_date")
+          bucket = determine_bucket(due_date, formatted_entry, @now_time, @starting_date)
+          formatted_entry["bucket"] = bucket
+          Rails.logger.info "#{self.class.name} Putting Canvas coming_up event with due_date #{formatted_entry["due_date"]} in #{bucket} bucket: #{formatted_entry}"
           tasks.push(formatted_entry)
         end
       end
