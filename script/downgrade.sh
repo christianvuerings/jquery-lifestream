f39daeb3133d58ba76d35974c679f79d5cdf0a6c
Chris Tweney <chris@media.berkeley.edu>
2013-02-11 15:00:48 -0800

    CLC-1220 Script to automate db downgrade procedure

---
+++ script/downgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script to downgrade the database to the last released version
+
+# on each release, update last_version to the latest db migration in db/migrate
+LAST_VERSION="2013012410480000"
+
+cd $( dirname "${BASH_SOURCE[0]}" )/..
+
+LOG=`date +"log/start-stop_%Y-%m-%d.log"`
+LOGIT="tee -a $LOG"
+
+echo | $LOGIT
+echo "------------------------------------------" | $LOGIT
+echo "`date`: Downgrading CalCentral to $LAST_VERSION on app node: `hostname -s`" | $LOGIT
+
+cd script
+./migrate.sh $LAST_VERSION
