95d04200ce5036d9ed8af3697f5a78deb6e078c7
Chris Tweney <chris@media.berkeley.edu>
2013-02-19 16:28:55 -0800

    CLC-1210 Add caching at CalLinkProxy level

--- lib/proxies/cal_link_proxy.rb
+++ lib/proxies/cal_link_proxy.rb
@@ -8,31 +8,42 @@ class CalLinkProxy < BaseProxy
     super(Settings.cal_link_proxy, options)
   end
 
-  def do_get(uid)
-    url = "#{Settings.cal_link_proxy.base_url}/api/memberships"
-    params = build_params uid
-    Rails.logger.info "#{self.class.name}: Fake = #@fake; Making request to #{url} on behalf of user #{uid}; params = #{params}"
-    begin
-      response = FakeableProxy.wrap_request(APP_ID, @fake, { :match_requests_on => [:method, :path, :body] }) {
-        Faraday::Connection.new(
-            :url => url,
-            :params => params
-        ).get
-      }
-      if response.status >= 400
-        Rails.logger.warn "#{self.class.name}: Connection failed: #{response.status} #{response.body}"
-        return nil
+  def self.access_granted?
+    settings = Settings.cal_link_proxy
+    settings.fake || (settings.public_key && settings.private_key)
+  end
+
+  def get_memberships(uid)
+    Rails.cache.fetch(
+        self.class.cache_key(uid),
+        :expires_in => Settings.cache.api_expires_in,
+        :race_condition_ttl => 2.seconds
+    ) do
+      url = "#{Settings.cal_link_proxy.base_url}/api/memberships"
+      params = build_params uid
+      Rails.logger.info "#{self.class.name}: Fake = #@fake; Making request to #{url} on behalf of user #{uid}; params = #{params}"
+      begin
+        response = FakeableProxy.wrap_request(APP_ID, @fake, {:match_requests_on => [:method, :path, :body]}) {
+          Faraday::Connection.new(
+              :url => url,
+              :params => params
+          ).get
+        }
+        if response.status >= 400
+          Rails.logger.warn "#{self.class.name}: Connection failed: #{response.status} #{response.body}"
+          return nil
+        end
+        Rails.logger.debug "#{self.class.name}: Remote server status #{response.status}, Body = #{response.body}"
+        {
+            :body => JSON.parse(response.body),
+            :status_code => response.status
+        }
+      rescue Faraday::Error::ConnectionFailed, Faraday::Error::TimeoutError
+        {
+            :body => "Remote server unreachable",
+            :status_code => 503
+        }
       end
-      Rails.logger.debug "#{self.class.name}: Remote server status #{response.status}, Body = #{response.body}"
-      {
-          :body => JSON.parse(response.body),
-          :status_code => response.status
-      }
-    rescue Faraday::Error::ConnectionFailed, Faraday::Error::TimeoutError
-      {
-          :body => "Remote server unreachable",
-          :status_code => 503
-      }
     end
   end
 
--- spec/lib/cal_link_proxy_spec.rb
+++ spec/lib/cal_link_proxy_spec.rb
@@ -7,7 +7,7 @@ describe CalLinkProxy do
   end
 
   it "should get the membership feed from CalLink" do
-    data = @client.do_get "300846"
+    data = @client.get_memberships "300846"
     data[:status_code].should_not be_nil
     if data[:status_code] == 200
       data[:body]["items"].should_not be_nil
