124434e9c263ee3a892cf0236bf50d94f2670840
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-02-19 16:33:00 -0800

    [CLC-1244] Pushing seed users into migration code

--- db/migrate/2013021122250300_create_user_auths.rb
+++ db/migrate/2013021122250300_create_user_auths.rb
@@ -10,6 +10,13 @@ class CreateUserAuths < ActiveRecord::Migration
     change_table :user_auths do |t|
       t.index [:uid], :unique => true
     end
+
+    # Seeding super users, test users.
+    super_users = %w(192517 323487 191779 238382 208861 675750 3222279 2040 904715 211159 978966)
+    super_users.each do |uid|
+      Rails.logger.info "Flagging #{uid} as a superuser"
+      UserAuth.new_or_update_superuser! uid
+    end
   end
 
   def down
--- db/migrate/2013021320072300_change_user_data_schema.rb
+++ db/migrate/2013021320072300_change_user_data_schema.rb
@@ -4,6 +4,27 @@ class ChangeUserDataSchema < ActiveRecord::Migration
       t.column :is_test_user, :boolean, :null => false, :default => false
     end
 
+    # Flagging test users.
+    test_user_ids = %w(11002820 61889)
+    test_user_ids += %w(321765 321703 324731 212388 212387)
+    #Injecting the universal test ids
+    add_range = lambda {|id_lower, id_upper|
+      test_user_ids += (id_lower..id_upper).to_a.map!{|arg| arg.to_s}
+    }
+    add_range.call(212372, 212378)
+    add_range.call(212379, 212381)
+    add_range.call(300846, 300945)
+    add_range.call(212382, 212386)
+    add_range.call(322587, 322590)
+    add_range.call(322583, 322586)
+    test_user_ids.uniq!
+
+    test_users = UserData.all(:conditions => ["uid IN (?)", test_user_ids] )
+    test_users.each do |entry|
+      Rails.logger.debug "Flagging #{entry.uid} as a test user"
+      entry.is_test_user = true
+      entry.save
+    end
   end
 
   def down
