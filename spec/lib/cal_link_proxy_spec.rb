9619824763dfec7112844e6dd72fd555951afa9b
Chris Tweney <chris@media.berkeley.edu>
2013-02-20 09:46:01 -0800

    CLC-1210 Add testext test for CalLink proxy

--- spec/lib/cal_link_proxy_spec.rb
+++ spec/lib/cal_link_proxy_spec.rb
@@ -2,16 +2,20 @@ require "spec_helper"
 
 describe CalLinkProxy do
 
-  before do
-    @client = CalLinkProxy.new
-  end
-
-  it "should get the membership feed from CalLink" do
-    data = @client.get_memberships "300846"
+  it "should get the fake membership feed from CalLink" do
+    client = CalLinkProxy.new({:fake => true})
+    data = client.get_memberships "300846"
     data[:status_code].should_not be_nil
     if data[:status_code] == 200
       data[:body]["items"].should_not be_nil
     end
+  end
+
+  it "should get the real membership feed from CalLink", :testext => true do
+    client = CalLinkProxy.new({:fake => true})
+    data = client.get_memberships "12345"
+    data[:status_code].should == 200
+    data[:body].should_not be_nil
   end
 
 end
