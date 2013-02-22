5f95024d2d6adc6f75dd7f3bc2f353e46eaa8fec
Scot Hacker <shacker@birdhouse.org>
2013-02-22 12:36:33 -0800

    CLC-1348 Implement CalCentral Settings Page Revisions https://jira.media.berkeley.edu/jira/browse/CLC-1348

--- app/assets/javascripts/angular/controllers/settingsController.js
+++ app/assets/javascripts/angular/controllers/settingsController.js
@@ -4,10 +4,34 @@
   /**
    * Settings controller
    */
-  calcentral.controller('SettingsController', ['apiService', function(apiService) {
+  calcentral.controller('SettingsController', ['$http', '$scope', 'apiService', function($http, $scope, apiService) {
 
     apiService.util.setTitle('Settings');
 
+    $scope.refreshServices = function() {
+
+      $scope.connected_services = [];
+      $scope.non_connected_services = [];
+
+      $http.get('/api/my/status').success(function(data) {
+        // Put connected/non-connected services into buckets we can iterate through and count.
+        // TODO: Format these into buckets in the API instead - CLC-1354
+        if (data.has_canvas_access_token) {
+          $scope.connected_services.push('canvas');
+        }
+        if (data.has_google_access_token) {
+          $scope.connected_services.push('google');
+        }
+
+        if (!data.has_canvas_access_token) {
+          $scope.non_connected_services.push('canvas');
+        }
+        if (!data.has_google_access_token) {
+          $scope.non_connected_services.push('google');
+        }
+      });
+    };
+    $scope.refreshServices();
   }]);
 
 })(window.calcentral);
