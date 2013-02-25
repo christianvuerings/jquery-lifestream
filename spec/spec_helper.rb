2370b1704b78b1cf9f04e72f3b90493dc5b94cf9
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-02-25 13:17:34 -0800

    [NOJIRA] Moving spec_helper_module inside spec/support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,7 @@ require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/rails'
 require 'webmock/rspec'
-require 'spec_helper_module'
+require 'support/spec_helper_module'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
