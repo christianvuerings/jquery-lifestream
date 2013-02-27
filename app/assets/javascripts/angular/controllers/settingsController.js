5fcf677565dbc410f3905a908bb07350016f5441
Scot Hacker <shacker@birdhouse.org>
2013-02-26 16:07:22 -0800

    nojira: Streamline bucketizing of connected services in angular settings controller

--- app/assets/javascripts/angular/controllers/settingsController.js
+++ app/assets/javascripts/angular/controllers/settingsController.js
@@ -15,20 +15,19 @@
 
       $http.get('/api/my/status').success(function(data) {
         // Put connected/non-connected services into buckets we can iterate through and count.
-        // TODO: Format these into buckets in the API instead - CLC-1354
+
         if (data.has_canvas_access_token) {
           $scope.connected_services.push('canvas');
-        }
-        if (data.has_google_access_token) {
-          $scope.connected_services.push('google');
-        }
-
-        if (!data.has_canvas_access_token) {
+        } else {
           $scope.non_connected_services.push('canvas');
         }
-        if (!data.has_google_access_token) {
+
+        if (data.has_google_access_token) {
+          $scope.connected_services.push('google');
+        } else {
           $scope.non_connected_services.push('google');
         }
+
       });
     };
     $scope.refreshServices();
