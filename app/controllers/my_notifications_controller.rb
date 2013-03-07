32832d496ee1809cfca9323d7897cf726a3d519a
Chris Tweney <chris@media.berkeley.edu>
2013-03-07 15:10:39 -0800

    CLC-1387 Undo incorrect caching logic

--- app/controllers/my_notifications_controller.rb
+++ app/controllers/my_notifications_controller.rb
@@ -1,23 +1,16 @@
-require 'lib/cacheable.rb'
-
 class MyNotificationsController < ApplicationController
 
-  extend Calcentral::Cacheable
-
-  def self.translators
+ def self.translators
     @translators ||= {}
   end
 
   def get_feed
-
     result = {'notifications' => []}
 
     if session[:user_id]
-      self.class.fetch_from_cache session[:user_id] do
-        Notification.where(:uid => session[:user_id]).each do |notification|
-          translator = (MyNotificationsController.translators[notification.translator] ||= notification.translator.constantize.new)
-          result['notifications'].push translator.translate notification
-        end
+      Notification.where(:uid => session[:user_id]).each do |notification|
+        translator = (MyNotificationsController.translators[notification.translator] ||= notification.translator.constantize.new)
+        result['notifications'].push translator.translate notification
       end
     end
 
