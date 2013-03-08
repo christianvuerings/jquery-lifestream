cecb20f60598cdae49fd7777450c1d24837dd0e8
Chris Tweney <chris@media.berkeley.edu>
2013-03-08 14:27:25 -0800

    CLC-1373 Crude trial of talking to Bearfacts API in code

---
+++ lib/proxies/bearfacts_proxy.rb
@@ -0,0 +1,44 @@
+class BearfactsProxy < BaseProxy
+
+  APP_ID = "Bearfacts"
+
+  def initialize(options = {})
+    super(Settings.bearfacts_proxy, options)
+  end
+
+  def get_profile
+    self.class.fetch_from_cache @uid do
+      url = "#{Settings.bearfacts_proxy.base_url}/student/#{@uid}"
+      Rails.logger.info "#{self.class.name}: Fake = #@fake; Making request to #{url} on behalf of user #{@uid}; cache expiration #{self.class.expires_in}"
+      begin
+        response = FakeableProxy.wrap_request(APP_ID + "_profile", @fake) {
+          Faraday::Connection.new(
+              :url => url,
+              :params => {
+                  :token => Settings.bearfacts_proxy.token
+              },
+              :ssl => {:verify => false}
+          ).get
+        }
+        if response.status >= 400
+          Rails.logger.warn "#{self.class.name}: Connection failed: #{response.status} #{response.body}"
+          return nil
+        end
+
+        doc = Nokogiri::XML response.body
+
+        Rails.logger.debug "#{self.class.name}: Remote server status #{response.status}, Body = #{doc.to_xml(:indent=>2)}"
+        {
+            :body => doc,
+            :status_code => response.status
+        }
+      rescue Faraday::Error::ConnectionFailed, Faraday::Error::TimeoutError => e
+        Rails.logger.warn "#{self.class.name}: Connection failed: #{e.class} #{e.message}"
+        {
+            :body => "Remote server unreachable",
+            :status_code => 503
+        }
+      end
+    end
+  end
+end
