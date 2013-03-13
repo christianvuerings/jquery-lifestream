7137a5d3a6ce458aaaf8672b4e5dda93e8cc5ce3
Christian Vuerings <vueringschristian@gmail.com>
2013-03-12 22:47:40 -0700

    v0.3.7 Disqus support

--- jquery.lifestream.js
+++ jquery.lifestream.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery Lifestream Plug-in
- * @version 0.3.6
+ * @version 0.3.7
  * Show a stream of your online activity
  *
  * Copyright 2011, Christian Vuerings - http://denbuzze.com
@@ -537,6 +537,68 @@ $.fn.lifestream.feeds.deviantart = function( config, callback ) {
 
 };
 })(jQuery);(function($) {
+$.fn.lifestream.feeds.disqus = function( config, callback ) {
+
+  var template = $.extend({},
+    {
+      post: 'commented on <a href="${url}">${thread.title}</a>',      
+      thread_like: 'liked <a href="${url}">${thread.title}</a>'
+    },
+    config.template),
+
+  parseDisqus = function( input ) {
+    var output = [], i = 0, j, item;
+
+    if(input) {
+      j = input.length;
+      for( ; i<j; i++) {
+        item = input[i];
+        
+        // replies to your comments are included by default
+        if (item.type !== 'reply') {
+          output.push({
+            date: new Date( item.createdAt ),
+            config: config,
+            html: $.tmpl( template[item.type], item.object )
+          });
+        }       
+      }
+    }
+
+    return output;
+  };
+
+  $.ajax({
+    url: "https://disqus.com/api/3.0/users/listActivity.json",
+      data: {
+      	user: config.user,
+        api_key: config.key
+      },
+    dataType: 'jsonp',
+    success: function( data ) {
+       if (data.code === 2) {
+        callback([]);
+        
+        // log error to console if not on IE
+        if (console && console.error) {
+          console.error('Error loading Disqus stream.', data.response);
+        }
+        return;
+      } else {
+        callback(parseDisqus(data.response));
+      }
+    }
+  });
+
+  // Expose the template.
+  // We use this to check which templates are available
+  return {
+    "template" : template
+  };
+
+};
+})(jQuery);
+(function($) {
 $.fn.lifestream.feeds.dribbble = function( config, callback ) {
 
     var template = $.extend({},
