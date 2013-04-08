e0cea54ba9a07f7331538576134c6bc964b5939c
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-04-08 14:20:02 -0700

    [NOJIRA] DRYing up controller logic

--- app/controllers/my_tasks_controller.rb
+++ app/controllers/my_tasks_controller.rb
@@ -6,41 +6,21 @@ class MyTasksController < ApplicationController
     render :json => MyTasks::Merged.new(session[:user_id], :original_user_id => session[:original_user_id]).get_feed.to_json
   end
 
-  def update_task
-    begin
-      my_tasks_model = MyTasks::Merged.new(session[:user_id], :original_user_id => session[:original_user_id])
-      render :json => my_tasks_model.update_task(request.request_parameters).to_json
-    rescue ArgumentError => e
-      return render :json => {error: "Invalid Arguments", message: e.message}.to_json, :status => 400
+  def self.define_passthrough(endpoint)
+    define_method endpoint do
+      begin
+        my_tasks_model = MyTasks::Merged.new(session[:user_id], :original_user_id => session[:original_user_id])
+        render :json => my_tasks_model.send(endpoint, request.request_parameters).to_json
+      rescue ArgumentError => e
+        return render :json => {error: "Invalid Arguments", message: e.message}.to_json, :status => 400
+      end
     end
   end
 
-  def insert_task
-    begin
-      my_tasks_model = MyTasks::Merged.new(session[:user_id], :original_user_id => session[:original_user_id])
-      render :json => my_tasks_model.insert_task(request.request_parameters).to_json
-    rescue ArgumentError => e
-      return render :json => {error: "Invalid Arguments", message: e.message}.to_json, :status => 400
-    end
-  end
-
-  def clear_completed_tasks
-    begin
-      my_tasks_model = MyTasks::Merged.new(session[:user_id], :original_user_id => session[:original_user_id])
-      render :json => my_tasks_model.clear_completed_tasks(request.request_parameters).to_json
-    rescue ArgumentError => e
-      return render :json => {error: "Invalid Arguments", message: e.message}.to_json, :status => 400
-    end
-  end
-
-  def delete_task
-    begin
-      my_tasks_model = MyTasks::Merged.new(session[:user_id], :original_user_id => session[:original_user_id])
-      render :json => my_tasks_model.delete_task(request.request_parameters).to_json
-    rescue ArgumentError => e
-      return render :json => {error: "Invalid Arguments", message: e.message}.to_json, :status => 400
-    end
-  end
+  define_passthrough :update_task
+  define_passthrough :insert_task
+  define_passthrough :clear_completed_tasks
+  define_passthrough :delete_task
 
   private
   def check_authentication
