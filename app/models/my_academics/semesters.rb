6f49c43e4087a157898a464ccd08a33812bafa7b
Chris Tweney <chris@media.berkeley.edu>
2013-04-18 08:53:00 -0700

    CLC-1648 Fill out remaining class details

--- app/models/my_academics/semesters.rb
+++ app/models/my_academics/semesters.rb
@@ -25,11 +25,21 @@ class MyAcademics::Semesters
       units = to_text(class_schedule.css("numberOfUnits"))
       title = to_text(class_schedule.css("courseTitle"))
       grade_option = to_text(class_schedule.css("pnpFlag")).upcase == "Y" ? "P/NP" : "Letter"
+      ccn = to_text(class_schedule.css("courseControlNumber"))
+      format = to_text(class_schedule.css("instructnFormatDet"))
+      section = "#{format} #{to_text(class_schedule.css("sectNum"))}"
+      schedule_string = "#{to_text(class_schedule.css("weekGroup"))} #{to_text(class_schedule.css("startTime"))}#{to_text(class_schedule.css("startTimeAmPmFlag"))}-#{to_text(class_schedule.css("endTime"))}#{to_text(class_schedule.css("endTimeAmPmFlag"))}"
+      instructor = to_text(class_schedule.css("instrShortName"))
       schedule << {
         :course_number => course_number,
+        :ccn => ccn,
         :title => title,
         :units => units,
-        :grade_option => grade_option
+        :grade_option => grade_option,
+        :section => section,
+        :format => format,
+        :schedule => schedule_string,
+        :instructor => instructor
       }
     end
 
