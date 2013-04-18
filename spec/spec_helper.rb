d56f92d7466934b2bde7178d64556621e3788bc5
Christian Vuerings <vueringschristian@gmail.com>
2013-04-17 17:49:56 -0700

    CLC-1679 - Have a way to disable simple_cov https://jira.media.berkeley.edu/jira/browse/CLC-1679

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
-require 'simplecov'
-SimpleCov.add_filter 'app/views'
-SimpleCov.start 'rails'
+ENV["SIMPLE_COV_ENABLED"] ||= "true"
+if ENV["SIMPLE_COV_ENABLED"] == "true"
+  require 'simplecov'
+  SimpleCov.add_filter 'app/views'
+  SimpleCov.start 'rails'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
