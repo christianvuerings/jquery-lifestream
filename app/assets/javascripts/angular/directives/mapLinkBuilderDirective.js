311e1e5eaf69df92683bd64247860c41b65807c9
Scot Hacker <shacker@birdhouse.org>
2013-05-13 12:12:04 -0700

    CLC-1847 Don't link exam schedules to map if building coords are missing https://jira.media.berkeley.edu/jira/browse/CLC-1847

--- app/assets/javascripts/angular/directives/mapLinkBuilderDirective.js
+++ app/assets/javascripts/angular/directives/mapLinkBuilderDirective.js
@@ -1,11 +1,11 @@
 (function(angular) {
   'use strict';
 
-  angular.module('calcentral.directives').directive('ccMapLinkBuilderDirective', function() {
+  angular.module('calcentral.directives').directive('ccMapLinkBuilderDirective', ['$compile', function($compile) {
 
     return {
         restrict: 'A',
-        link: function(scope) {
+        link: function(scope, elm) {
 
           // building_link_name is used in the location name handed to Google Maps.
           // Need to pre-process in case it includes special chars. We are guaranteed
@@ -24,9 +24,17 @@
             building_tooltip_name += ' ' + scope.exam.location.raw_location;
           }
           scope.exam.location.building_tooltip_name = building_tooltip_name;
-        },
-        template: '<a href="https://maps.google.com/maps?q={{exam.location.lat}},{{exam.location.lon}}+({{exam.location.building_link_name}})" title="{{exam.location.building_tooltip_name}}">{{exam.location.raw_location}}</a>'
+
+          // Link to map only if we have both lat and lon; otherwise just display building name
+          var element = '';
+          if (scope.exam.location.lat && scope.exam.location.lon) {
+            element = $compile('<a href="https://maps.google.com/maps?q={{exam.location.lat}},{{exam.location.lon}}+({{exam.location.building_link_name}})" title="{{exam.location.building_tooltip_name}}">{{exam.location.raw_location}} <i class="cc-icon icon-map-marker"></i></a>')(scope);
+          } else {
+            element = $compile('<span>{{exam.location.raw_location}}</span>')(scope);
+          }
+          elm.append(element);
+        }
     };
-  });
+  }]);
 
 })(window.angular);
