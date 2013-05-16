80823783b47233986127df291af5ec3936ac50b6
Chris Tweney <chris@media.berkeley.edu>
2013-05-16 07:47:15 -0700

    CLC-1753 Add action caching to index and blog feed

--- app/controllers/bootstrap_controller.rb
+++ app/controllers/bootstrap_controller.rb
@@ -1,20 +1,27 @@
 class BootstrapController < ApplicationController
   include ActiveRecordHelper
+  before_filter :check_databases_alive
+  caches_action :index
   respond_to :html
 
   def index
     respond_to do |t|
       t.html do
-        # so that an error gets thrown if postgres is dead.
-        if !UserData.database_alive?
-          raise "CalCentral database is currently unavailable"
-        end
-        # so an error gets thrown if Oracle is dead.
-        if !CampusData.database_alive?
-          raise "Campus database is currently unavailable"
-        end
         @server_settings = ServerRuntime.get_settings
       end
+    end
+  end
+
+  private
+
+  def check_databases_alive
+    # so that an error gets thrown if postgres is dead.
+    if !UserData.database_alive?
+      raise "CalCentral database is currently unavailable"
+    end
+    # so an error gets thrown if Oracle is dead.
+    if !CampusData.database_alive?
+      raise "Campus database is currently unavailable"
     end
   end
 
