2e33533e3b8fea14db16dd8f1caadfcee6d5e713
Eli Cochran <eli@media.berkeley.edu>
2013-05-17 13:56:02 -0700

    CLC-1881, added target='_blank' to the project site link in the footer

--- public/canvas/canvas-customization.js
+++ public/canvas/canvas-customization.js
@@ -5,7 +5,7 @@
     $('<li><a href="https://calcentral.berkeley.edu/dashboard">CalCentral Dashboard</a></li>').appendTo($calcentral_header);
     $('#topbar').prepend($calcentral_header);
     $('#footer span').wrap('<div />');
-    var $calcentral_footer = $('<p><span>Canvas Pilot,</span> part of the <a href="http://ets.berkeley.edu/bspace-replacement">bSpace Replacement project</a></p>');
+    var $calcentral_footer = $('<p><span>Canvas Pilot,</span> part of the <a href="http://ets.berkeley.edu/bspace-replacement" target="_blank">bSpace Replacement project</a></p>');
     $('#footer div').prepend($calcentral_footer);
   });
 })(window.document, window.$);
