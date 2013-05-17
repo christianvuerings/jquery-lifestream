7963987c510f37169de7038a6da62bf12f64ab8d
Scot Hacker <shacker@birdhouse.org>
2013-05-17 16:07:07 -0700

    CLC-1451 Wire up My Classes widget with campus class data https://jira.media.berkeley.edu/jira/browse/CLC-1451
    - The my/classes feed also now includes CCN, for use in deep-linking back into our own class pages, which expect CCN in the URL.

--- app/controllers/my_classes_controller.rb
+++ app/controllers/my_classes_controller.rb
@@ -2,7 +2,7 @@ class MyClassesController < ApplicationController
 
   def get_feed
     if session[:user_id]
-      render :json => MyClasses.new(session[:user_id]).get_feed.to_json
+      render :json => MyClasses.new(session[:user_id], :original_user_id => session[:original_user_id]).get_feed.to_json
     else
       render :json => {}.to_json
     end
