16aeb179f9584a5415aa6e671654fce686450435
Armin Roșu <armin.rosu@gmail.com>
2013-05-18 12:12:47 +0300

    fewer OCD triggers in core (i.e. beautify)

--- src/core.js
+++ src/core.js
@@ -87,10 +87,10 @@
           if ( item.html ) {
             $('<li class="'+ settings.classname + '-' +
                item.config.service + '">').data( "name", item.config.service )
-                                           .data( "url", item.url || "#" )
-                                           .data( "time", item.date )
-                                           .append( item.html )
-                                           .appendTo( ul );
+                                          .data( "url", item.url || "#" )
+                                          .data( "time", item.date )
+                                          .append( item.html )
+                                          .appendTo( ul );
           }
         }
 
@@ -144,7 +144,7 @@
       // At then end we call the load method.
       if( !jQuery.tmpl ) {
         jQuery.getScript(
-          "http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.js",
+          'http://ajax.aspnetcdn.com/ajax/jquery.templates/beta1/jquery.tmpl.js',
           load);
       } else {
         load();
@@ -160,8 +160,8 @@
    * @return {String} A valid YQL URL
    */
   $.fn.lifestream.createYqlUrl = function( query ) {
-      return ( "http://query.yahooapis.com/v1/public/yql?q=__QUERY__&env=" +
-      "store://datatables.org/alltableswithkeys&format=json")
+      return ( 'http://query.yahooapis.com/v1/public/yql?q=__QUERY__&env=' +
+      'store://datatables.org/alltableswithkeys&format=json')
         .replace( "__QUERY__" , encodeURIComponent( query ) );
   };
 
@@ -176,7 +176,7 @@
    */
   if(!Object.keys) {
     Object.keys = function(o){
-      if (o !== Object(o)){
+      if (o !== Object(o)) {
         throw new TypeError('Object.keys called on non-object');
       }
       var ret=[],p;
