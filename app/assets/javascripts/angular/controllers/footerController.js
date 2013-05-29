82499181d5110199f34f8d8318db176d3dd0ae4a
Christian Vuerings <vueringschristian@gmail.com>
2013-05-29 15:03:04 -0700

    CLC-1853 - Load footer information through API request https://jira.media.berkeley.edu/jira/browse/CLC-1853

--- app/assets/javascripts/angular/controllers/footerController.js
+++ app/assets/javascripts/angular/controllers/footerController.js
@@ -4,6 +4,24 @@
   /**
    * Footer controller
    */
-  calcentral.controller('FooterController', angular.noop);
+  calcentral.controller('FooterController', ['$http', '$scope', function($http, $scope) {
+
+    $scope.footer = {
+      showInfo: false
+    };
+
+    var loadServerInformation = function() {
+      $http.get('/api/server_info').success(function(data) {
+        angular.extend($scope, data);
+      });
+    };
+
+    $scope.$watch('footer.showInfo', function(showInfo) {
+      if (showInfo && !$scope.versions) {
+        loadServerInformation();
+      }
+    });
+
+  }]);
 
 })(window.calcentral);
