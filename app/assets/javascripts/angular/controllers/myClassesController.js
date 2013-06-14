382bf8e393b80625320c6b3a7d85a4e9f6968817
Christian Vuerings <vueringschristian@gmail.com>
2013-06-13 22:37:28 -0700

    CLC-1971 - Front-end - Show all classes https://jira.media.berkeley.edu/jira/browse/CLC-1971

--- app/assets/javascripts/angular/controllers/myClassesController.js
+++ app/assets/javascripts/angular/controllers/myClassesController.js
@@ -6,9 +6,45 @@
    */
   calcentral.controller('MyClassesController', ['$http', '$scope', function($http, $scope) {
 
+    var campusClasses = {};
+    var otherClasses = [];
+
+    var splitClasses = function(classes) {
+      for (var i = 0; i < classes.length; i++) {
+        if (classes[i].emitter === 'Campus') {
+          campusClasses[classes[i].id] = classes[i];
+        } else {
+          otherClasses.push(classes[i]);
+        }
+      }
+    };
+
+    var addSubclasses = function() {
+      for (var j = 0; j < otherClasses.length; j++) {
+        for (var k = 0; k < otherClasses[j].courses.length; k++) {
+          var course = otherClasses[j].courses[k];
+          if (!campusClasses[course.id].subclasses) {
+            campusClasses[course.id].subclasses = [];
+          }
+          campusClasses[course.id].subclasses.push(otherClasses[j]);
+        }
+      }
+    };
+
+    var parseClasses = function(classes) {
+      splitClasses(classes);
+      addSubclasses();
+      $scope.allClassesLength = Object.keys(campusClasses).length;
+      $scope.allClasses = campusClasses;
+    };
+
     var getMyClasses = function() {
-      $http.get('/api/my/classes').success(function(data) {
+      //$http.get('/api/my/classes').success(function(data) {
+      $http.get('/dummy/json/classes.json').success(function(data) {
         angular.extend($scope, data);
+        if (data.classes) {
+          parseClasses(data.classes);
+        }
       });
     };
 
--- app/assets/javascripts/angular/services/widgetService.js
+++ app/assets/javascripts/angular/services/widgetService.js
@@ -18,6 +18,16 @@
           items[i]._show = false;
         }
       }
+
+      // Also make it work for objects
+      if (angular.isObject(items)) {
+        for (var j in items) {
+          if (items.hasOwnProperty(j) && items[j].$$hashKey !== item.$$hashKey) {
+            items[j]._show = false;
+          }
+        }
+      }
+
       analyticsService.trackEvent(['Detailed view', item._show ? 'Open' : 'Close', widget]);
     };
 
