9488bb6daae1d1de49fe7c90a37286a411124dd1
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-06-28 14:42:26 -0700

    [CLC-1631] Dropping old Canvas and Google tokens.

---
+++ db/migrate/2013062807582200_drop_old_tokens.rb
@@ -0,0 +1,13 @@
+class DropOldTokens < ActiveRecord::Migration
+  def up
+    target_tokens = %w(Google-2013040911200984 Google-2013022123412000
+      Google-2013022123181900 Canvas-20130423 Google-2013040912314334)
+    target_tokens.each do |token_id|
+      say "Removing app_id: #{token_id} tokens"
+      Oauth2Data.where(:app_id => token_id).delete_all
+    end
+  end
+
+  def down
+  end
+end
