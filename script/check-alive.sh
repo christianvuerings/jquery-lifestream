fe0eb081d59670d3dee2bfef1a31f269901ab180
Chris Tweney <chris@media.berkeley.edu>
2013-07-01 14:42:06 -0700

    CLC-2055 Small tweak to deployment scripts

--- script/check-alive.sh
+++ script/check-alive.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Script to check whether Calcentral is alive.
 
+IP_ADDR=`/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+
 cd /tmp
-rm -rf localhost\:3000
-wget --recursive --quiet http://localhost:3000/
-cd localhost\:3000
+rm -rf $IP_ADDR\:3000
+wget --recursive --quiet http://$IP_ADDR:3000/
+cd $IP_ADDR\:3000
 
 grep "Git commit\:" index.html > /dev/null || { echo "WARNING: Calcentral is dead or seriously malfunctioning!" ; exit 1 ; }
 
