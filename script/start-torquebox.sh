6683614ecec860666695112f9be87a021a9ccec0
Chris Tweney <chris@media.berkeley.edu>
2013-07-03 13:40:43 -0700

    CLC-2055 Give torquebox more threads and db connections

--- script/start-torquebox.sh
+++ script/start-torquebox.sh
@@ -27,6 +27,7 @@ OPTS=${CALCENTRAL_JRUBY_OPTS:="-Xcext.enabled=true"}
 export JRUBY_OPTS=$OPTS
 JVM_OPTS=${CALCENTRAL_JVM_OPTS:="\-server \-verbose:gc \-XX:+PrintGCTimeStamps \-XX:+PrintGCDetails \-XX:+UseParallelOldGC \-Xms1200m \-Xmx1200m \-Xmn500m \-XX:PermSize=256m \-XX:MaxPermSize=256m"}
 LOG_DIR=${CALCENTRAL_LOG_DIR:=`pwd`"/log"}
+MAX_THREADS=${CALCENTRAL_MAX_THREADS:="90"}
 export CALCENTRAL_LOG_DIR=$LOG_DIR
 IP_ADDR=`/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
 
@@ -35,7 +36,7 @@ cd deploy
 JBOSS_HOME=`bundle exec torquebox env jboss_home`
 cp ~/.calcentral_config/standalone-ha.xml $JBOSS_HOME/standalone/configuration/
 
-nohup bundle exec torquebox run -b $IP_ADDR -p=3000 --jvm-options="$JVM_OPTS" --clustered --max-threads=25 < /dev/null > $TORQUEBOX_LOG 2>> $LOG  &
+nohup bundle exec torquebox run -b $IP_ADDR -p=3000 --jvm-options="$JVM_OPTS" --clustered --max-threads=$MAX_THREADS < /dev/null > $TORQUEBOX_LOG 2>> $LOG  &
 cd ..
 
 # wait a bit to let server start up
