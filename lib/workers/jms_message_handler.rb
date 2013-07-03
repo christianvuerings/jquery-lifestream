78d85ac5494c670aed66a0063199e18428c4cfd4
Chris Tweney <chris@media.berkeley.edu>
2013-07-03 13:33:42 -0700

    CLC-2145 Fix JMS logging; tweak JMS and HotPlate log levels so we get more info in production (WARN level)

--- lib/workers/jms_message_handler.rb
+++ lib/workers/jms_message_handler.rb
@@ -5,22 +5,22 @@ class JmsMessageHandler
   end
 
   def handle(message)
-    Rails.logger.info "#{self.class.name} #{Thread.current} is now reading #{message}"
+    Rails.logger.debug "#{self.class.name} #{Thread.current} is now reading #{message}"
     if message[:text]
       message_data = JSON.parse message[:text]
-      Rails.logger.info "#{self.class.name} message_data = #{message_data}"
+      Rails.logger.warn "#{self.class.name} message_data = #{message_data}"
       if message_data['eventNotification'] && message_data['eventNotification']['event']
         @processors.each do |processor|
           processor.process(message_data['eventNotification']["event"], message[:timestamp])
         end
       else
-        Rails.logger.info "#{self.class.name} #{Thread.current} JMS message has text but no eventNotification => event, skipping"
+        Rails.logger.warn "#{self.class.name} #{Thread.current} JMS message has text but no eventNotification => event, skipping"
       end
     else
-      Rails.logger.info "#{self.class.name} #{Thread.current} JMS message has no text, skipping"
+      Rails.logger.warn "#{self.class.name} #{Thread.current} JMS message has no text, skipping"
     end
   rescue JSON::ParserError => e
-    Rails.logger.warn "#{self.class.name} #{Thread.current} Skipping JMS message that has invalid JSON: #{e}"
+    Rails.logger.error "#{self.class.name} #{Thread.current} Skipping JMS message that has invalid JSON: #{e}"
   end
 
 end
