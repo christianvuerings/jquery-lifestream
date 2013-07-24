6e7b39eb7be382360e3bec2ee0e42002cec07238
Chris Tweney <chris@media.berkeley.edu>
2013-07-24 10:20:31 -0700

    CLC-2178 Followup - strengthen our dupe check against dupes of the same CCN in the same JMS message

---
+++ db/migrate/2013072410051400_de_duplicate_notifications2.rb
@@ -0,0 +1,25 @@
+class DeDuplicateNotifications2 < ActiveRecord::Migration
+
+  def up
+    dupes = {}
+    all_notifications = Notification.all
+    all_notifications.each do |n|
+      start_date = n.occurred_at
+      end_date = start_date.advance(:days => 1)
+      possible_dupes = Notification.where(:uid => n.uid, :translator => n.translator, :occurred_at => start_date...end_date)
+      possible_dupes.each do |poss|
+        if n.id != poss.id
+          unless dupes[n.id]
+            dupes[poss.id] = true
+          end
+        end
+      end
+    end
+    Rails.logger.warn "Deleting the following notifications which are duplicates: #{dupes}"
+    Notification.delete(dupes.keys)
+  end
+
+  def down
+
+  end
+end
