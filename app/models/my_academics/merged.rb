305a7de81e98578f03d5e88a90387fea7a8afdb3
Chris Tweney <chris@media.berkeley.edu>
2013-07-29 15:18:49 -0700

    CLC-2250 Fix expiration logic

--- app/models/my_academics/merged.rb
+++ app/models/my_academics/merged.rb
@@ -1,11 +1,7 @@
 class MyAcademics::Merged < MyMergedModel
 
   def self.expires_in
-    # Bearfacts data is refreshed daily at 0730, so we will always expire at 0800 sharp on the day after today.
-    # nb: memcached interprets expiration values greater than 30 days worth of seconds as a Unix timestamp. This
-    # logic may not work on caching systems other than memcached.
-    tomorrow = Time.zone.today.to_time_in_current_zone.to_datetime.advance(:days => 1, :hours => 8)
-    tomorrow.to_i
+    self.bearfacts_derived_expiration
   end
 
   def get_feed_internal
--- lib/proxies/bearfacts_proxy.rb
+++ lib/proxies/bearfacts_proxy.rb
@@ -7,11 +7,7 @@ class BearfactsProxy < BaseProxy
   end
 
   def self.expires_in
-    # Bearfacts data is refreshed daily at 0730, so we will always expire at 0800 sharp on the day after today.
-    # nb: memcached interprets expiration values greater than 30 days worth of seconds as a Unix timestamp. This
-    # logic may not work on caching systems other than memcached.
-    tomorrow = Time.zone.today.to_time_in_current_zone.to_datetime.advance(:days => 1, :hours => 8)
-    tomorrow.to_i
+    self.bearfacts_derived_expiration
   end
 
   def lookup_student_id
