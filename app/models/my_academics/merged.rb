586296f7d5369eafcdd9bea5c4ea37387f19ec3c
Chris Tweney <chris@media.berkeley.edu>
2013-07-29 16:22:40 -0700

    CLC-2250 Clarifying comment

--- app/models/my_academics/merged.rb
+++ app/models/my_academics/merged.rb
@@ -1,5 +1,7 @@
 class MyAcademics::Merged < MyMergedModel
 
+  # If MyAcademics ever includes data from data sources OTHER than our Bearfacts materialized views,
+  # which are updated only once a day, then remove this in favor of the normal expiration scheme.
   def self.expires_in
     self.bearfacts_derived_expiration
   end
