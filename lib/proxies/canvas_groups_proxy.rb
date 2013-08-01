85377ca3848b6f07c32c7104561aecb57ca960d3
Ray Davis <ray@media.berkeley.edu>
2013-08-01 08:34:42 -0700

    CLC-2252 Switch to stable login_id as Canvas user ID This CalCentral change must be in place before destabilizing the sis_user_id.

--- lib/proxies/canvas_groups_proxy.rb
+++ lib/proxies/canvas_groups_proxy.rb
@@ -1,7 +1,7 @@
 class CanvasGroupsProxy < CanvasProxy
 
   def groups
-    request("users/self/groups?as_user_id=sis_user_id:#{@uid}", "_groups")
+    request("users/self/groups?as_user_id=sis_login_id:#{@uid}", "_groups")
   end
 
 end
--- lib/proxies/canvas_user_profile_proxy.rb
+++ lib/proxies/canvas_user_profile_proxy.rb
@@ -1,7 +1,7 @@
 class CanvasUserProfileProxy < CanvasProxy
 
   def user_profile
-    request("users/sis_user_id:#{@uid}/profile", "_user_profile")
+    request("users/sis_login_id:#{@uid}/profile", "_user_profile")
   end
 
 end
