20338a48c287c92a5385bd0239e856f5bdf910a3
Chris Tweney <chris@media.berkeley.edu>
2013-08-12 15:26:32 -0700

    CLC-2321 Don't log Canvas profile 404s as errors

--- lib/proxies/canvas_user_profile_proxy.rb
+++ lib/proxies/canvas_user_profile_proxy.rb
@@ -4,4 +4,10 @@ class CanvasUserProfileProxy < CanvasProxy
     request("users/sis_login_id:#{@uid}/profile", "_user_profile")
   end
 
+  def log_error(fetch_options, response)
+    # 404 for this proxy just means the user doesn't have a Canvas profile, so don't bother logging.
+    unless response.status == 404
+      super
+    end
+  end
 end
