61cfc2ddb16a9f048700ecb07ad7c81b8f5224a8
Ray Davis <ray@media.berkeley.edu>
2013-08-19 15:51:06 -0700

    CLC-2364 Use SomeClass.expires_in rather than depending on explicit settings Especially since "nil" expiration turns into "never expire this" for memcached

--- app/controllers/blog_feed_controller.rb
+++ app/controllers/blog_feed_controller.rb
@@ -1,6 +1,6 @@
 class BlogFeedController < ApplicationController
 
-  caches_action(:get_latest_release_notes, :expires_in => Settings.cache.expiration.BlogFeed)
+  caches_action(:get_latest_release_notes, :expires_in => BlogFeed.expires_in)
 
   def get_latest_release_notes
     render :json => BlogFeed.new.get_latest_release_notes
