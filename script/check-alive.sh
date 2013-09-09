100ad580d055211471c9b63b35b59022756dff55
Chris Tweney <chris@media.berkeley.edu>
2013-09-09 14:28:08 -0700

    CLC-2464 Add a robust server-is-alive API endpoint

--- script/check-alive.sh
+++ script/check-alive.sh
@@ -6,9 +6,10 @@ IP_ADDR=`/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 
 cd /tmp
 rm -rf $IP_ADDR\:3000
 wget --recursive --quiet http://$IP_ADDR:3000/
+wget --recursive --quiet http://$IP_ADDR:3000/api/ping
 cd $IP_ADDR\:3000
 
-grep "Git commit\:" index.html > /dev/null || { echo "WARNING: Calcentral is dead or seriously malfunctioning!" ; exit 1 ; }
+grep "\"server_alive\"\:true" ping > /dev/null || { echo "WARNING: Calcentral is dead or seriously malfunctioning!" ; exit 1 ; }
 
 echo "Server is alive"
 
