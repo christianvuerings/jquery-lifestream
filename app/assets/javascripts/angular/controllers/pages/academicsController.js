0461b692d6cd93807ff9cc9b55771b3512750af5
Christian Vuerings <vueringschristian@gmail.com>
2013-09-10 14:46:58 -0700

    CLC-2484 - When loading my academics, it shows "this data not available" message when loading https://jira.media.berkeley.edu/jira/browse/CLC-2484

--- app/assets/javascripts/angular/controllers/pages/academicsController.js
+++ app/assets/javascripts/angular/controllers/pages/academicsController.js
@@ -202,9 +202,6 @@
 
       $scope.semesters = data.semesters;
 
-      // Some users (e.g. test-xxx users) may be missing the "student" role but still need ability to view My Academics pages
-      $scope.can_view_academics = $scope.api.user.profile.roles.student || $scope.api.user.profile.roles.faculty || $scope.college_and_level.standing == 'Undergraduate';
-
       $scope.is_undergratuate = ($scope.college_and_level.standing === 'Undergraduate');
 
       $scope.teaching = parseTeaching(data.teaching_semesters);
@@ -310,6 +307,9 @@
     // Wait until user profile is fully loaded before hitting academics data
     $scope.$on('calcentral.api.user.isAuthenticated', function(event, isAuthenticated) {
       if (isAuthenticated) {
+        // Some users (e.g. test-xxx users) may be missing the "student" role but still need ability to view My Academics pages
+        $scope.can_view_academics = $scope.api.user.profile.roles.student || $scope.api.user.profile.roles.faculty || $scope.college_and_level.standing === 'Undergraduate';
+
         $http.get('/api/my/academics').success(parseAcademics);
       }
     });
