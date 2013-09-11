d829403b4a2952365c2f705c3ea5cc10933f35c6
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-09-11 13:26:46 -0700

    [CLC-2122] Update test coverage

--- spec/models/my_activities_spec.rb
+++ spec/models/my_activities_spec.rb
@@ -25,20 +25,6 @@ describe "MyActivities" do
     end
   end
 
-  it "should get properly formatted registration blocks from fake Bearfacts" do
-    CanvasProxy.stub(:access_granted?).and_return(false)
-    SakaiProxy.stub(:access_granted?).and_return(false)
-    oski_bearfacts_proxy = BearfactsRegblocksProxy.new({:user_id => "61889", :fake => true})
-    BearfactsRegblocksProxy.stub(:new).and_return(oski_bearfacts_proxy)
-    oski_activities = MyActivities.new("61889").get_feed
-    oski_activities[:activities].empty?.should be_false
-    oski_activities[:activities].each do |act|
-      act[:emitter].should == "BearFacts"
-      act[:source].should_not == "Bearfacts"
-      @documented_types.include?(act[:type]).should be_true
-    end
-  end
-
   it "should successfuly handle well translated responses from notifications" do
     user = UserApi.new "300846"
     user.record_first_login
@@ -92,5 +78,58 @@ describe "MyActivities" do
       @documented_types.include?(act[:type]).should be_true
     end
   end
+
+
+  context "MyRegBlocks" do
+    it "should get properly formatted registration blocks from fake Bearfacts" do
+      CanvasProxy.stub(:access_granted?).and_return(false)
+      SakaiProxy.stub(:access_granted?).and_return(false)
+      oski_bearfacts_proxy = BearfactsRegblocksProxy.new({:user_id => "61889", :fake => true})
+      BearfactsRegblocksProxy.stub(:new).and_return(oski_bearfacts_proxy)
+      oski_activities = MyActivities.new("61889").get_feed
+      oski_activities[:activities].empty?.should be_false
+      oski_activities[:activities].each do |act|
+        act[:emitter].should == "BearFacts"
+        act[:source].should_not == "Bearfacts"
+        @documented_types.include?(act[:type]).should be_true
+      end
+    end
+
+    it "include in feed for recently cleared, very old blocks" do
+      CanvasProxy.stub(:access_granted?).and_return(false)
+      SakaiProxy.stub(:access_granted?).and_return(true)
+      SakaiUserSitesProxy.stub(:new).and_return(@fake_sakai_user_sites)
+      oski_bearfacts_proxy = BearfactsRegblocksProxy.new({:user_id => "61889", :fake => true})
+      BearfactsRegblocksProxy.stub(:new).and_return(oski_bearfacts_proxy)
+      oski_blocks = MyRegBlocks.new({user_id: "61889", fake: true})
+      mangled_inactive = oski_blocks.get_feed[:inactive_blocks].map do |block|
+        {
+          cleared_date: oski_blocks.format_date(Time.now.to_datetime),
+          blocked_date: oski_blocks.format_date(Time.at(0).to_datetime)
+        }.reverse_merge(block)
+      end
+      mangled_oski_blocks = oski_blocks.get_feed.merge({ inactive_blocks: mangled_inactive })
+      MyRegBlocks.any_instance.stub(:get_feed).and_return(mangled_oski_blocks)
+
+      oski_activities = MyActivities.new("61889").get_feed
+      oski_activities[:activities].empty?.should be_false
+      oski_activities[:activities].select do |act|
+        act[:emitter]== "BearFacts" && act[:type] == "message" && act[:title].include?("Block Cleared")
+      end
+
+    end
+
+    context "4xx response from bearfacts proxy" do
+      before(:each) { MyRegBlocks.any_instance.stub(:get_feed).and_return({ available: false }) }
+
+      it "should not malform the activities passed into append_reg_blocks" do
+        my_activities = MyActivities.new(@user_id)
+        activities = "foo"
+        my_activities.send(:append_regblocks, activities)
+        activities.should eq("foo")
+      end
+    end
+  end
+
 
 end
