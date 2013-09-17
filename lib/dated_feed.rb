03de67ba5425d7d7beb4f1b7f04cdd1ea4034980
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-09-17 09:11:43 -0700

    [CLC-2530] Going back to the ActiveRecordHelper model

--- lib/dated_feed.rb
+++ lib/dated_feed.rb
@@ -1,26 +1,31 @@
 module DatedFeed
 
   def self.included(base)
-    base.extend(ClassAndInstanceMethods)
+    base.extend(ClassMethods)
   end
 
   def format_date(datetime, date_string_format="%-m/%d")
-    self.format_date(datetime, date_string_format)
+    DatedFeed.shared_format_date(datetime, date_string_format)
   end
 
-  module ClassAndInstanceMethods
+  module ClassMethods
     def format_date(datetime, date_string_format="%-m/%d")
-      {
-        :epoch => datetime.to_i,
-        :date_time => datetime.rfc3339,
-        :date_string => datetime.strftime(date_string_format)
-      }
-    rescue NoMethodError
-      {
-        :epoch => nil,
-        :date_time => nil,
-        :date_string => nil
-      }
+      DatedFeed.shared_format_date(datetime, date_string_format)
     end
+  end
+
+  protected
+  def self.shared_format_date(datetime, date_string_format="%-m/%d")
+    {
+      :epoch => datetime.to_i,
+      :date_time => datetime.rfc3339,
+      :date_string => datetime.strftime(date_string_format)
+    }
+  rescue NoMethodError
+    {
+      :epoch => nil,
+      :date_time => nil,
+      :date_string => nil
+    }
   end
 end
