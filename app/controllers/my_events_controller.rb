1df2b720483a0b32cb779b37a9e316a8e8870dac
Yu-Hung Lin <yuhung@yuhunglin.com>
2013-09-26 16:07:11 -0700

    [CLC-2506] Fixed implementation with tests

--- app/controllers/my_events_controller.rb
+++ app/controllers/my_events_controller.rb
@@ -10,7 +10,12 @@ class MyEventsController < ApplicationController
       response = GoogleEventsInsertProxy.new(user_id: session[:user_id]).insert_event(input.stringify_keys)
 
       result = response.data || {}
-      respond_with(response.data) do |format|
+      if result.blank?
+        logger.warn "GoogleEventsInsertProxy.insert_event response for user #{session[:user_id]} should not be blank.
+          Payload: #{input.inspect}"
+      end
+      result = result.to_hash.merge({ status:true }) unless result.blank?
+      return respond_with(result) do |format|
         format.json { render json: result.to_json, status: response.status}
       end
     end
@@ -22,6 +27,13 @@ class MyEventsController < ApplicationController
   def check_google_access
     return error_response unless GoogleProxy.access_granted?(session[:user_id])
     return error_response if (is_acting_as_nonfake_user? && !GoogleProxy.allow_pseudo_user?)
+  end
+
+  #TODO: de-duplicate by pushing MergedModel's method up into ApplicationController
+  def is_acting_as_nonfake_user?
+    session[:original_user_id] &&
+      session[:user_id] != session[:original_user_id] &&
+      !UserAuth.is_test_user?(session[:user_id])
   end
 
   def error_response
@@ -37,14 +49,14 @@ class MyEventsController < ApplicationController
     %w(start end).each do |key|
       if params[key] && params[key][:epoch]
         date_time = DateTime.strptime(params[key][:epoch].to_s, '%s') rescue ''
-        result[key.to_sym] = { dateTime: date_time.new_offset('-7:00').rfc3339(2) } if date_time.present?
+        result[key.to_sym] = { dateTime: date_time.new_offset('-7:00').rfc3339(3) } if date_time.present?
       end
     end
 
     valid = %w(summary start end).all? { |key| result[key.to_sym].present? }
     return {} unless valid
 
-    result.merge({calendarId: "primary"})
+    {calendarId: "primary"}.merge(result)
   end
 
 end
---
+++ spec/controllers/my_events_controller_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe MyEventsController do
+  let(:random_id) { rand(99999).to_s }
+
+  shared_examples "failure response" do
+    it { should_not be_success }
+    it { subject.status.should eq(400) }
+    it { subject.content_type.should eq("application/json") }
+    it { JSON.parse(subject.body)[:status].should be_false }
+  end
+
+  context "#create" do
+    context "failure scenarios" do
+      context "JSON request, unauthenticated" do
+        subject { post :create, { format: 'json'} }
+
+        it { should_not be_success }
+        it { subject.status.should eq(302) }
+        it { subject.content_type.symbol.should eq(:html) }
+        its(:location) { should be_include("login") }
+      end
+
+      context "request type HTML, authenticated" do
+        before(:each) { session[:user_id] = random_id }
+
+        subject { post :create, { format: 'html'} }
+
+        it { should_not be_success }
+        it { subject.status.should eq(406) }
+        it { subject.content_type.symbol.should eq(:html) }
+      end
+
+      context "request type JSON, authenticated, no google access" do
+        before(:each) { session[:user_id] = random_id }
+
+        subject { post :create, { format: 'json'} }
+        it_behaves_like "failure response"
+      end
+
+      context "request type JSON, authenticated, google access, bad payload" do
+        before(:each) do
+          session[:user_id] = random_id
+          described_class.any_instance.stub(:check_google_access).and_return(true)
+          described_class.any_instance.should_receive(:sanitize_input!).once
+        end
+
+        subject { post :create, { format: 'json', summary: "foo!" } }
+        it_behaves_like "failure response"
+      end
+
+      context "request type JSON, authenticated, google access, bad dates in payload" do
+        before(:each) do
+          session[:user_id] = random_id
+          described_class.any_instance.stub(:check_google_access).and_return(true)
+        end
+
+        subject do
+          post :create, {
+            format: 'json',
+            summary: "foo!",
+            start: { epoch: 'foo' },
+            end: { epoch: 'baz' }
+          }
+        end
+
+        it_behaves_like "failure response"
+      end
+
+    end
+
+    context "success scenarios" do
+      let!(:recorded_valid_input) do
+        {
+          summary: 'Fancy event',
+          start: { epoch: 1380013560 },
+          end: { epoch: 1380017160 }
+        }
+      end
+      before(:each) do
+        session[:user_id] = random_id
+        described_class.any_instance.stub(:check_google_access).and_return(true)
+        fake_proxy = GoogleEventsInsertProxy.new(fake: true, fake_options: { match_requests_on: [:method, :path, :body] })
+        GoogleEventsInsertProxy.stub(:new).and_return(fake_proxy)
+      end
+
+      subject do
+        post :create, {
+          format: 'json',
+        }.merge(recorded_valid_input)
+      end
+
+      it { should be_success }
+      it { subject.status.should eq(200) }
+      it { subject.content_type.should eq("application/json") }
+      it { JSON.parse(subject.body)["status"].should be_true }
+      it { JSON.parse(subject.body)["summary"].should eq("Fancy event") }
+      it { JSON.parse(subject.body)["id"].should be_present }
+    end
+  end
+
+end
