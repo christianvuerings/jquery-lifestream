9e2637d4dbfe77adf4b5b92134ccf8e24cf8edd9
Chris Tweney <chris@media.berkeley.edu>
2013-09-26 13:33:58 -0700

    CLC-2401 Remove unnecessary log

--- app/models/my_financials.rb
+++ app/models/my_financials.rb
@@ -1,10 +1,8 @@
 class MyFinancials < MyMergedModel
 
   def get_feed_internal
-    logger.info "uid = #{@uid}"
     proxy = FinancialsProxy.new({:user_id => @uid})
-    response = proxy.get
-    body = response.try(:[], :body)
+    body = proxy.get.try(:[], :body)
     body.try(:[], "student") || {}
   end
 
