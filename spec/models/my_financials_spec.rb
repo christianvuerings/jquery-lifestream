f87ae0d82d377bc83547f9ea4969a0a5abfd99d5
Chris Tweney <chris@media.berkeley.edu>
2013-09-26 12:05:42 -0700

    CLC-2401 Oops, make the fake proxy really fake

--- spec/models/my_financials_spec.rb
+++ spec/models/my_financials_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "MyFinancials" do
 
   let!(:oski_uid) { "61889" }
-  let!(:fake_financials_proxy) { FinancialsProxy.new({:user_id => oski_uid, :fake => false}) }
+  let!(:fake_financials_proxy) { FinancialsProxy.new({:user_id => oski_uid, :fake => true}) }
   before(:each) { FinancialsProxy.stub(:new).and_return(fake_financials_proxy) }
 
   shared_examples "blank feed" do
