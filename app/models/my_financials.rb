7e0edf6ee07993842df2ffae5daab4151611b65e
Chris Tweney <chris@media.berkeley.edu>
2013-10-07 15:41:12 -0700

    CLC-2626 Enforce financials feature flag at model and proxy layers

--- app/models/my_financials.rb
+++ app/models/my_financials.rb
@@ -1,9 +1,13 @@
 class MyFinancials < MyMergedModel
 
   def get_feed_internal
-    proxy = FinancialsProxy.new({user_id: @uid})
-    body = proxy.get.try(:[], :body)
-    body.try(:[], "student") || {}
+    if Settings.features.financials
+      proxy = FinancialsProxy.new({user_id: @uid})
+      body = proxy.get.try(:[], :body)
+      body.try(:[], "student") || {}
+    else
+      {}
+    end
   end
 
 end
