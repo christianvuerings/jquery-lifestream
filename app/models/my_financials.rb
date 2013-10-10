43ae4a744de9a96efd53dc0fb9c7fe18a34e03fc
Chris Tweney <chris@media.berkeley.edu>
2013-10-10 10:06:10 -0700

    CLC-2620 Include current term in financials feed

--- app/models/my_financials.rb
+++ app/models/my_financials.rb
@@ -1,13 +1,18 @@
 class MyFinancials < MyMergedModel
 
   def get_feed_internal
+    feed = {}
     if Settings.features.financials
       proxy = FinancialsProxy.new({user_id: @uid})
-      body = proxy.get.try(:[], :body)
-      body.try(:[], "student") || {}
-    else
-      {}
+      proxy_response = proxy.get
+      if proxy_response && body = proxy_response[:body]
+        if student = body["student"]
+          feed.merge!(student)
+          feed.merge!({"current_term" => Settings.sakai_proxy.current_terms.first})
+        end
+      end
     end
+    feed
   end
 
 end
