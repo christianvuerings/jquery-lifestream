2e91168fdb312728068888a868dbaf5f95affc42
Jason Miller <jason.c.miller@berkeley.edu>
2013-10-17 11:57:44 -0700

    NOJIRA: Added UID for Jason Miller as a SuperUser in CalCentral

--- db/migrate/2013021122250300_create_user_auths.rb
+++ db/migrate/2013021122250300_create_user_auths.rb
@@ -13,7 +13,7 @@ class CreateUserAuths < ActiveRecord::Migration
     end
 
     # Seeding super users
-    super_users = %w(192517 323487 191779 238382 208861 675750 3222279 2040 904715 211159 978966)
+    super_users = %w(192517 323487 191779 238382 208861 675750 3222279 2040 904715 211159 978966 1044957)
     super_users.each do |uid|
       Rails.logger.info "Flagging #{uid} as a superuser"
       UserAuth.new_or_update_superuser! uid
