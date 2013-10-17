d420b39f55799046f72a9d864f2e36ef4febd35d
Edward Look <edwlook@gmail.com>
2013-10-17 10:48:24 -0700

    CLC-2710 - Deprecated responseInterceptors in httpConfiguration

--- app/assets/javascripts/angular/services/spinnerInterceptorService.js
+++ app/assets/javascripts/angular/services/spinnerInterceptorService.js
@@ -4,42 +4,40 @@
 
   angular.module('calcentral.services').factory('spinnerInterceptorService', ['$q' , function($q) {
 
-    /**
-     * Success function, will happen when the request was successful
-     * @param {Object} response JSON object containing response params
-     */
-    var success = function(response) {
+    return {
 
-      // The data will be a string when it's a template that has been requested.
-      if (angular.isObject(response.data)) {
-        response.data._is_loading = false;
+      /**
+       * Success function, will happen when the request was successful
+       * @param {Object} response JSON object containing response params
+       */
+      response: function(response) {
+
+        // The data will be a string when it's a template that has been requested.
+        if (angular.isObject(response.data)) {
+          response.data._is_loading = false;
+        }
+        return response;
+      },
+
+      /**
+       * Error function, will happen when we get a 4xx or 5xx exception
+       * @param {Object} response JSON object containing response params
+       */
+      responseError: function(response) {
+
+        // TODO we'll need to change this so we can show a valuable
+        // message to the user when an error occurs
+        // We can do this as soon as we get good error responses back from the server.
+        if (angular.isObject(response.data)) {
+          response.data._is_loading = false;
+        } else {
+          response.data = {
+            _is_loading: false
+          };
+        }
+        return $q.reject(response);
       }
-      return response;
-    };
 
-    /**
-     * Error function, will happen when we get a 4xx or 5xx exception
-     * @param {Object} response JSON object containing response params
-     */
-    var error = function(response) {
-
-      // TODO we'll need to change this so we can show a valuable
-      // message to the user when an error occurs
-      // We can do this as soon as we get good error responses back from the server.
-      if (angular.isObject(response.data)) {
-        response.data._is_loading = false;
-      } else {
-        response.data = {
-          _is_loading: false
-        };
-      }
-      return $q.reject(response);
-
-    };
-
-    // Return the function
-    return function(promise) {
-      return promise.then(success, error);
     };
 
   }]);
