be1e092c89be0dbe08142bc19187179a98e23d4d
Haroon Rasheed <haroonrasheed@berkeley.edu>
2013-10-25 17:13:29 -0700

    [CLC-2705] Switching between users you act as is cumbersome

--- spec/features/act_as_user_spec.rb
+++ spec/features/act_as_user_spec.rb
@@ -40,35 +40,31 @@ feature "act_as_user" do
     # disabling the cache_warmer while we're switching back and forth between users
     # The switching back triggers a cache invalidation, while the warming thread is still running.
     Calcentral::USER_CACHE_WARMER.stub(:warm).and_return(nil)
+    UserAuth.stub(:is_superuser?).with('192517').and_return(true)
+    UserAuth.stub(:is_superuser?).with('2040').and_return(false)
+    UserAuth.stub(:is_superuser?).with('1234').and_return(false)
+    UserAuth.stub(:is_superuser?).with('9876').and_return(false)
+
     login_with_cas "192517"
-    UserAuth.stub(:is_superuser?, '192517').and_return(true)
-    suppress_rails_logging {
-      act_as_user "2040"
-    }
+    act_as_user "2040"
     visit "/api/my/status"
     response = JSON.parse(page.body)
     response["is_logged_in"].should be_true
     response["uid"].should == "2040"
 
-    suppress_rails_logging {
-      act_as_user "1234"
-    }
+    act_as_user "1234"
     visit "/api/my/status"
     response = JSON.parse(page.body)
     response["is_logged_in"].should be_true
     response["uid"].should == "1234"
 
-    suppress_rails_logging {
-      act_as_user "9876"
-    }
+    act_as_user "9876"
     visit "/api/my/status"
     response = JSON.parse(page.body)
     response["is_logged_in"].should be_true
     response["uid"].should == "9876"
 
-    suppress_rails_logging {
-     stop_act_as_user
-    }
+    stop_act_as_user
     visit "/api/my/status"
     response = JSON.parse(page.body)
     response["is_logged_in"].should be_true
