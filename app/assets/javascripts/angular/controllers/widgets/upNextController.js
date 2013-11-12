8d3de73ce66e92ee39228c83e33d167de31e9dba
Christian Vuerings <vueringschristian@gmail.com>
2013-11-12 08:00:30 -0800

    CLC-2757 - Misleading initial dashboard display on Up Next

--- app/assets/javascripts/angular/controllers/widgets/upNextController.js
+++ app/assets/javascripts/angular/controllers/widgets/upNextController.js
@@ -6,9 +6,20 @@
    */
   calcentral.controller('UpNextController', ['$http', '$scope', function($http, $scope) {
 
+    /**
+     * Make sure that we're not showing wrong date information to the user.
+     * This will make sure that the date that is shown in the UI is the
+     * same as the last modified date of the feed.
+     * @param {Integer} epoch Last modified date epoch
+     */
+    var setLastModifiedDate = function(epoch) {
+      $scope.lastModifiedDate = new Date(epoch * 1000);
+    };
+
     var getUpNext = function() {
       $http.get('/api/my/up_next').success(function(data) {
         angular.extend($scope, data);
+        setLastModifiedDate(data.last_modified.timestamp.epoch);
       });
     };
 
