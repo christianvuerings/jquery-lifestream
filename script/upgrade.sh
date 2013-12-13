3b9b44fd1276a9af95ca0bf9790ffd9aa3b3f951
Ray Davis <ray@media.berkeley.edu>
2013-12-13 13:40:24 -0800

    CLC-2933 Reduce chance of DB erasure on bad deploy

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -10,7 +10,7 @@ fi
 
 ./script/init.d/calcentral maint
 
-./script/update-build.sh
+./script/update-build.sh || { echo "ERROR: update-build failed" ; exit 1 ; }
 
 # run migrate.sh only if we are on node 1
 if [ "X$NODEONE" != "X" ]; then
