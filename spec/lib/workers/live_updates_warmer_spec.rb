be3e7edafd25b182c322aad8ac20cb4c70f1593c
Mike Blake <chikoon@gmail.com>
2014-01-31 10:08:48 -0800

    CLC-3187-ii: LiveUpdates: eliminate MERGED_FEEDS_EXPIRATION
    https://jira.media.berkeley.edu/jira/browse/CLC-3187
    
    Deleted MERGED_FEEDS_EXPIRATION instance of UserCacheInvalidator.
    
    Since UserCacheWarmer now forces write to the cache, there's no
    need to expire the cache on merged feeds via observers.
    
    Also deleted MERGED_FEEDS_EXPIRATION from the cache
    cache_tools initializer and references to it in the
    live_updates_warmer_spec.

--- spec/lib/workers/live_updates_warmer_spec.rb
+++ spec/lib/workers/live_updates_warmer_spec.rb
@@ -10,7 +10,6 @@ describe "LiveUpdatesWarmer" do
   it "should not explode if an error gets thrown during the warming" do
     UserVisit.record "1234"
     UserCacheWarmer.stub(:do_warm).and_raise(TypeError)
-    Calcentral::MERGED_FEEDS_EXPIRATION.should_receive(:notify).once
 
     @warmer.warmup_merged_feeds "1234"
   end
@@ -29,7 +28,6 @@ describe "LiveUpdatesWarmer" do
 
   it "should process warmup request messages" do
     UserCacheWarmer.stub(:do_warm).and_return(nil)
-    Calcentral::MERGED_FEEDS_EXPIRATION.should_receive(:notify).with(@random_id)
     LiveUpdatesWarmer.should_receive(:increment).exactly(2).times
     UserCacheWarmer.should_receive(:do_warm).with(@random_id)
     @warmer.on_message(@random_id)
