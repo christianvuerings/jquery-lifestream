ec1cc2215cb0cefba960db9c863469ec7ced9aea
Chris Tweney <chris@media.berkeley.edu>
2014-02-05 13:43:59 -0800

    CLC-3197 BearfactsProxy now uses generic exception handling

--- lib/proxies/bearfacts_proxy.rb
+++ lib/proxies/bearfacts_proxy.rb
@@ -13,6 +13,12 @@ class BearfactsProxy < BaseProxy
   end
 
   def request(path, vcr_cassette, params = {})
+    safe_request("Remote server unreachable") do
+      internal_do_request(path, vcr_cassette, params)
+    end
+  end
+
+  def internal_do_request(path, vcr_cassette, params = {})
     self.class.fetch_from_cache(@uid) do
       student_id = lookup_student_id
       if student_id.nil?
@@ -24,40 +30,31 @@ class BearfactsProxy < BaseProxy
       else
         url = "#{Settings.bearfacts_proxy.base_url}#{path}"
         logger.info "Fake = #@fake; Making request to #{url} on behalf of user #{@uid}, student_id = #{student_id}; cache expiration #{self.class.expires_in}"
-        begin
-          response = FakeableProxy.wrap_request(APP_ID + "_" + vcr_cassette, @fake, {:match_requests_on => [:method, :path]}) {
-            token_params = {token: Settings.bearfacts_proxy.token}
-            if (Settings.bearfacts_proxy.app_id.present? && Settings.bearfacts_proxy.app_key.present?)
-              token_params.merge!({app_id: Settings.bearfacts_proxy.app_id,
-                                   app_key: Settings.bearfacts_proxy.app_key,})
-            end
-
-            Faraday::Connection.new(
-              :url => url,
-              :params => params.merge(token_params),
-              :ssl => {:verify => false},
-              :request => {
-                :timeout => Settings.application.outgoing_http_timeout
-              }
-            ).get
-          }
-          if response.status >= 400
-            logger.error "Connection failed: #{response.status} #{response.body}"
-            return nil
+        response = FakeableProxy.wrap_request(APP_ID + "_" + vcr_cassette, @fake, {:match_requests_on => [:method, :path]}) {
+          token_params = {token: Settings.bearfacts_proxy.token}
+          if (Settings.bearfacts_proxy.app_id.present? && Settings.bearfacts_proxy.app_key.present?)
+            token_params.merge!({app_id: Settings.bearfacts_proxy.app_id,
+                                 app_key: Settings.bearfacts_proxy.app_key, })
           end
 
-          logger.debug "Remote server status #{response.status}, Body = #{response.body}"
-          {
-            :body => response.body,
-            :status_code => response.status
-          }
-        rescue Faraday::Error::ConnectionFailed, Faraday::Error::TimeoutError, Errno::EHOSTUNREACH => e
-          logger.error "Connection failed: #{e.class} #{e.message}"
-          {
-            :body => "Remote server unreachable",
-            :status_code => 503
-          }
+          Faraday::Connection.new(
+            :url => url,
+            :params => params.merge(token_params),
+            :ssl => {:verify => false},
+            :request => {
+              :timeout => Settings.application.outgoing_http_timeout
+            }
+          ).get
+        }
+        if response.status >= 400
+          raise Calcentral::ProxyError.new("Connection failed: #{response.code} #{response.body}; url = #{url}")
         end
+
+        logger.debug "Remote server status #{response.status}, Body = #{response.body}"
+        {
+          :body => response.body,
+          :status_code => response.status
+        }
       end
     end
   end
