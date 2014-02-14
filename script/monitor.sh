9dffd31d6639e012e4eb020b83f52a7a853598f5
Ray Davis <ray@media.berkeley.edu>
2014-02-13 16:30:24 -0800

    CLC-3282 monitor.sh attaching to wrong process

--- script/monitor.sh
+++ script/monitor.sh
@@ -9,7 +9,7 @@ while true; do
   STATS=`(sleep 1 ; echo "stats"; sleep 1; echo "quit") | telnet $HOSTNAME 11211`
   echo "==========================================="
   echo `date`
-  jmap -heap `jps -mlv|grep jruby|head -1|awk '{print $1}'`
+  jmap -heap `jps -mlv|grep 'torquebox-server'|head -1|awk '{print $1}'`
   echo $STATS | awk -F"STAT" '{for(i=2;i<NF;++i)print $i}'
   sleep $DELAY
 done
