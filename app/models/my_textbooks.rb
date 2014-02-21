c306bab4b44eccae95192f81e623d78be7729c7c
Chris Tweney <chris@media.berkeley.edu>
2014-02-21 12:43:07 -0800

    CLC-3291 Quick fix for Textbooks so it won't cache JSON derivations of error states

--- app/models/my_textbooks.rb
+++ app/models/my_textbooks.rb
@@ -8,18 +8,14 @@ class MyTextbooks < MyMergedModel
   end
 
   def get_feed_as_json(*opts)
-    self.class.fetch_from_cache "json-#{@ccn}-#{@slug}" do
-      init
-      feed = get_feed_internal(*opts)
-      feed.to_json
-    end
+    get_feed_internal
   end
 
   def get_feed_internal
     feed = {}
     if Settings.features.textbooks
       proxy = TextbooksProxy.new({user_id: @uid, ccns: @ccns, slug: @slug})
-      proxy_response = proxy.get
+      proxy_response = proxy.get_as_json
       feed = proxy_response
     end
     feed
--- lib/calcentral/proxy_error.rb
+++ lib/calcentral/proxy_error.rb
@@ -2,10 +2,18 @@ module Calcentral
   class ProxyError < Exception
     attr_accessor :wrapped_exception, :log_message, :response
 
-    def initialize(log_message=nil, response=nil, wrapped_exception=nil)
+    def initialize(log_message, *args)
       @log_message = log_message
-      @response = response
-      @wrapped_exception = wrapped_exception
+      if args && args.length > 0
+        @response = args[0]
+        @wrapped_exception = args[1]
+      else
+        @wrapped_exception = nil
+        @response = {
+          :body => log_message,
+          :status_code => 500
+        }
+      end
     end
   end
 end
