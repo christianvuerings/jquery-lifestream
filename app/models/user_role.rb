2668b2fab8ffe88c89e33f5334a24b368c01c586
Chris Tweney <chris@media.berkeley.edu>
2014-03-13 09:42:12 -0700

    CLC-3410 Move some user models into app/models/user

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,3 +1,5 @@
+# TODO rename this class to LinkRole, it has nothing to do with users. It's used only as part of the MyCampusLinks feature.
+# Renaming will require a db migration.
 class UserRole < ActiveRecord::Base
   attr_accessible :name, :slug, :link_ids
   has_and_belongs_to_many :links
