332c1a9c0df487028c131652e4ff6aac7e51e20c
Mike Blake <chikoon@gmail.com>
2014-03-21 22:21:55 +0100

    "removed acting_as? method from UserSpecificModel The method was never used and is essencially the same as the model's  is_acting_as_nonfake_user? method.

--- app/models/user_specific_model.rb
+++ app/models/user_specific_model.rb
@@ -1,4 +1,4 @@
-class UserSpecificModel < AbstractModel
++class UserSpecificModel < AbstractModel
 
   def initialize(uid, options={})
     super(uid, options)
@@ -14,13 +14,6 @@ class UserSpecificModel < AbstractModel
     else
       @uid
     end
-  end
-
-  # chikoon: this method could be used by any model that inherits from UserSpecificModel
-  # the problem is that many methods don't, and so this acting_as? method will
-  # most probably need to be removed
-  def acting_as?
-    (@original_uid && (@original_uid != @uid))
   end
 
   def is_acting_as_nonfake_user?
