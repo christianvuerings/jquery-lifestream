3b231c85465848bdf0b869b56a6939e09ac943f4
Chris Tweney <chris@media.berkeley.edu>
2014-04-07 14:30:42 -0700

    CLC-3568 Work around Rails 4 date conversion bug

--- app/models/my_tasks/tasks_module.rb
+++ app/models/my_tasks/tasks_module.rb
@@ -33,27 +33,13 @@ module MyTasks
       if !due_date.blank?
         tomorrow = starting_date.advance(:days => 1)
 
-        # TODO Rails 4 bug? due_date.to_i shows up as one day later than due_date's string representation.
-        Rails.logger.error "--------------------------------------------------"
-        Rails.logger.error "due_date >= now_time: #{due_date >= now_time}"
-        Rails.logger.error "due_date >= tomorrow: #{due_date >= tomorrow}"
-        Rails.logger.error "due data < tomorrow: #{due_date < tomorrow}"
-
-        Rails.logger.error "due_date.to_i >= now_time.to_i: #{due_date.to_i >= now_time.to_i}"
-        Rails.logger.error "due_date.to_i >= tomorrow.to_i: #{due_date.to_i >= tomorrow.to_i}"
-        Rails.logger.error "due data.to_i < tomorrow.to_i: #{due_date.to_i < tomorrow.to_i}"
-
-        Rails.logger.error "--------------------------------------------------"
-
-        if due_date.to_i < now_time.to_i
+        if due_date.to_time.to_i < now_time.to_time.to_i
           bucket = "Overdue"
-        elsif due_date.to_i >= now_time.to_i && due_date.to_i < tomorrow.to_i
+        elsif due_date.to_time.to_i >= now_time.to_time.to_i && due_date.to_time.to_i < tomorrow.to_time.to_i
           bucket = "Today"
-        elsif due_date.to_i >= tomorrow.to_i
+        elsif due_date.to_time.to_i >= tomorrow.to_time.to_i
           bucket = "Future"
         end
-
-        Rails.logger.debug "XXX starting_date = #{starting_date}, to_i = #{starting_date.to_i}; tomorrow = #{tomorrow}, to_i = #{tomorrow.to_i}; due_date = #{due_date}, to_i = #{due_date.to_i}"
 
         Rails.logger.debug "#{self.class.name} In determine_bucket, bucket = #{bucket}, due_date = #{due_date}, now_time = #{now_time}, tomorrow = #{tomorrow}; formatted entry = #{formatted_entry}"
       end
