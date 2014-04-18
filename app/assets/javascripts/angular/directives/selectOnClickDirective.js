cdc03455cf05d4a3cc68849937caf8f1ac9b7782
Edward Look <edwlook@gmail.com>
2014-04-17 23:29:14 -0700

    Remove space before round braces

--- app/assets/javascripts/angular/directives/selectOnClickDirective.js
+++ app/assets/javascripts/angular/directives/selectOnClickDirective.js
@@ -4,8 +4,8 @@
   angular.module('calcentral.directives').directive('ccSelectOnClickDirective', function() {
     return {
       restrict: 'A',
-      link: function (scope, elm) {
-        elm.on('click', function () {
+      link: function(scope, elm) {
+        elm.on('click', function() {
           this.select();
         });
       }
