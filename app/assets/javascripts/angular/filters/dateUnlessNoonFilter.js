c80407aed839690195134f552e9e4e8d5a52ca3a
Christian Vuerings <vueringschristian@gmail.com>
2014-04-24 15:17:11 -0700

    CLC-3670: Change name convention of the AngularJS filters https://jira.ets.berkeley.edu/jira/browse/CLC-3670

--- app/assets/javascripts/angular/filters/dateUnlessNoonFilter.js
+++ app/assets/javascripts/angular/filters/dateUnlessNoonFilter.js
@@ -1,7 +1,7 @@
 (function(angular) {
   'use strict';
 
-  angular.module('calcentral.filters').filter('cc.dateUnlessNoon', function($filter) {
+  angular.module('calcentral.filters').filter('dateUnlessNoonFilter', function($filter) {
     return function(millisecondsEpoch, format) {
       var date = $filter('date')(millisecondsEpoch, format);
       date = date.replace('12:00 PM', 'Noon');
