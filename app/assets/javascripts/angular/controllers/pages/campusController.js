a6d69eb6a49d3d3dc280e5d72d5558f1d4b44e8a
Christian Vuerings <vueringschristian@gmail.com>
2014-04-24 13:42:41 -0700

    CLC-3669: Refactor campus links controller & filters https://jira.ets.berkeley.edu/jira/browse/CLC-3669

--- app/assets/javascripts/angular/controllers/pages/campusController.js
+++ app/assets/javascripts/angular/controllers/pages/campusController.js
@@ -4,150 +4,21 @@
   /**
    * Campus controller
    */
-
-  angular.module('calcentral.controllers').controller('CampusController', function($http, $routeParams, $scope, apiService) {
-
-    /**
-     * Add to the subcategories list if it doesn't exist yet
-     * @param {String} subcategory The subcategory you want to add
-     */
-    var addToSubcategories = function(subcategory) {
-      if ($scope.subcategories.indexOf(subcategory) === -1) {
-        $scope.subcategories.push(subcategory);
-      }
-    };
-
-    /**
-     * Add to the top categories
-     * @param {Object} link Link object
-     */
-    var addToTopCategories = function(link) {
-      for (var i = 0; i < link.categories.length; i++) {
-        $scope.topcategories[link.categories[i].topcategory] = true;
-      }
-    };
-
-    /**
-     * Depending on the roles, determine whether the current user should be able to view the link
-     * @param {Object} link Link object
-     * @return {Boolean} Whether the user should be able to view the link
-     */
-    var canViewLink = function(link) {
-      for (var i in link.roles) {
-        if (link.roles.hasOwnProperty(i) &&
-            link.roles[i] === true &&
-            link.roles[i] ===  $scope.api.user.profile.roles[i]) {
-          addToTopCategories(link);
-          return true;
-        }
-      }
-      return false;
-    };
-
-    /**
-     * Check whether a link is in a current category
-     * @param {Object} link Link object
-     * @return {Boolean} Whether a link is in the current category
-     */
-    var isLinkInCategory = function(link) {
-      link.subCategories = [];
-      for (var i = 0; i < link.categories.length; i++) {
-        if (link.categories[i].topcategory === $scope.currentTopCategory) {
-          link.subCategories.push(link.categories[i].subcategory);
-        }
-      }
-      return (link.subCategories.length > 0);
-    };
-
-    /**
-     * Set the links for the current page
-     * @param {Array} links The list of links that need to be parsed
-     */
-    var setLinks = function(links) {
-      $scope.links = [];
-      $scope.subcategories = [];
-      $scope.topcategories = {};
-      angular.forEach(links, function(link) {
-        if (canViewLink(link) && isLinkInCategory(link)) {
-          $scope.links.push(link);
-          for (var i = 0; i < link.subCategories.length; i++) {
-            addToSubcategories(link.subCategories[i]);
-          }
-        }
-      });
-      $scope.subcategories.sort();
-    };
-
-    /**
-     * Get the category name, when you feed in an id
-     * @param {String} categoryId A category id
-     * @return {String} The category name
-     */
-    var getCategoryName = function(categoryId) {
-      var navigation = $scope.navigation;
-
-      // We want to explicitly check for undefined here
-      // since other values need to result in a 404.
-      if (categoryId === undefined) {
-        return navigation[0].categories[0].name;
-      }
-
-      for (var i = 0; i < navigation.length; i++) {
-        for (var j = 0; j < navigation[i].categories.length; j++) {
-          if (navigation[i].categories[j].id === categoryId) {
-            return navigation[i].categories[j].name;
-          }
-        }
-      }
-    };
-
-    /**
-     * Get the links
-     */
-    var getLinks = function() {
-      // Data contains "links" and "navigation"
-      var linkDataUrl = '/json/campuslinks_v16.json';
-      if ($scope.api.user.profile.features.live_campus_links_data) {
-        linkDataUrl = '/api/my/campuslinks';
-      }
-      $http.get(linkDataUrl).success(function(campusdata) {
-      //$http.get('/json/campuslinks.json').success(function(campusdata) {
-        angular.extend($scope, campusdata);
-
-        $scope.currentTopCategory = getCategoryName($routeParams.category);
-        var title = 'Campus - ' + $scope.currentTopCategory;
-        apiService.util.setTitle(title);
-
-        setLinks($scope.links);
-      });
-    };
+  angular.module('calcentral.controllers').controller('CampusController', function($routeParams, $scope, apiService, campusLinksFactory) {
 
     // We need to wait until the user is loaded
     $scope.$on('calcentral.api.user.isAuthenticated', function(event, isAuthenticated) {
       if (isAuthenticated) {
-        getLinks();
+        campusLinksFactory.getLinks($routeParams.category).then(function(data) {
+          angular.extend($scope, data);
+
+          // Set the page title
+          var title = 'Campus - ' + data.currentTopCategory;
+          apiService.util.setTitle(title);
+        });
       }
     });
 
-  })
-
-  // There is no way to pass in a parameter to a filter, so we need to create our own
-  // http://stackoverflow.com/questions/11753321
-  // This filter will allow us to only show items in a certain subcategory
-  .filter('linksubcategory', function() {
-    return function(items, name) {
-      var arrayToReturn = [];
-      for (var i = 0; i < items.length; i++){
-        var item = items[i];
-        for (var j = 0; j < item.subCategories.length; j++) {
-          if (item.subCategories[j] === name) {
-            arrayToReturn.push(item);
-          }
-        }
-      }
-
-      return arrayToReturn;
-    };
   });
 
 })(window.angular);
---
+++ app/assets/javascripts/angular/factories/campusLinksFactory.js
@@ -0,0 +1,146 @@
+(function(angular) {
+
+  'use strict';
+
+  /**
+   * Campus Links Factory - get links from the campuslinks API
+   */
+  angular.module('calcentral.factories').factory('campusLinksFactory', function(apiService, $http) {
+
+    /**
+     * Add to the subcategories list if it doesn't exist yet
+     * @param {String} subcategory The subcategory you want to add
+     * @param {Array} subcategories The subcategories array
+     */
+    var addToSubcategories = function(subcategory, subcategories) {
+      if (subcategories.indexOf(subcategory) === -1) {
+        subcategories.push(subcategory);
+      }
+    };
+
+    /**
+     * Add to the top categories
+     * @param {Object} link Link object
+     * @param {Object} topcategories Top categories object
+     */
+    var addToTopCategories = function(link, topcategories) {
+      for (var i = 0; i < link.categories.length; i++) {
+        topcategories[link.categories[i].topcategory] = true;
+      }
+    };
+
+    /**
+     * Depending on the roles, determine whether the current user should be able to view the link
+     * @param {Object} link Link object
+     * @return {Boolean} Whether the user should be able to view the link
+     */
+    var canViewLink = function(link) {
+      for (var i in link.roles) {
+        if (link.roles.hasOwnProperty(i) &&
+            link.roles[i] === true &&
+            link.roles[i] ===  apiService.user.profile.roles[i]) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    /**
+     * Check whether a link is in a current category
+     * @param {Object} link Link object
+     * @param {String} currentTopCategory The current top category
+     * @return {Boolean} Whether a link is in the current category
+     */
+    var isLinkInCategory = function(link, currentTopCategory) {
+      link.subCategories = [];
+      for (var i = 0; i < link.categories.length; i++) {
+        if (link.categories[i].topcategory === currentTopCategory) {
+          link.subCategories.push(link.categories[i].subcategory);
+        }
+      }
+      return (link.subCategories.length > 0);
+    };
+
+    /**
+     * Compile the campus links
+     * @param {Array} links The list of links that need to be parsed
+     */
+    var compileLinks = function(links, currentTopCategory) {
+      var response = {
+        links: [],
+        subcategories: [],
+        topcategories: {}
+      };
+      angular.forEach(links, function(link) {
+        var canUserViewLink = canViewLink(link);
+        if (canUserViewLink) {
+          addToTopCategories(link, response.topcategories);
+        }
+        if (canUserViewLink && isLinkInCategory(link, currentTopCategory)) {
+          response.links.push(link);
+          for (var i = 0; i < link.subCategories.length; i++) {
+            addToSubcategories(link.subCategories[i], response.subcategories);
+          }
+        }
+      });
+      response.subcategories.sort();
+      return response;
+    };
+
+    /**
+     * Get the category name, when you feed in an id
+     * @param {String} categoryId A category id
+     * @param {Object} navigation The navigation object
+     * @return {String} The category name
+     */
+    var getCategoryName = function(categoryId, navigation) {
+
+      // We want to explicitly check for undefined here
+      // since other values need to result in a 404.
+      if (categoryId === undefined) {
+        return navigation[0].categories[0].name;
+      }
+
+      for (var i = 0; i < navigation.length; i++) {
+        for (var j = 0; j < navigation[i].categories.length; j++) {
+          if (navigation[i].categories[j].id === categoryId) {
+            return navigation[i].categories[j].name;
+          }
+        }
+      }
+    };
+
+    var parseCampusLinks = function(campusLinksResponse, categoryId) {
+      var data = campusLinksResponse.data;
+
+      var currentTopCategory = getCategoryName(categoryId, data.navigation);
+      var compileResponse = compileLinks(data.links, currentTopCategory);
+
+      data.currentTopCategory = currentTopCategory;
+      data.links = compileResponse.links;
+      data.subcategories = compileResponse.subcategories;
+      data.topcategories = compileResponse.topcategories;
+
+      return data;
+    };
+
+    var getLinks = function(categoryId) {
+
+      // Data contains "links" and "navigation"
+      var linkDataUrl = '/json/campuslinks_v16.json';
+      if (apiService.user.profile.features.live_campus_links_data) {
+        linkDataUrl = '/api/my/campuslinks';
+      }
+
+      return $http.get(linkDataUrl).then(function(response) {
+        return parseCampusLinks(response, categoryId);
+      });
+    };
+
+    return {
+      getLinks: getLinks
+    };
+
+  });
+
+}(window.angular));
