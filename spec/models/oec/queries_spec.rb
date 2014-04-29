2f8a343e0be7f7041d7d222448b882e2c81b0546
Ray Davis <ray@media.berkeley.edu>
2014-04-29 11:32:24 -0700

    CLC-3002 Make it possible to test OEC outside Bamboo-testext; change OEC term configuration

--- spec/models/oec/queries_spec.rb
+++ spec/models/oec/queries_spec.rb
@@ -1,44 +1,41 @@
 require "spec_helper"
 
 describe Oec::Queries do
-
-  context "looking up students" do
-    subject { Oec::Queries.get_all_students(["11684"]) }
-    it { should_not be_nil }
-    unless Oec::Queries.test_data?
-      it { subject[0]["ldap_uid"].should_not be_nil }
+  let(:test_ccn) do
+    if Oec::Queries.test_data?
+      '7309'
+    else
+      '11684'
     end
   end
 
-  context "looking up instructors" do
-    subject { Oec::Queries.get_all_instructors(["11684"]) }
+  context "looking up students" do
+    subject { Oec::Queries.get_all_students([test_ccn]) }
     it { should_not be_nil }
-    unless Oec::Queries.test_data?
-      it { subject[0]["ldap_uid"].should_not be_nil }
-    end
+    it { subject[0]["ldap_uid"].should_not be_nil }
+  end
+
+  context "looking up instructors" do
+    subject { Oec::Queries.get_all_instructors([test_ccn]) }
+    it { should_not be_nil }
+    it { subject[0]["ldap_uid"].should_not be_nil }
   end
 
   context "looking up courses", :testext => true do
     subject { Oec::Queries.get_all_courses }
     it { should_not be_nil }
-    unless Oec::Queries.test_data?
-      it { subject[0]["course_id"].should_not be_nil }
-    end
+    it { subject[0]["course_id"].should_not be_nil }
   end
 
   context "looking up course_instructors" do
-    subject { Oec::Queries.get_all_course_instructors(["11684"]) }
+    subject { Oec::Queries.get_all_course_instructors([test_ccn]) }
     it { should_not be_nil }
-    unless Oec::Queries.test_data?
-      it { subject[0]["ldap_uid"].should_not be_nil }
-    end
+    it { subject[0]["ldap_uid"].should_not be_nil }
   end
 
   context "looking up course_students" do
-    subject { Oec::Queries.get_all_course_students(["11684"]) }
+    subject { Oec::Queries.get_all_course_students([test_ccn]) }
     it { should_not be_nil }
-    unless Oec::Queries.test_data?
-      it { subject[0]["ldap_uid"].should_not be_nil }
-    end
+    it { subject[0]["ldap_uid"].should_not be_nil }
   end
 end
