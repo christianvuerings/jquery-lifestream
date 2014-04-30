c89c52a4914f6e13ac30384d728df2b70dcddc50
Chris Tweney <chris@media.berkeley.edu>
2014-04-30 14:13:50 -0700

    CLC-3384 Rewrite MyFinancials spec to current standards

--- spec/models/financials/my_financials_spec.rb
+++ spec/models/financials/my_financials_spec.rb
@@ -1,36 +1,70 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Financials::MyFinancials do
 
-  let!(:oski_uid) { "61889" }
-  let!(:fake_financials_proxy) { Financials::Proxy.new({user_id: oski_uid, fake: true}) }
-  before(:each) { Financials::Proxy.stub(:new).and_return(fake_financials_proxy) }
+  let!(:oski_uid) { '61889' }
+  let!(:fake_proxy) { Financials::Proxy.new({user_id: oski_uid, fake: true}) }
+  before(:each) { Financials::Proxy.stub(:new).and_return(fake_proxy) }
 
-  context "happy path" do
-    subject { Financials::MyFinancials.new(oski_uid).get_feed }
-    it { should_not be_nil }
-    its(["summary"]) { should_not be_nil }
-    its(["currentTerm"]) { should == Berkeley::Terms.fetch.current.to_english }
-    its(["apiVersion"]) { should == "1.0.6" }
+  subject { Financials::MyFinancials.new(oski_uid).get_feed }
+
+  shared_examples 'an empty feed' do
+    it 'should have just 2 fields' do
+      expect(subject.length).to eq 2
+    end
   end
 
-  context "it should not explode on a proxy error state" do
-    before(:each) { fake_financials_proxy.stub(:get).and_return(
+  shared_examples 'a feed with the common live-updates fields' do
+    it 'should have a lastModified field' do
+      expect(subject[:lastModified]).to be
+    end
+    it 'should have a feedName field' do
+      expect(subject[:feedName]).to eq 'Financials::MyFinancials'
+    end
+  end
+
+  context 'happy path for #get_feed' do
+    it 'has a non-nil model response' do
+      expect(subject).to be
+    end
+    it 'has a summary' do
+      expect(subject['summary']).to be
+    end
+    it 'includes the English translation of the current term' do
+      expect(subject['currentTerm']).to eq Berkeley::Terms.fetch.current.to_english
+    end
+    it 'includes an apiVersion field' do
+      expect(subject['apiVersion']).to eq '1.0.6'
+    end
+    it_behaves_like 'a feed with the common live-updates fields'
+  end
+
+  context 'when the proxy returns an error condition' do
+    before { fake_proxy.stub(:get).and_return(
       {
-        body: "an error message",
+        body: 'an error message',
         statusCode: 500
       }
     ) }
-    subject { Financials::MyFinancials.new(oski_uid).get_feed }
-    it { subject.length.should == 4 }
-    its([:body]) { should == "an error message"}
-    its([:statusCode]) { should == 500 }
+    it 'should have an error message text' do
+      expect(subject[:body]).to eq 'an error message'
+    end
+    it 'should have a 500 status code' do
+      expect(subject[:statusCode]).to eq 500
+    end
+    it_behaves_like 'a feed with the common live-updates fields'
   end
 
-  context "it should not explode on a null proxy response" do
-    before(:each) { fake_financials_proxy.stub(:get).and_return(nil) }
-    subject { Financials::MyFinancials.new(oski_uid).get_feed }
-    it { subject.length.should == 2 }
+  context 'when the proxy returns nil' do
+    before { fake_proxy.stub(:get).and_return(nil) }
+    it_behaves_like 'an empty feed'
+    it_behaves_like 'a feed with the common live-updates fields'
+  end
+
+  context 'when the financials feature flag is disabled' do
+    before { Settings.features.stub(:financials).and_return(false) }
+    it_behaves_like 'an empty feed'
+    it_behaves_like 'a feed with the common live-updates fields'
   end
 
 end
