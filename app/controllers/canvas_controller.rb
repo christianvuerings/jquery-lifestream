468f6076afaae8ea1f97d839ead16c3b6a870155
Jason Miller <jason@redconfetti.com>
2014-05-02 14:42:53 -0700

    CLC-3646 - Added CanvasController#user_can_create_course_site API end point

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -1,7 +1,7 @@
 class CanvasController < ApplicationController
   include ClassLogger
 
-  before_filter :set_cross_origin_access_control_headers, :only => [:external_tools]
+  before_filter :set_cross_origin_access_control_headers, :only => [:external_tools, :user_can_create_course_site]
   rescue_from StandardError, with: :handle_api_exception
   rescue_from Errors::ClientError, with: :handle_client_error
 
@@ -17,4 +17,16 @@ class CanvasController < ApplicationController
     render json: Canvas::ExternalTools.new.public_list.to_json
   end
 
+  # Indicates if a Canvas user is authorized to provision course sites
+  # GET /api/academics/canvas/user_can_create_course_site
+  def user_can_create_course_site
+    authorization = false
+    campus_uid = Canvas::UserProfile.new(:canvas_user_id => params[:canvas_user_id]).login_id
+    if campus_uid
+      user = User::Auth.get(campus_uid.to_s)
+      policy = User::AuthPolicy.new(user,user)
+      authorization = policy.can_create_canvas_course_site?
+    end
+    render json: {'canCreateCourseSite' => authorization}.to_json
+  end
 end
