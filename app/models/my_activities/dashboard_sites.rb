0f74115dd3edcd95263594b7302348c25201c945
Mike Blake <chikoon@gmail.com>
2014-05-02 19:19:18 +0200

    NOJIRA-separate-MyActivities-classes-from-module
    In MyActivities, changed module::class to module;class so that the
    class really has the parent MyActivities
    
      MyActivities::DashboardSites
      MyActivities::NotificationActivities
      MyActivities::RegBlocks
      MyActivities::SakaiAnnouncements
    
    See: http://blog.elpassion.com/ruby-gotchas/

--- app/models/my_activities/dashboard_sites.rb
+++ app/models/my_activities/dashboard_sites.rb
@@ -1,9 +1,9 @@
-class MyActivities::DashboardSites
-
-  def self.fetch(uid, options={})
-    dashboard_sites = MyClasses::Merged.new(uid, options).get_feed[:classes]
-    dashboard_sites.concat(MyGroups::Merged.new(uid, options).get_feed[:groups])
-    dashboard_sites
+module MyActivities
+  class DashboardSites
+    def self.fetch(uid, options={})
+      dashboard_sites = MyClasses::Merged.new(uid, options).get_feed[:classes]
+      dashboard_sites.concat(MyGroups::Merged.new(uid, options).get_feed[:groups])
+      dashboard_sites
+    end
   end
-
 end
--- app/models/my_activities/reg_blocks.rb
+++ app/models/my_activities/reg_blocks.rb
@@ -1,81 +1,83 @@
-class MyActivities::RegBlocks
+module MyActivities
+  class RegBlocks
 
-  def self.append!(uid, activities)
-    blocks_feed = Bearfacts::MyRegBlocks.new(uid).get_feed
-    if blocks_feed.empty? || blocks_feed[:available] == false
-      return activities
-    end
+    def self.append!(uid, activities)
+      blocks_feed = Bearfacts::MyRegBlocks.new(uid).get_feed
+      if blocks_feed.empty? || blocks_feed[:available] == false
+        return activities
+      end
 
-    %w(active_blocks inactive_blocks).each do |block_category|
-      blocks_feed[block_category.to_sym].each do |block|
-        notification = process_block!(block)
-        activities << notification if notification.present?
+      %w(active_blocks inactive_blocks).each do |block_category|
+        blocks_feed[block_category.to_sym].each do |block|
+          notification = process_block!(block)
+          activities << notification if notification.present?
+        end
       end
     end
-  end
 
-  private
-  def self.process_block!(block)
-    blocked_date = block.try(:[], :blocked_date).try(:[], :epoch)
-    cleared_date = block.try(:[], :cleared_date).try(:[], :epoch)
-    if include_in_feed?(blocked_date, cleared_date)
+    private
+    def self.process_block!(block)
+      blocked_date = block.try(:[], :blocked_date).try(:[], :epoch)
+      cleared_date = block.try(:[], :cleared_date).try(:[], :epoch)
+      if include_in_feed?(blocked_date, cleared_date)
+        block.merge!(
+          {
+            id: '',
+            source: block[:short_description] || '',
+            source_url: "https://bearfacts.berkeley.edu/bearfacts/",
+            url: "https://bearfacts.berkeley.edu/bearfacts/",
+            emitter: "Bear Facts"
+          })
+        if cleared_date
+          process_cleared_block!(block)
+        else
+          process_active_block!(block)
+        end
+
+        unless (block[:block_type] == 'Academic' && block[:reason] == 'Academic')
+          block[:title] += ": #{block[:reason]}"
+        end
+
+        Rails.logger.debug "#{self.class.name} Reg block is in feed, type = #{block[:block_type]}," \
+            "blocked_date = #{blocked_date}; cleared_date = #{cleared_date}"
+        block
+      else
+        Rails.logger.debug "#{self.class.name} Reg block too old to include in feed, skipping. " \
+            "blocked_date = #{blocked_date}; cleared_date = #{cleared_date}"
+        nil
+      end
+    end
+
+    def self.process_cleared_block!(block)
       block.merge!(
         {
-          id: '',
-          source: block[:short_description] || '',
-          source_url: "https://bearfacts.berkeley.edu/bearfacts/",
-          url: "https://bearfacts.berkeley.edu/bearfacts/",
-          emitter: "Bear Facts"
+          type: "message",
+          date: block[:cleared_date],
+          title: "#{block[:block_type]} Block Cleared",
+          summary: "This block, placed on #{block[:blocked_date][:dateString]}, "\
+              "was cleared on #{block[:cleared_date][:dateString]}."
         })
-      if cleared_date
-        process_cleared_block!(block)
-      else
-        process_active_block!(block)
+    end
+
+    def self.process_active_block!(block)
+      block.merge!(
+        {
+          type: "alert",
+          date: block[:blocked_date],
+          title: "#{block[:block_type]} Block Placed",
+          summary: block[:message],
+        }
+      )
+    end
+
+    def self.include_in_feed?(blocked_date, cleared_date)
+      if blocked_date && blocked_date.to_i >= MyActivities::Merged.cutoff_date
+        return true
       end
-
-      unless (block[:block_type] == 'Academic' && block[:reason] == 'Academic')
-        block[:title] += ": #{block[:reason]}"
+      if cleared_date && cleared_date.to_i >= MyActivities::Merged.cutoff_date
+        return true
       end
-
-      Rails.logger.debug "#{self.class.name} Reg block is in feed, type = #{block[:block_type]}," \
-          "blocked_date = #{blocked_date}; cleared_date = #{cleared_date}"
-      block
-    else
-      Rails.logger.debug "#{self.class.name} Reg block too old to include in feed, skipping. " \
-          "blocked_date = #{blocked_date}; cleared_date = #{cleared_date}"
-      nil
+      false
     end
-  end
-
-  def self.process_cleared_block!(block)
-    block.merge!(
-      {
-        type: "message",
-        date: block[:cleared_date],
-        title: "#{block[:block_type]} Block Cleared",
-        summary: "This block, placed on #{block[:blocked_date][:dateString]}, "\
-            "was cleared on #{block[:cleared_date][:dateString]}."
-      })
-  end
-
-  def self.process_active_block!(block)
-    block.merge!(
-      {
-        type: "alert",
-        date: block[:blocked_date],
-        title: "#{block[:block_type]} Block Placed",
-        summary: block[:message],
-      }
-    )
-  end
-
-  def self.include_in_feed?(blocked_date, cleared_date)
-    if blocked_date && blocked_date.to_i >= MyActivities::Merged.cutoff_date
-      return true
-    end
-    if cleared_date && cleared_date.to_i >= MyActivities::Merged.cutoff_date
-      return true
-    end
-    false
   end
 end
