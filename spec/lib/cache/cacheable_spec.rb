156bb91ef8cbdae639e245159f107bb15f83990e
Chris Tweney <chris@media.berkeley.edu>
2014-05-30 10:24:32 -0700

    CLC-3696 Followup increasing test coverage on Cacheable

--- spec/lib/cache/cacheable_spec.rb
+++ spec/lib/cache/cacheable_spec.rb
@@ -9,8 +9,9 @@ describe Cache::Cacheable do
     end
   end
 
-  context "with nil values" do
-    let(:randkey) { rand(99999).to_s }
+  let(:randkey) { rand(99999).to_s }
+
+  context 'with nil values' do
     describe '#fetch_from_cache' do
       before do
         expect(Rails.cache).to receive(:write).once.with(randkey, NilClass, anything).and_call_original
@@ -29,4 +30,31 @@ describe Cache::Cacheable do
     end
   end
 
+  context 'smart fetch' do
+    context 'with normal hash values' do
+      let(:normal_hash) { {foo: 'bar'} }
+      context 'when no exceptions occur' do
+        before do
+          expect(Rails.cache).to receive(:read).twice.with(randkey).and_call_original
+          expect(Rails.cache).to receive(:write).once.with(
+                                   randkey,
+                                   normal_hash,
+                                   {expires_in: Settings.cache.expiration.default, force: true}
+                                 ).and_call_original
+        end
+        it 'caches a hash' do
+          first_val = TestCacheable.smart_fetch_from_cache({id: randkey}) do
+            normal_hash
+          end
+          expect(first_val).to eq normal_hash
+          second_val= TestCacheable.smart_fetch_from_cache({id: randkey}) do
+            normal_hash
+          end
+          expect(second_val).to eq normal_hash
+        end
+      end
+    end
+  end
+
 end
+
