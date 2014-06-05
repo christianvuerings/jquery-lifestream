fbfd265ee902a93e288075e100bde515da35fc2a
Christian Vuerings <vueringschristian@gmail.com>
2014-06-05 11:39:14 -0700

    CLC-3812 - gpa_units -> gpaUnits

--- app/models/my_academics/gpa_units.rb
+++ app/models/my_academics/gpa_units.rb
@@ -6,7 +6,7 @@ module MyAcademics
       student_info = CampusOracle::Queries.get_student_info(@uid) || {}
       return data if student_info.nil?
 
-      data[:gpa_units] = {
+      data[:gpaUnits] = {
         cumulative_gpa: student_info["cum_gpa"].nil? ? nil: student_info["cum_gpa"].to_f,
         total_units: student_info["tot_units"].nil? ? nil : student_info["tot_units"].to_f
       }
