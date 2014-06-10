f2deb3e39ddb7fb186902946e6676a3566aa7dde
Jason Miller <jason@redconfetti.com>
2014-06-09 18:45:17 -0700

    CLC-3399 - Refactored CanvasRostersController to use Pundit authorization

--- app/controllers/canvas_rosters_controller.rb
+++ app/controllers/canvas_rosters_controller.rb
@@ -1,17 +1,22 @@
 class CanvasRostersController < RostersController
   include ClassLogger
 
-  # GET /api/academics/rosters/canvas/:canvas_course_id
+  before_filter :api_authenticate
+  before_filter :authorize_viewing_rosters
+  rescue_from StandardError, with: :handle_api_exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def authorize_viewing_rosters
+    raise Pundit::NotAuthorizedError, "Canvas Course ID not present in embedded rosters request: session user = #{session[:user_id]}" if session[:canvas_course_id].blank?
+    canvas_course = Canvas::Course.new(:user_id => session[:user_id], :canvas_course_id => session[:canvas_course_id].to_i)
+    authorize canvas_course, :is_canvas_course_teacher_or_assistant?
+  end
+
+  # GET /api/academics/rosters/canvas/embedded
   def get_feed
-    if (model = valid_model(params[:canvas_course_id], "Canvas"))
-      if (feed = model.get_feed)
-        render :json => feed.to_json
-      else
-        render :nothing => true, :status => 401
-      end
-    else
-      render :nothing => true, :status => 401
-    end
+    proxy = Canvas::CanvasRosters.new(session[:user_id], course_id: session[:canvas_course_id].to_i)
+    feed = proxy.get_feed
+    render :json => feed.to_json
   end
 
 end
