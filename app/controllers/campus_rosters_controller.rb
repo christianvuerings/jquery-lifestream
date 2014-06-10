256ac64bf7226b691c70ab04a52ec5c265d71d5d
Jason Miller <jason@redconfetti.com>
2014-06-09 18:53:35 -0700

    CLC-3399 - Refactored CampusRostersController to use Pundit authorization

--- app/controllers/campus_rosters_controller.rb
+++ app/controllers/campus_rosters_controller.rb
@@ -1,6 +1,17 @@
 class CampusRostersController < RostersController
   include ClassLogger
 
+  before_filter :api_authenticate
+  before_filter :authorize_viewing_rosters
+  rescue_from StandardError, with: :handle_api_exception
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def authorize_viewing_rosters
+    # raise Pundit::NotAuthorizedError, "Canvas Course ID not present in embedded rosters request: session user = #{session[:user_id]}" if session[:canvas_course_id].blank?
+    # canvas_course = Canvas::Course.new(:user_id => session[:user_id], :canvas_course_id => session[:canvas_course_id].to_i)
+    # authorize canvas_course, :is_canvas_course_teacher_or_assistant?
+  end
+
   # GET /api/academics/rosters/campus/:campus_course_id
   def get_feed
     if (model = valid_model(params[:campus_course_id], "Campus"))
--- spec/controllers/campus_rosters_controller_spec.rb
+++ spec/controllers/campus_rosters_controller_spec.rb
@@ -1,18 +1,33 @@
 require "spec_helper"
+require "support/canvas_shared_examples"
 
 describe CampusRostersController do
 
-  it "should return error for campus rosters if user is not authorized" do
-    user_id = rand(99999)
-    session[:user_id] = user_id
-    campus_course_id = "mec_eng-132-2014-b"
+  let(:campus_course_id) { "mec_eng-132-2014-b" }
+  let(:user_id) { rand(99999) }
 
-    Rosters::Campus.any_instance.stub(:get_feed).and_return(nil)
-    get :get_feed, campus_course_id: campus_course_id
-    assert_response(401)
-    student_id = rand(99999)
-    get :photo, campus_course_id: campus_course_id, person_id: student_id
-    assert_response(401)
+  context "when serving course rosters feed" do
+
+    before { session[:user_id] = user_id }
+
+    it_should_behave_like "an api endpoint" do
+      before { allow_any_instance_of(Rosters::Campus).to receive(:get_feed).and_raise(RuntimeError, "Something went wrong") }
+      let(:make_request) { get :get_feed, campus_course_id: campus_course_id }
+    end
+
+    it_should_behave_like "a user authenticated api endpoint" do
+      let(:make_request) { get :get_feed, campus_course_id: campus_course_id }
+    end
+
+    it "should return error for campus rosters if user is not authorized" do
+      Rosters::Campus.any_instance.stub(:get_feed).and_return(nil)
+      get :get_feed, campus_course_id: campus_course_id
+      assert_response(401)
+      student_id = rand(99999)
+      get :photo, campus_course_id: campus_course_id, person_id: student_id
+      assert_response(401)
+    end
+
   end
 
 end
