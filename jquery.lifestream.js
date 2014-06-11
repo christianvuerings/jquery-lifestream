39e425265f02d5145602efaf2a49544cd60cd011
Christian Vuerings <vueringschristian@gmail.com>
2014-06-10 19:59:12 -0700

    Fix stackoverflow feed

--- jquery.lifestream.js
+++ jquery.lifestream.js
@@ -2172,23 +2172,13 @@ $.fn.lifestream.feeds.stackoverflow = function( config, callback ) {
     question_link = "http://stackoverflow.com/questions/";
 
     if(item.timeline_type === "badge") {
-      text = "was " + item.action + " the '" + item.description + "' badge";
-      title = item.detail;
       link = stackoverflow_link + "?tab=reputation";
     }
-    else if (item.timeline_type === "comment") {
-       text = "commented on";
-       title = item.description;
-       link = question_link + item.post_id;
-    }
-    else if (item.timeline_type === "revision" ||
-        item.timeline_type === "accepted" ||
-        item.timeline_type === "askoranswered") {
-      text = (item.timeline_type === 'askoranswered' ?
-             item.action : item.action + ' ' + item.post_type);
-      title = item.detail || item.description || "";
-      link = question_link + item.post_id;
-    }
+
+    text = item.timeline_type;
+    title = item.title || item.detail || "";
+    link = link || question_link + item.post_id;
+
     return {
       link: link,
       title: title,
@@ -2200,17 +2190,16 @@ $.fn.lifestream.feeds.stackoverflow = function( config, callback ) {
   };
 
   $.ajax({
-    url: "https://api.stackoverflow.com/1.1/users/" + config.user +
-      "/timeline?jsonp",
+    url: "https://api.stackexchange.com/2.1/users/" + config.user +
+      "/timeline?site=stackoverflow",
     dataType: "jsonp",
     jsonp: 'jsonp',
     success: function( data ) {
-      var output = [], i = 0, j;
+      var output = [];
 
-      if(data && data.total && data.total > 0 && data.user_timelines) {
-        j = data.user_timelines.length;
-        for( ; i<j; i++) {
-          var item = data.user_timelines[i];
+      if(data && data.items) {
+        for(var i = 0 ; i < data.items.length; i++) {
+          var item = data.items[i];
           output.push({
             date: convertDate(item.creation_date),
             config: config,
