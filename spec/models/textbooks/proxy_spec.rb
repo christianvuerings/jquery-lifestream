6649d2d243345d217387d2d9f861c7e216f382bb
Ray Davis <ray@media.berkeley.edu>
2014-06-20 12:34:30 -0700

    CLC-3783 New Cal store logo; protect tests against reordered pages

--- spec/models/textbooks/proxy_spec.rb
+++ spec/models/textbooks/proxy_spec.rb
@@ -17,78 +17,84 @@ describe Textbooks::Proxy do
   end
 
   describe '#get' do
-    subject { Textbooks::Proxy.new({ccns: ccns, slug: slug}).get }
+    describe 'live testext tests enabled for order-independent expectations' do
+      subject { Textbooks::Proxy.new({ccns: ccns, slug: slug}).get }
 
-    context 'valid CCN and term slug' do
-      let(:ccns) { ['26262'] }
-      let(:slug) {'fall-2014'}
-      it 'produces the expected textbook feed' do
-        it_is_a_normal_server_response
-        it_has_at_least_one_title
-        book_list = subject[:books][:bookDetails][0]
-        expect(book_list[:type]).to eq 'Required'
-        first_book = book_list[:books][0]
-        [:isbn, :image, :edition, :publisher, :amazonLink, :cheggLink, :oskicatLink, :googlebookLink].each do |key|
-          expect(first_book[key]).to be_present
+      context 'valid CCN and term slug' do
+        let(:ccns) { ['26262'] }
+        let(:slug) {'fall-2014'}
+        it 'produces the expected textbook feed' do
+          it_is_a_normal_server_response
+          it_has_at_least_one_title
+          book_list = subject[:books][:bookDetails][0]
+          expect(book_list[:type]).to eq 'Required'
+          first_book = book_list[:books][0]
+          [:isbn, :image, :edition, :publisher, :amazonLink, :cheggLink, :oskicatLink, :googlebookLink].each do |key|
+            expect(first_book[key]).to be_present
+          end
+          expect(first_book[:image]).to_not match /http:/
         end
-        expect(first_book[:image]).to_not match /http:/
+      end
+
+      context 'an unknown CCN' do
+        let(:ccns) { ['09259'] }
+        let(:slug) {'fall-2014'}
+        it 'returns a helpful message' do
+          it_is_a_normal_server_response
+          feed = subject[:books]
+          expect(feed[:hasBooks]).to be_false
+          expect(feed[:bookUnavailableError]).to eq 'Textbook information for this course could not be found.'
+        end
+      end
+
+      context 'an unknown term code' do
+        let(:ccns) { ['26262'] }
+        let(:slug) {'fall-2074'}
+        it 'returns a helpful message' do
+          it_is_a_normal_server_response
+          feed = subject[:books]
+          expect(feed[:hasBooks]).to be_false
+          expect(feed[:bookUnavailableError]).to eq 'Textbook information for this term could not be found.'
+        end
+      end
+
+      context 'multiple CCNs, only one of which has books' do
+        let(:ccns) { ['09259', '26262'] }
+        let(:slug) {'fall-2014'}
+        it 'finds the one with books' do
+          it_is_a_normal_server_response
+          it_has_at_least_one_title
+        end
       end
     end
 
-    context 'multiple CCNs, only one of which has books' do
-      let(:ccns) { ['09259', '26262'] }
-      let(:slug) {'fall-2014'}
-      it 'finds the one with books' do
-        it_is_a_normal_server_response
-        it_has_at_least_one_title
-      end
-    end
+    describe 'order-dependent tests work from recorded data' do
+      subject { Textbooks::Proxy.new({ccns: ccns, slug: slug, fake: true}).get }
 
-    context 'an unknown CCN' do
-      let(:ccns) { ['09259'] }
-      let(:slug) {'fall-2014'}
-      it 'returns a helpful message' do
-        it_is_a_normal_server_response
-        feed = subject[:books]
-        expect(feed[:hasBooks]).to be_false
-        expect(feed[:bookUnavailableError]).to eq 'Textbook information for this course could not be found.'
+      context 'a required text with no ISBN' do
+        let(:ccns) { ['62120'] }
+        let(:slug) {'summer-2014'}
+        it 'provides a bookstore link to get the non-ISBN text' do
+          it_is_a_normal_server_response
+          it_has_at_least_one_title
+          necronomicon = subject[:books][:bookDetails][0][:books][1]
+          expect(necronomicon[:publisher]).to eq 'UNIVERSITY CUSTOM PUBLISHING'
+          expect(necronomicon[:isbn]).to be_nil
+          expect(necronomicon[:bookstoreLink]).to be_present
+        end
       end
-    end
 
-    context 'an unknown term code' do
-      let(:ccns) { ['26262'] }
-      let(:slug) {'fall-2074'}
-      it 'returns a helpful message' do
-        it_is_a_normal_server_response
-        feed = subject[:books]
-        expect(feed[:hasBooks]).to be_false
-        expect(feed[:bookUnavailableError]).to eq 'Textbook information for this term could not be found.'
-      end
-    end
-
-    context 'a required text with no ISBN' do
-      let(:ccns) { ['62120'] }
-      let(:slug) {'summer-2014'}
-      it 'provides a bookstore link to get the non-ISBN text' do
-        it_is_a_normal_server_response
-        it_has_at_least_one_title
-        necronomicon = subject[:books][:bookDetails][0][:books][1]
-        expect(necronomicon[:publisher]).to eq 'UNIVERSITY CUSTOM PUBLISHING'
-        expect(necronomicon[:isbn]).to be_nil
-        expect(necronomicon[:bookstoreLink]).to be_present
-      end
-    end
-
-    context 'a choice of required texts' do
-      let(:ccns) { ['53798'] }
-      let(:slug) {'fall-2014'}
-      it 'provides a bookstore link to get the title with choices' do
-        it_is_a_normal_server_response
-        it_has_at_least_one_title
-        choices = subject[:books][:bookDetails][0][:books][1]
-        expect(choices[:hasChoices]).to be_true
-        expect(choices[:title]).to be_present
-        expect(choices[:bookstoreLink]).to be_present
+      context 'a choice of required texts' do
+        let(:ccns) { ['53798'] }
+        let(:slug) {'fall-2014'}
+        it 'provides a bookstore link to get the title with choices' do
+          it_is_a_normal_server_response
+          it_has_at_least_one_title
+          choices = subject[:books][:bookDetails][0][:books][1]
+          expect(choices[:hasChoices]).to be_true
+          expect(choices[:title]).to be_present
+          expect(choices[:bookstoreLink]).to be_present
+        end
       end
     end
 
