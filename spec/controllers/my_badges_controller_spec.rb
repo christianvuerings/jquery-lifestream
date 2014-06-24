fbd47ecdca8fbd1788f1c83396c410de919e2894
Ray Davis <ray@media.berkeley.edu>
2014-06-24 15:54:12 -0700

    CLC-3811 Add view-as censoring tests for MyBadges and MyTasks

--- spec/controllers/my_badges_controller_spec.rb
+++ spec/controllers/my_badges_controller_spec.rb
@@ -40,7 +40,34 @@ describe MyBadgesController do
     json_response["studentInfo"].present?.should be_true
     json_response["studentInfo"].is_a?(Hash).should be_true
     json_response["studentInfo"].keys.count.should == 3
+  end
 
+  context 'viewing-as' do
+    let(:user_id) { rand(99999).to_s }
+    let(:original_user_id) { rand(99999).to_s }
+    before do
+      session[:user_id] = user_id
+      expect(Settings.google_proxy).to receive(:fake).at_least(:once).and_return(true)
+      expect(Settings.app_alerts_proxy).to receive(:fake).at_least(:once).and_return(true)
+      expect(Settings.bearfacts_proxy).to receive(:fake).at_least(:once).and_return(true)
+    end
+    it 'should not return Google data from a cached real-user feed' do
+      get :get_feed
+      feed = JSON.parse(response.body)
+      expect(feed['alert']['title']).to be_present
+      expect(feed['studentInfo']['regBlock']).to be_present
+      ['bcal', 'bdrive', 'bmail'].each do |service|
+        expect(feed['badges'][service]['count']).to be > 0
+      end
+      session[:original_user_id] = original_user_id
+      get :get_feed
+      feed = JSON.parse(response.body)
+      expect(feed['alert']['title']).to be_present
+      expect(feed['studentInfo']['regBlock']).to be_present
+      ['bcal', 'bdrive', 'bmail'].each do |service|
+        expect(feed['badges'][service]['count']).to eq 0
+      end
+    end
   end
 
 end
