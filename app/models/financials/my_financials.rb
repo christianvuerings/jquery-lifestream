21faa1b5a80190ec0077f14817910ae3c6d05656
Ray Davis <ray@media.berkeley.edu>
2014-07-17 15:11:13 -0700

    CLC-3960 Only cache a single copy of the Financials feed

--- app/models/financials/my_financials.rb
+++ app/models/financials/my_financials.rb
@@ -1,28 +1,100 @@
 module Financials
   # This model class caches a JSON translation of CFV data fetched by Financials::Proxy.
   # Inheriting from UserSpecificModel means it participates in the live-updates cache
-  # invalidation and warmup cycle. At the model layer the cache entries persist forever
-  # until a live-updates cycle hits them (see LiveUpdatesWarmer and IsUpdatedController).
-  # At the proxy layer cache entries have shorter expiration times, tuned to the external
-  # data's update frequency.
+  # invalidation and warmup cycle.
   class MyFinancials < UserSpecificModel
+    include Cache::LiveUpdatesEnabled, Cache::UserCacheExpiry, SafeJsonParser
+    include User::Student
 
-    include Cache::LiveUpdatesEnabled
+    # Due to the potential size of finance data, the JSON version of the feed is not cached separately.
+    def self.caches_json?
+      false
+    end
+
+    def warm_cache
+      get_feed(false)
+    end
+
+    # Calling to_json on a large finances structure can take a very long time, and so
+    # a JSON-fied version of the feed is cached and returned instead.
+    def get_feed(force_cache_write=false)
+      # smart_fetch_from_cache provides helpful services like special cache handling for exceptions.
+      self.class.smart_fetch_from_cache({
+        force_write: force_cache_write,
+        id: @uid,
+        user_message_on_exception: user_message_on_exception
+      }) do
+        feed = get_feed_internal
+        status_code = feed[:statusCode]
+        feed_json = feed.to_json
+        if (status_code >= 400) && (status_code != 404)
+          raise Errors::ProxyError.new("Connection failed for UID #{@uid}: #{status_code} #{feed[:body]}", feed_json)
+        end
+        feed_json
+      end
+    end
 
     def get_feed_internal
-      feed = {}
-      proxy = Financials::Proxy.new({user_id: @uid})
-      proxy_response = proxy.get
-      if proxy_response && body = proxy_response[:body]
-        if body.is_a?(Hash) && student = body["student"]
-          feed.merge!(student)
-          feed.merge!({"currentTerm" => Berkeley::Terms.fetch.current.to_english})
-          feed.merge!({"apiVersion" => proxy_response[:apiVersion]})
-        else
-          feed.merge!(proxy_response)
-        end
+      student_id = lookup_student_id
+      if student_id.blank?
+        # don't continue if student id can't be found.
+        logger.info "Lookup of student_id for uid #@uid failed, cannot call CFV API"
+        feed = no_billing_data_response
+      else
+        response = Financials::Proxy.new(user_id: @uid, student_id: student_id).get
+        feed = parse_response(response)
       end
-      feed
+      feed.merge(feed_metadata(feed, instance_key))
+    end
+
+    def parse_response(response)
+      if response.code < 400
+        body = safe_json(response.body)
+        if body && (student = body['student'])
+          feed = {
+            apiVersion: api_version(response),
+            currentTerm: Berkeley::Terms.fetch.current.to_english,
+            statusCode: response.code
+          }
+          feed.merge!(student)
+        else
+          logger.debug("Response missing student data for UID #{@uid}: status = #{response.code}, body = #{response.body}")
+          {
+            body: body,
+            statusCode: response.code
+          }
+        end
+      elsif response.code == 404
+        no_billing_data_response
+      else
+        {
+          body: user_message_on_exception,
+          statusCode: response.code
+        }
+      end
+    end
+
+    def api_version(response)
+      # CFV API sends its version number in an HTTP header. We can use this on the front end to deal with
+      # version differences between deployment environments.
+      # CFV chose to use underscores in the HTTP header name, but VCR (and some other projects) automatically
+      # convert underscores in a header name to hyphens. We therefore need to check using both.
+      if response.headers && ((version = response.headers['x_cfv_api_version']) || (version = response.headers['x-cfv-api-version']))
+        version.is_a?(Array) ? version[0] : version
+      else
+        nil
+      end
+    end
+
+    def no_billing_data_response
+      {
+        body: "You are seeing this message because CalCentral does not have CARS billing data for your account. If you are a new student, your account may not have been assessed charges yet. Please try again later. Current or former students should contact us for further assistance using the Feedback link below.",
+        statusCode: 404
+      }
+    end
+
+    def user_message_on_exception
+      'My Finances is currently unavailable. Please try again later.'
     end
 
   end
--- app/models/financials/proxy.rb
+++ app/models/financials/proxy.rb
@@ -1,87 +1,32 @@
 module Financials
   # This Proxy class gets data from the external CFV HTTP service.
   class Proxy < BaseProxy
-
-    include ClassLogger, SafeJsonParser
-    include Cache::UserCacheExpiry
-    include User::Student
+    include ClassLogger
 
     # APP_ID must be unique within the system, and is used by VCR and the front-end in various ways.
     APP_ID = 'CFV'
 
     def initialize(options = {})
       super(Settings.financials_proxy, options)
+      @student_id = options[:student_id]
     end
 
     def get
-      # smart_fetch_from_cache provides helpful services like writing only successful entries to the cache.
-      self.class.smart_fetch_from_cache({id: @uid, user_message_on_exception: 'My Finances is currently unavailable. Please try again later.'}) do
-        request_internal('financials')
-      end
-    end
+      url = "#{Settings.financials_proxy.base_url}/student/#{@student_id}"
+      logger.info "Fake = #@fake; Making request to #{url} on behalf of user #{@uid}; cache expiration #{self.class.expires_in}"
 
-    private
-
-    def request_internal(vcr_cassette)
-      # not all external data sources need a student id, but this one does.
-      student_id = lookup_student_id
-
-      if student_id.blank?
-        # don't continue if student id can't be found.
-        logger.info "Lookup of student_id for uid #@uid failed, cannot call CFV API"
-        {
-          body: "You are seeing this message because CalCentral does not have CARS billing data for your account. If you are a new student, your account may not have been assessed charges yet. Please try again later. Current or former students should contact us for further assistance using the Feedback link below.",
-          statusCode: 400
-        }
-      else
-        url = "#{Settings.financials_proxy.base_url}/student/#{student_id}"
-        logger.info "Fake = #@fake; Making request to #{url} on behalf of user #{@uid}, student_id = #{student_id}; cache expiration #{self.class.expires_in}"
-
-        # HTTParty is our preferred HTTP library. FakeableProxy provides the (deprecated) VCR response recording system.
-        response = ActiveSupport::Notifications.instrument('proxy', { url: url, class: self.class }) do
-          FakeableProxy.wrap_request(APP_ID + "_" + vcr_cassette, @fake, {match_requests_on: [:method, :path]}) {
-            HTTParty.get(
-              url,
-              digest_auth: {username: Settings.financials_proxy.username, password: Settings.financials_proxy.password},
-              timeout: Settings.application.outgoing_http_timeout
-            )
-          }
-        end
-
-        # handle errors that we expect, like 404s and 500s, with a helpful error message.
-        # raising a ProxyError will make smart_fetch_from_cache skip writing to the cache.
-        # if an error might heal itself the next time the proxy is called, that's what you
-        # want to do. Otherwise, if you want to cache error states for a long time, just return
-        # the error in the proxy body.
-        if response.code == 404
-          logger.debug "Connection failed: #{response.code} #{response.body}; url = #{url}"
-          body = "You are seeing this message because CalCentral does not have CARS billing data for your account. If you are a new student, your account may not have been assessed charges yet. Please try again later. Current or former students should contact us for further assistance using the Feedback link below."
-        elsif response.code >= 400
-          body = 'My Finances is currently unavailable. Please try again later.'
-          raise Errors::ProxyError.new("Connection failed: #{response.code} #{response.body}; url = #{url}", {
-            body: body,
-            statusCode: response.code
-          })
-        else
-          body = safe_json(response.body)
-        end
-
-        # CFV API sends its version number in an HTTP header. We use this on the front end in various ways.
-        # VCR mangles the x_cfv_api_version header to x-cfv-api-version when replaying in fake mode.
-        api_version = nil
-        version_header = (@fake ? "x-cfv-api-version" : "x_cfv_api_version")
-        if response.headers && response.headers[version_header]
-          version = response.headers[version_header]
-          api_version = (version.is_a?(Array) ? version[0] : version)
-        end
-
-        logger.debug "Remote server status #{response.code}; url = #{url}"
-        {
-          body: body,
-          statusCode: response.code,
-          apiVersion: api_version
+      # HTTParty is our preferred HTTP library. FakeableProxy provides the (deprecated) VCR response recording system.
+      response = ActiveSupport::Notifications.instrument('proxy', { url: url, class: self.class }) do
+        FakeableProxy.wrap_request(APP_ID + "_financials", @fake, {match_requests_on: [:method, :path]}) {
+          HTTParty.get(
+            url,
+            digest_auth: {username: Settings.financials_proxy.username, password: Settings.financials_proxy.password},
+            timeout: Settings.application.outgoing_http_timeout
+          )
         }
       end
+      response
     end
+
   end
 end
