015594cfc1c1774e503276aa8f5eca91810ddf15
Chris Tweney <chris@media.berkeley.edu>
2014-08-06 10:50:31 -0700

    CLC-4039 Still more environmental needs for cron

--- script/calendar.sh
+++ script/calendar.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Script to run student class calendar export. This is intended to be run from cron.
 
+# Make sure the normal shell environment is in place, since it may not be
+# when running as a cron job.
+source "$HOME/.bash_profile"
+
 cd $( dirname "${BASH_SOURCE[0]}" )/..
 
 LOG=`date +"$PWD/log/calendar_%Y-%m-%d.log"`
@@ -10,12 +14,15 @@ LOGIT="tee -a $LOG"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 source .rvmrc
 
+export RAILS_ENV=production
+export LOGGER_STDOUT=only
+export LOGGER_LEVEL=INFO
+
 echo | $LOGIT
 echo "------------------------------------------" | $LOGIT
 echo "`date`: Calendar export started on app node: `hostname -s`..." | $LOGIT
 
 cd deploy
-export RAILS_ENV="production"
 bundle exec rake calendar:preprocess calendar:export | $LOGIT
 
 echo "`date`: Calendar export completed" | $LOGIT
