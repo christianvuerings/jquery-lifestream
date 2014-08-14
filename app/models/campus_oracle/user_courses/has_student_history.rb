cc66de5f84e876876c1b4d8f274c712bf3cbdc35
Chris Tweney <chris@media.berkeley.edu>
2014-08-14 12:48:01 -0700

    CLC-4083 Use uid as the cache key to participate in expire functionality

--- app/models/campus_oracle/user_courses/has_student_history.rb
+++ app/models/campus_oracle/user_courses/has_student_history.rb
@@ -5,7 +5,7 @@ module CampusOracle
       include Cache::UserCacheExpiry
 
       def has_student_history?
-        self.class.fetch_from_cache "has_student_history-#{@uid}" do
+        self.class.fetch_from_cache @uid do
           CampusOracle::Queries.has_student_history?(@uid, @academic_terms)
         end
       end
