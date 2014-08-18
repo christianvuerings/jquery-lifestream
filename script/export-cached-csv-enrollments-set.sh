abb7ef12611cc7cda4baccd357672a18044e0970
Jason Miller <jason@redconfetti.com>
2014-08-18 09:35:40 -0700

    CLC-4091 - Added cmdline script to run Canvas cached enrollments CSV export script via Rake task

---
+++ script/export-cached-csv-enrollments-set.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script to update all users within Canvas from People/Guest Oracle view
+
+# Make sure the normal shell environment is in place, since it may not be
+# when running as a cron job.
+source "$HOME/.bash_profile"
+
+cd $( dirname "${BASH_SOURCE[0]}" )/..
+
+LOG=`date +"$PWD/log/canvas_cached_enrollments_csv_%Y-%m-%d.log"`
+LOGIT="tee -a $LOG"
+
+# Enable rvm and use the correct Ruby version and gem set.
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+source .rvmrc
+
+export RAILS_ENV=production
+export LOGGER_STDOUT=only
+export LOGGER_LEVEL=INFO
+export JRUBY_OPTS="-Xcext.enabled=true -J-client -X-C"
+
+echo | $LOGIT
+echo "------------------------------------------" | $LOGIT
+echo "`date`: About to run the Canvas cached enrollments CSV export script..." | $LOGIT
+
+cd deploy
+
+bundle exec rake canvas:export_enrollments_to_csv_set | $LOGIT
