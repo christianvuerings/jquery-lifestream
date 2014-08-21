8b0713655f15f7b38720000652bd57f7b650efbc
Chris Tweney <chris@media.berkeley.edu>
2014-08-21 16:05:51 -0700

    CLC-2816 Calculate estimated cumulative GPA based on official cumulative GPA data

--- app/models/my_academics/gpa_units.rb
+++ app/models/my_academics/gpa_units.rb
@@ -7,8 +7,9 @@ module MyAcademics
       return data if student_info.nil?
 
       data[:gpaUnits] = {
-        cumulativeGpa: student_info["cum_gpa"].nil? ? nil: student_info["cum_gpa"].to_f,
-        totalUnits: student_info["tot_units"].nil? ? nil : student_info["tot_units"].to_f
+        cumulativeGpa: student_info['cum_gpa'].nil? ? nil: student_info['cum_gpa'].to_f,
+        totalUnits: student_info['tot_units'].nil? ? nil : student_info['tot_units'].to_f,
+        totalUnitsAttempted: student_info['lgr_tot_attempt_unit'].nil? ? nil : student_info['lgr_tot_attempt_unit'].to_f
       }
     end
   end
