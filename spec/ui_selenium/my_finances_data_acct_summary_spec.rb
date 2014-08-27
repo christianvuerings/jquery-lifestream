5940ca65e8a1d9356ea589f06b7a3f55433e36c0
Paul Farestveit <pfarestveit@berkeley.edu>
2014-08-27 14:10:55 -0700

    CLC-4166: remove unhappy My Fin data sanity test

--- spec/ui_selenium/my_finances_data_acct_summary_spec.rb
+++ spec/ui_selenium/my_finances_data_acct_summary_spec.rb
@@ -72,10 +72,6 @@ describe 'My Finances', :testui => true do
               BigDecimal.new(fin_api_page.account_balance_str).should eql(BigDecimal.new(fin_api_page.min_amt_due_str) +
                 BigDecimal.new(fin_api_page.future_activity_str))
             end
-            it 'shows a minimum amount due now that is equal to the sum of the current balance plus the past due amount for UID ' + uid do
-              BigDecimal.new(fin_api_page.min_amt_due_str).should eql(BigDecimal.new(fin_api_page.total_current_balance_str) +
-                BigDecimal.new(fin_api_page.past_due_amt_str))
-            end
             if fin_api_page.account_balance >= 0
               it 'shows an account balance that is greater than or equal to the minimum amount due now for UID ' + uid do
                 fin_api_page.account_balance.should be >= fin_api_page.min_amt_due
