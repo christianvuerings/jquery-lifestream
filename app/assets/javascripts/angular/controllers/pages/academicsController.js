5197d3402a7eef5b8a1f73ea197ef5dc66fd4c6d
Joel Graycar <jgraycar@berkeley.edu>
2014-09-22 15:33:48 -0700

    fixed typo bug

--- app/assets/javascripts/angular/controllers/pages/academicsController.js
+++ app/assets/javascripts/angular/controllers/pages/academicsController.js
@@ -249,7 +249,7 @@
       }
 
       $scope.isLSStudent = isLSStudent($scope.collegeAndLevel);
-      $scope.isLawStudent = isLawStudent($scope.collegeAndlevel);
+      $scope.isLawStudent = isLawStudent($scope.collegeAndLevel);
       $scope.isUndergraduate = ($scope.collegeAndLevel && $scope.collegeAndLevel.standing === 'Undergraduate');
 
       $scope.teaching = parseTeaching(data.teachingSemesters);
