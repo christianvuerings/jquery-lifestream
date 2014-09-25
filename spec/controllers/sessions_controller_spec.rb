cb927dc1a4f17b09d90c22fc721ce54d637e5652
John Crossman <johncrossman@berkeley.edu>
2014-09-25 12:04:18 -0700

    CLC-4140, tests on re-authenticating in SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,23 +2,38 @@ require "spec_helper"
 
 describe SessionsController do
   let(:user_id) { random_id }
-  before do
+  let(:cookie_hash) { {} }
+
+  before(:each) do
     session[:user_id] = user_id
   end
 
-  context 'when re-authenticating via view-as' do
-    it 'CAS uid does not match original user uid' do
-      allow(controller).to receive(:cookies).and_return({reauthenticated: nil})
-      :create_reauth_cookie
-      session[:original_user_id] = user_id
-      session["arbitrary"] = user_id
-
-      @request.env["omniauth.auth"] = { uid: "some_other_#{user_id}" }
-      get :lookup, renew: "true"
-
-      @response.status.should eq(302)
-      cookies[:reauthenticated].should be_nil
-      session.empty?.should be_true
+  describe 're-authenticating' do
+    context 're-authenticating via view-as' do
+      it 'logout user when CAS uid does not match original user uid' do
+        # cookie_hash = {}
+        expect(controller).to receive(:cookies).and_return(cookie_hash)
+        :create_reauth_cookie
+        session[:original_user_id] = user_id
+        @request.env["omniauth.auth"] = { uid: "some_other_#{user_id}" }
+        get :lookup, renew: "true"
+        @response.status.should eq(302)
+        cookies[:reauthenticated].should be_nil
+        session.empty?.should be_true
+      end
+      it 'reset session when CAS uid does not match uid in session' do
+        expect(controller).to receive(:cookies).and_return(cookie_hash)
+        :create_reauth_cookie
+        session[:original_user_id] = user_id
+        @request.env['omniauth.auth'] = { 'uid' => user_id }
+        get :lookup, renew: 'true'
+        @response.status.should eq(302)
+        reauth_cookie_value = cookie_hash[:reauthenticated]
+        reauth_cookie_value.should_not be_nil
+        reauth_cookie_value[:value].should be_true
+        session.empty?.should be_false
+        session[:user_id].should eql(user_id)
+      end
     end
   end
 
