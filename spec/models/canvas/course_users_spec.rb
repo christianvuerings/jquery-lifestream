799ec8b7f29975a5a2f26e67f7595182d6873946
Jason Miller <jason@redconfetti.com>
2014-10-07 17:54:54 -0700

    CLC-4321 - Removed Canvas::CourseUsers#course_grades_csv

--- spec/models/canvas/course_users_spec.rb
+++ spec/models/canvas/course_users_spec.rb
@@ -22,6 +22,11 @@ describe Canvas::CourseUsers do
     expect(users[0]['enrollments'][0]['id']).to eq 20187382
     expect(users[0]['enrollments'][0]['type']).to eq "StudentEnrollment"
     expect(users[0]['enrollments'][0]['role']).to eq "StudentEnrollment"
+    expect(users[0]['enrollments'][0]['grades']).to be_an_instance_of Hash
+    expect(users[0]['enrollments'][0]['grades']['current_score']).to eq 34.9
+    expect(users[0]['enrollments'][0]['grades']['final_score']).to eq 34.9
+    expect(users[0]['enrollments'][0]['grades']['current_grade']).to eq "F"
+    expect(users[0]['enrollments'][0]['grades']['final_grade']).to eq "F"
   end
 
   it "uses cache by default" do
@@ -36,33 +41,6 @@ describe Canvas::CourseUsers do
     users = subject.course_users(:cache => false)
     expect(users).to be_an_instance_of Array
     expect(users.count).to eq 6
-  end
-
-  context "when providing course user grades" do
-    it "returns information relevant to egrades csv export" do
-      course_user_grades_csv_string = subject.course_grades_csv
-      expect(course_user_grades_csv_string).to be_an_instance_of String
-      course_user_grades_csv = CSV.parse(course_user_grades_csv_string, {headers: true})
-      expect(course_user_grades_csv.count).to eq 6
-      course_user_grades_csv.each do |user_grade|
-        expect(user_grade).to be_an_instance_of CSV::Row
-        expect(user_grade['uid']).to be_an_instance_of String
-        expect(user_grade['grade']).to be_an_instance_of String
-        expect(user_grade['comment']).to be_an_instance_of String
-      end
-      expect(course_user_grades_csv[0]['uid']).to eq "4000123"
-      expect(course_user_grades_csv[0]['grade']).to eq "34.9"
-      expect(course_user_grades_csv[0]['comment']).to eq ""
-
-      expect(course_user_grades_csv[5]['uid']).to eq "4000272"
-      expect(course_user_grades_csv[5]['grade']).to eq "10.5"
-      expect(course_user_grades_csv[5]['comment']).to eq ""
-    end
-
-    it "should not source data from cache" do
-      expect(Canvas::CourseUsers).to_not receive(:fetch_from_cache)
-      course_user_grades_csv = subject.course_grades_csv
-    end
   end
 
 end
