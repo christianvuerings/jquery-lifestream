8a527307892b49151cf3f5b6f3f19d6b358dbbf3
Chris Tweney <chris@media.berkeley.edu>
2014-11-06 16:41:04 -0800

    CLC-4410 Handle batch requests in slices of 500

--- app/models/google_apps/batch.rb
+++ app/models/google_apps/batch.rb
@@ -9,37 +9,39 @@ module GoogleApps
     end
 
     def run_batch
-      # TODO deal with batches more than 1000 entries in a way that's transparent to users of this proxy.
       results = []
       return results if @requests.blank?
-
-      batch = Google::APIClient::BatchRequest.new
-
-      @requests.each do |request_params|
-        page_params = setup_page_params request_params
-        request_hash = GoogleApps::Client.generate_request_hash page_params
-        batch.add request_hash do |result|
-          logger.debug "Batch response status: #{result.response.status}"
-          results << result
-          if request_params[:callback].present?
-            request_params[:callback].call result
-          end
-        end
-      end
 
       client = GoogleApps::Client.client
       client.authorization = @authorization
 
-      # before running the batch, refresh the token because it may have expired.
-      # make sure the batch doesn't take more than an hour to process or the token
-      # will expire again during the run.
-      # see https://github.com/google/google-api-ruby-client/issues/167
-      client.authorization.fetch_access_token!
-      update_access_tokens!
+      @requests.each_slice(500) do |slice|
+        batch = Google::APIClient::BatchRequest.new
+        slice.each do |request_params|
+          page_params = setup_page_params request_params
+          request_hash = GoogleApps::Client.generate_request_hash page_params
+          batch.add request_hash do |result|
+            logger.debug "Batch response status: #{result.response.status}"
+            results << result
+            if request_params[:callback].present?
+              request_params[:callback].call result
+            end
+          end
+        end
 
+        # before running the batch, refresh the token because it may have expired.
+        # make sure the batch doesn't take more than an hour to process or the token
+        # will expire again during the run.
+        # see https://github.com/google/google-api-ruby-client/issues/167
+        client.authorization.fetch_access_token!
+        update_access_tokens!
+
+        client.execute batch
+      end
+
+      # empty out requests so we can be called again
       @requests = []
 
-      client.execute batch
       results
     end
   end
