f94b6056f699c748a01cf2caab11d5a12357a11d
Jason Miller <jason@redconfetti.com>
2014-11-19 12:50:42 -0800

    CLC-4492 - Configured boilerplate AngularJS controller/template for Manage Official Sections LTI app

---
+++ app/assets/javascripts/angular/controllers/pages/canvasCourseManageOfficialSectionsController.js
@@ -0,0 +1,11 @@
+/* jshint camelcase: false */
+(function(angular) {
+  'use strict';
+
+  /**
+   * Canvas Manage Official Sections LTI app controller
+   */
+  angular.module('calcentral.controllers').controller('CanvasCourseManageOfficialSectionsController', function(apiService) {
+    apiService.util.setTitle('Manage Official Sections');
+  });
+})(window.angular);
