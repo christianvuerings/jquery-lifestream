6fd884d071c216cfa791ec72d59db82e6ed9cabd
Ray Davis <ray@media.berkeley.edu>
2014-11-26 11:15:04 -0800

    CLC-3862 Stay at jscs 1.7.3 for now to avoid bug in 1.8.0 Needed to re-enable our Travis-CI jobs

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -1,5 +1,7 @@
 npm -g install jshint
-npm -g install jscs
+# Version 1.8.0 of jscs incorrectly flags indented parameter lists as
+# "Missing space before function parameter".
+npm -g install jscs@1.7.3
 gem install scss-lint
 scss-lint app/assets/stylesheets/
 [[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
