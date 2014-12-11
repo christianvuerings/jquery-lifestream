6b1e3d83dfa7c2c0f42c4aef8ef9f5a3743ae463
Ray Davis <ray@media.berkeley.edu>
2014-12-11 12:51:05 -0800

    CLC-4615 Disable check in new of scss-lint; control version

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -1,4 +1,4 @@
 npm install
-gem install scss-lint
+gem install scss-lint --version 0.31.0
 scss-lint app/assets/stylesheets/
 [[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
