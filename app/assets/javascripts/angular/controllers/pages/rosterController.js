855394c006504ccca5c648bb2258af86db46b1e8
Jason Miller <jason@redconfetti.com>
2015-01-23 15:18:14 -0800

    CLC-4719 - Refactored to use HTML with AngularJS directive logic to display proper header content

--- app/assets/javascripts/angular/controllers/pages/rosterController.js
+++ app/assets/javascripts/angular/controllers/pages/rosterController.js
@@ -10,20 +10,6 @@
       apiService.util.setTitle('Roster Photos');
     }
 
-    $scope.updateFilterDescription = function() {
-      var textSearch = '';
-      var sectionDesc = '';
-      if ($scope.search && $scope.search.$) {
-        textSearch = ' - Text Search: ' + $scope.search.$;
-      }
-      if ($scope.searchSection) {
-        sectionDesc = $scope.searchSection.name;
-      } else {
-        sectionDesc = 'All Sections';
-      }
-      $scope.filterDescription = sectionDesc + textSearch;
-    };
-
     $scope.studentInSectionFilter = function(student) {
       if (!$scope.searchSection) {
         return true;
@@ -40,7 +26,6 @@
         angular.extend($scope, data);
         $scope.course = $scope[$scope.context + '_course'];
         apiService.util.iframeUpdateHeight();
-        $scope.updateFilterDescription();
       }).error(function(data, status) {
         angular.extend($scope, data);
         $scope.errorStatus = status;
