9d21dc78fa4b91fcdc08137c6ee8ba6d66967570
Ray Davis <ray@media.berkeley.edu>
2015-01-27 09:05:04 -0800

    CLC-4762 Paginate Canvas Terms API

--- app/models/canvas/terms.rb
+++ app/models/canvas/terms.rb
@@ -5,13 +5,16 @@ module Canvas
     end
 
     def terms
-      response = self.class.fetch_from_cache do
-         request_uncached("accounts/#{settings.account_id}/terms", "_terms")
-      end
-      if response && response.status == 200 && terms_object = safe_json(response.body)
-        terms_object['enrollment_terms']
-      else
-        []
+      self.class.fetch_from_cache do
+        all_terms = []
+        params = 'per_page=100'
+        while params do
+          response = request_uncached("accounts/#{settings.account_id}/terms?#{params}", "_terms")
+          break unless (response && response.status == 200 && terms_list = safe_json(response.body))
+          all_terms.concat(terms_list['enrollment_terms'])
+          params = next_page_params(response)
+        end
+        all_terms
       end
     end
   end
