99123fa65d3cca5b2eaad909cb85fe2f5779df92
Christian Vuerings <vueringschristian@gmail.com>
2015-01-29 19:27:09 -0800

    CLC-4735 - Update the front-end test script to work with bamboo & travis

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -1,4 +1,8 @@
+node_version=`node --version`
+echo "Node version: $node_version"
+npm config set strict-ssl false
 npm install
+npm run build-production
 gem install scss-lint --version 0.31.0
-scss-lint app/assets/stylesheets/
+scss-lint src/assets/stylesheets/
 [[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
