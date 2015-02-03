9828408960e564f7be5ce2ef0154cf278bbe798c
Jason Miller <jason@redconfetti.com>
2015-02-02 17:16:29 -0800

    CLC-4779 - Site Provisioning: Create a 'Create a Project Site' tool context

---
+++ src/assets/javascripts/angular/controllers/pages/canvasCreateProjectSiteController.js
@@ -0,0 +1,10 @@
+(function(angular) {
+  'use strict';
+
+  /**
+   * Canvas 'Create a Site' overview index controller
+   */
+  angular.module('calcentral.controllers').controller('canvasCreateProjectSiteController', function(apiService) {
+    apiService.util.setTitle('Create a Project Site');
+  });
+})(window.angular);
