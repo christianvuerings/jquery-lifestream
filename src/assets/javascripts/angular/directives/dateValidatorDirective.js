b6eda20d9440e165674a9098235023bde7a5d3dc
Nolan Chan <nolanchan@berkeley.edu>
2015-02-02 16:33:45 -0800

    Validation Error no longer persists on date

--- src/assets/javascripts/angular/directives/dateValidatorDirective.js
+++ src/assets/javascripts/angular/directives/dateValidatorDirective.js
@@ -6,20 +6,13 @@
       require: 'ngModel',
       restrict: 'A',
       link: function(scope, elm, attrs, ctrl) {
-        ctrl.$parsers.unshift(function(viewValue) {
+        ctrl.$validators.ccDateValidator = function(viewValue) {
           // Date regex for mm/dd/yyyy modified from http://www.regular-expressions.info/dates.html
           // Allows spaces, slashes, dots and spaces as delimiters
           var mmddyyRegex = /^(0[1-9]|1[012])[\/](0[1-9]|[12][0-9]|3[01])[\/](19|20)\d\d$/;
-          if (mmddyyRegex.test(viewValue) || viewValue === '') {
-            // Regex is valid
-            ctrl.$setValidity('ccDateValidator', true);
-            return viewValue;
-          } else {
-            // Regex invalid, return undefined (no model update)
-            ctrl.$setValidity('ccDateValidator', false);
-            return undefined;
-          }
-        });
+          // Return true if regex is valid or no input, else false
+          return (mmddyyRegex.test(viewValue) || viewValue === '');
+        };
       }
     };
   });
