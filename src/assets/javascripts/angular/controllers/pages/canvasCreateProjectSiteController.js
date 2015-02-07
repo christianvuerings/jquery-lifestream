752bc1350f82a6e11501bc5b1fb546e115266d79
Jason Miller <jason@redconfetti.com>
2015-02-06 17:29:36 -0800

    CLC-4781 - Implemented authorization upon 'Create a Project Site' tool loading

--- src/assets/javascripts/angular/controllers/pages/canvasCreateProjectSiteController.js
+++ src/assets/javascripts/angular/controllers/pages/canvasCreateProjectSiteController.js
@@ -4,10 +4,29 @@
   /**
    * Canvas 'Create a Site' overview index controller
    */
-  angular.module('calcentral.controllers').controller('CanvasCreateProjectSiteController', function(apiService) {
+  angular.module('calcentral.controllers').controller('CanvasCreateProjectSiteController', function(apiService, canvasSiteCreationFactory, $scope) {
     apiService.util.setTitle('Create a Project Site');
 
     $scope.accessDeniedError = "This feature is only available to faculty and staff.";
 
+    var loadAuthorization = function() {
+      canvasSiteCreationFactory.getAuthorizations()
+        .success(function(data) {
+          console.log(data);
+          if (!data && (typeof(data.authorizations.canCreateProjectSite) === 'undefined')) {
+            $scope.authorizationError = 'failure';
+          } else {
+            angular.extend($scope, data);
+            if ($scope.authorizations.canCreateProjectSite === false) {
+              $scope.authorizationError = 'unauthorized';
+            }
+          }
+        })
+        .error(function() {
+          $scope.authorizationError = 'failure';
+        });
+    };
+
+    loadAuthorization();
   });
 })(window.angular);
