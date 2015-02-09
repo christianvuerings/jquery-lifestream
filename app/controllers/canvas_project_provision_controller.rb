839a79eb9adcbd5e829c116a2ca08c84e7fc88c6
Jason Miller <jason@redconfetti.com>
2015-02-09 12:07:21 -0800

    CLC-4781 - Added Canvas::ProjectProvision and CanvasProjectProvisionController

---
+++ app/controllers/canvas_project_provision_controller.rb
@@ -0,0 +1,20 @@
+class CanvasProjectProvisionController < ApplicationController
+  include ClassLogger
+
+  before_filter :api_authenticate
+  before_filter :authorize_creating_project_site
+  rescue_from StandardError, with: :handle_api_exception
+  rescue_from Errors::ClientError, with: :handle_client_error
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def authorize_creating_project_site
+    authorize current_user, :can_create_canvas_project_site?
+  end
+
+  # POST /api/academics/canvas/project_provision/create.json
+  def create_project_site
+    worker = Canvas::ProjectProvision.new(session[:user_id])
+    course_details = worker.create_project(params[:name])
+    render json: course_details.to_json
+  end
+end
---
+++ spec/models/canvas/project_provision_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe Canvas::ProjectProvision do
+  let(:user_id) { rand(99999).to_s }
+  let(:valid_course) do
+    {
+      "id"=>23,
+      "account_id"=>1,
+      "name"=>"Example Project Site",
+      "course_code"=>"Example Course Code",
+      "sis_course_id"=>"PROJ:67f4b934525501cb",
+      "workflow_state"=>"unpublished"
+    }
+  end
+  subject { Canvas::ProjectProvision.new(user_id) }
+
+  describe '#unique_sis_project_id' do
+    it 'returns unique sis_course_id for new project sites' do
+      allow(SecureRandom).to receive(:hex).and_return('67f4b934525501cb', '15fb56bedaa3b437')
+      course_1 = double()
+      course_2 = double()
+      expect(course_1).to receive(:course).and_return(valid_course)
+      expect(course_2).to receive(:course).and_return(nil)
+      expect(Canvas::SisCourse).to receive(:new).and_return(course_1, course_2)
+      sis_course_id = subject.unique_sis_project_id
+      expect(sis_course_id).to eq 'PROJ:15fb56bedaa3b437'
+    end
+
+    it 'raises exception if unique sis_course_id not found after 15 attempts' do
+      allow_any_instance_of(Canvas::SisCourse).to receive(:course).and_return(valid_course)
+      expect { subject.unique_sis_project_id }.to raise_error(RuntimeError, 'Unable to find unique SIS Course ID for Project Site')
+    end
+  end
+
+  describe '#create_project' do
+    let(:account_id) { Settings.canvas_proxy.projects_account_id }
+    let(:term_id) { Settings.canvas_proxy.projects_term_id }
+    let(:url_root) { Settings.canvas_proxy.url_root }
+    let(:project_name) { 'Test Project' }
+    let(:unique_sis_project_id) { '67f4b934525501cb' }
+    let(:new_course) do
+      {
+        "id"=>23,
+        "account_id"=> account_id,
+        "name"=> project_name,
+        "course_code"=> project_name,
+        "sis_course_id"=>"PROJ:#{unique_sis_project_id}",
+        "workflow_state"=>"unpublished"
+      }
+    end
+    let(:success_response) { double(status: 200, body: new_course.to_json) }
+    let(:failure_response) { double(status: 500, body: nil) }
+
+    before do
+      allow(subject).to receive(:unique_sis_project_id).and_return(unique_sis_project_id)
+      allow_any_instance_of(Canvas::Course).to receive(:create).and_return(valid_course)
+    end
+
+    it 'raises exception if error encountered with API request' do
+      expect_any_instance_of(Canvas::Course).to receive(:create).with(account_id, project_name, project_name, term_id, unique_sis_project_id).and_return(failure_response)
+      expect { subject.create_project(project_name) }.to raise_error Errors::ProxyError
+    end
+
+    it 'returns project site url' do
+      expect_any_instance_of(Canvas::Course).to receive(:create).with(account_id, project_name, project_name, term_id, unique_sis_project_id).and_return(success_response)
+      result = subject.create_project(project_name)
+      expect(result).to be_an_instance_of Hash
+      expect(result[:projectSiteId]).to eq 23
+      expect(result[:projectSiteUrl]).to eq url_root + '/courses/' + result[:projectSiteId].to_s
+    end
+  end
+end
