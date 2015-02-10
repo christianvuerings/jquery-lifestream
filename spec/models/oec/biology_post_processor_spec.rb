df1b4117aece65c33ec999fe13b09f0a43b3ac65
John Crossman <johncrossman@berkeley.edu>
2015-02-09 17:39:58 -0800

    OEC-551, no duplicate rows for BIO sections

--- spec/models/oec/biology_post_processor_spec.rb
+++ spec/models/oec/biology_post_processor_spec.rb
@@ -1,8 +1,11 @@
 describe Oec::BiologyPostProcessor do
 
-  before(:suite) do
+  let!(:csv_file_hash) { {} }
+
+  before do
+    export_dir = 'tmp/oec'
     dept_names = %w(BIOLOGY INTEGBI MCELLBI 'POL SCI')
-    expect(Settings.oec).to receive(:departments).exactly(1).and_return dept_names
+    expect(Settings.oec).to receive(:departments).at_least(:once).and_return dept_names
     dept_names.each do |dept_name|
       courses_query = []
       CSV.read('fixtures/oec/courses_wrapper.csv').each_with_index do |row, index|
@@ -11,43 +14,41 @@ describe Oec::BiologyPostProcessor do
         end
       end
       expect(Oec::Queries).to receive(:get_courses).with(nil, dept_name).exactly(1).times.and_return courses_query
+      export = Oec::Courses.new(dept_name, export_dir).export
+      csv_file_hash[dept_name] = CSV.read export[:filename]
     end
-    Oec::BiologyPostProcessor.new.post_process
+    biology_post_processor_patterns = { 'MCELLBI' => ' 1A[L]?', 'INTEGBI' => ' 1B[L]?' }
+    Oec::BiologyPostProcessor.new('BIOLOGY', biology_post_processor_patterns, export_dir, export_dir).post_process
   end
 
   context 'Biology 1A and 1B entries moved to MCELLBI and INTEGBI, respectively' do
 
     context 'reading BIOLOGY csv file' do
-      subject { get_csv 'BIOLOGY' }
+      subject { csv_file_hash['BIOLOGY'] }
       it {
         contain_exactly('COURSE_ID', '2015-B-87672')
       }
     end
 
     context 'reading INTEGBI csv file' do
-      subject { get_csv 'INTEGBI' }
+      subject { csv_file_hash['INTEGBI'] }
       it {
         contain_exactly('COURSE_ID', '2015-B-54432', '2015-B-87675')
       }
     end
 
     context 'reading MCELLBI csv file' do
-      subject { get_csv 'MCELLBI' }
+      subject { csv_file_hash['MCELLBI'] }
       it {
         contain_exactly('COURSE_ID', '2015-B-54441', '2015-B-87690', '2015-B-87693', '2015-B-87691')
       }
     end
 
     context 'reading POL SCI csv file' do
-      subject { get_csv 'POL SCI' }
+      subject { csv_file_hash['POL SCI'] }
       it {
         contain_exactly('COURSE_ID', '2015-B-72198')
       }
-    end
-
-    def get_csv(dept_name)
-      export = Oec::Courses.new(dept_name, 'tmp/oec').export
-      CSV.read export[:filename]
     end
 
   end
