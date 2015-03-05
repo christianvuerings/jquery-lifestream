5397628a997742d9a57a9feb3bdcbf7fe696a759
John Crossman <johncrossman@berkeley.edu>
2015-03-04 17:00:09 -0800

    OEC-563, report line number of duplicate UIDs in dept csv

--- spec/models/oec/dept_confirmed_data_spec.rb
+++ spec/models/oec/dept_confirmed_data_spec.rb
@@ -6,9 +6,10 @@ describe Oec::DeptConfirmedData do
     confirmed_data = Oec::DeptConfirmedData.new('fixtures/oec', actual_csv_files + missing_csv_files)
     confirmed_data_hash = confirmed_data.confirmed_data_per_dept
     confirmed_data_hash.keys.should match_array actual_csv_files
-    confirmed_data.warnings_per_dept.length.should eq 4
-    confirmed_data.warnings_per_dept['POL SCI'].length.should eq 2
-    confirmed_data.warnings_per_dept['STAT'].length.should eq 3
+    warnings = confirmed_data.warnings_per_dept
+    warnings.length.should eq 4
+    warnings['POL SCI']['WARN'].length.should eq 4
+    warnings['STAT']['WARN'].length.should eq 3
   end
 
   it 'should not load the STAT_courses_confirmed.csv because it was not requested' do
@@ -18,7 +19,9 @@ describe Oec::DeptConfirmedData do
     confirmed_data.confirmed_data_per_dept.keys.should match_array csv_files
     # Expect report of missing MCELLBI and INTEGBI files because BIOLOGY was requested
     confirmed_data.warnings_per_dept.length.should eq 3
-    confirmed_data.warnings_per_dept['POL SCI'].length.should eq 2
+    warnings = confirmed_data.warnings_per_dept['POL SCI']['WARN']
+    warnings[2] =~ /2015-B-71424-1002514(.*)duplicate/
+    warnings[3] =~ /2015-B-71419 (.*)duplicate/
   end
 
 end
