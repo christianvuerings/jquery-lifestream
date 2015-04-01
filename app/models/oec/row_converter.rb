506e4bf9ad09a98afd091fe8d9690d4a890c0a99
John Crossman <johncrossman@berkeley.edu>
2015-04-01 11:57:55 -0700

    OEC-581, oec:diff should validate ccn, ldap and instructor_func

--- app/models/oec/row_converter.rb
+++ app/models/oec/row_converter.rb
@@ -2,19 +2,17 @@ module Oec
   class RowConverter
 
     attr_reader :hashed_row
+    attr_reader :warnings
 
     def initialize(row)
       course_id = row[0]
       split_course_id = course_id.split('-')
       cross_listings = row[3]
-      ccn = split_course_id[2].split('_')[0]
       cross_listed_name = cross_listings.present? ? cross_listings[/\((.*)\)/, 1] : nil
       @hashed_row = {
-        'term_yr' => split_course_id[0],
         'term_cd' => split_course_id[1],
-        'course_cntl_num' => ccn,
         'course_id' => course_id,
-        'course_name' => row[1],
+        'course_name' => row[1].strip,
         'cross_listed_flag' => row[2],
         'cross_listed_name' => cross_listed_name,
         'course_title_short' => cross_listings.present? ? cross_listings[/(.*?)\s\(/, 1] : nil,
@@ -23,18 +21,36 @@ module Oec
         'instruction_format' => row[6],
         'section_num' => row[7],
         'primary_secondary_cd' => row[8],
-        'ldap_uid' => row[9],
         'first_name' => row[10],
         'last_name' => row[11],
         'email_address' => row[12],
-        'instructor_func' => row[13],
         'blue_role' => row[14],
         'evaluate' => row[15],
-        'evaluation_type' => row[16],
-        'modular_course' => row[17],
-        'start_date' => row[18],
-        'end_date' => row[19]
+        'dept_form' => row[16],
+        'evaluation_type' => row[17],
+        'modular_course' => row[18],
+        'start_date' => row[19],
+        'end_date' => row[20]
       }
+      # Validate certain fields
+      @warnings = []
+      put_valid_i(row, 'term_yr', split_course_id[0], '2\d{3}')
+      put_valid_i(row, 'course_cntl_num', split_course_id[2].split('_')[0], '[1-9]\d{3,4}')
+      put_valid_i(row, 'ldap_uid', row[9], '\d{3,6}')
+      put_valid_i(row, 'instructor_func', row[13], '[1-4]')
+    end
+
+    def put_valid_i(row, column_name, value, regex)
+      value.strip! unless value.nil?
+      matches_regex = value =~ /^#{regex}$/
+      # Record value regardless of validity. Report warning, if necessary.
+      @hashed_row[column_name] = matches_regex ? value.to_i : value
+      unless matches_regex
+        @warnings << <<-eos
+          #{value} in #{column_name} column does not match #{regex}. Raw row data:
+          #{row.to_s}
+        eos
+      end
     end
 
   end
