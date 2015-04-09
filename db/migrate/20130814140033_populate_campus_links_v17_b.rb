8b520d65efa1a17e1b2baaaaefbb94fa2b4d9432
Ray Davis <ray@media.berkeley.edu>
2015-04-09 08:59:11 -0700

    CLC-5105 Remove excess CampusLink snapshots

--- db/migrate/20130814140033_populate_campus_links_v17_b.rb
+++ db/migrate/20130814140033_populate_campus_links_v17_b.rb
@@ -1,7 +1,7 @@
 class PopulateCampusLinksV17B < ActiveRecord::Migration
   def self.up
     Links::CampusLinkLoader.delete_links!
-    Links::CampusLinkLoader.load_links! "/public/json/campuslinks_v17_b.json"
+    Links::CampusLinkLoader.load_links! "/public/json/campuslinks.json"
   end
 
 end
