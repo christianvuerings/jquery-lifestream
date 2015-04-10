7ffbb0597b08e4dcadaaf730a98430468bab1c52
Jason Miller <jason@redconfetti.com>
2015-04-09 18:16:38 -0700

    CLC-5068 - Updated Canvas::BackgroundJob.unique_job_id to return unused cache key without requiring unique UID or Canvas course ID

--- app/models/canvas/background_job.rb
+++ app/models/canvas/background_job.rb
@@ -3,12 +3,15 @@ module Canvas
     include TorqueBox::Messaging::Backgroundable
 
     def self.unique_job_id
-      Time.now.to_f.to_s.gsub('.', '')
+      15.times do
+        cache_key_candidate = "#{self.name.to_sym}.#{Time.now.to_f.to_s.gsub('.', '')}-#{SecureRandom.hex(8)}"
+        return cache_key_candidate if Rails.cache.read(cache_key_candidate).nil?
+      end
+      raise RuntimeError, 'Unable to find unique Canvas Background Job ID'
     end
 
     def self.find(cache_key)
       Rails.cache.fetch(cache_key)
     end
-
   end
 end
