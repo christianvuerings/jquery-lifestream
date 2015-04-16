11ffa0152a163a781b7e4bcfeea1e4b6bbde834c
John Crossman <johncrossman@berkeley.edu>
2015-04-16 16:11:29 -0700

    CLC-5130, use find_by instead of lookup

--- spec/models/webcast/preferences_spec.rb
+++ spec/models/webcast/preferences_spec.rb
@@ -2,39 +2,32 @@ describe Webcast::Preferences do
 
   describe '#lookup' do
     before do
-      Webcast::Preferences.create(
-        {
-          year: 2015,
-          term_cd: 'D',
-          ccn: 1234,
-          opt_out: true})
-      Webcast::Preferences.create(
-        {
-          year: 2016,
-          term_cd: 'B',
-          ccn: 5678,
-          opt_out: false})
+      Webcast::Preferences.create params(2015, 'D', 1234, true)
+      Webcast::Preferences.create params(2016, 'B', 5678, false)
     end
 
     it 'should deny per uniqueness constraint' do
-      expect {
-        Webcast::Preferences.create(
-          {
-            year: 2016,
-            term_cd: 'B',
-            ccn: 5678,
-            opt_out: true})
-      }.to raise_error NameError
+      expect { Webcast::Preferences.create params(2016, 'B', 5678, true) }.to raise_error NameError
     end
 
     it 'should not find matching record' do
-      expect(Webcast::Preferences.lookup(2015, 'B', 1234)).to be_nil
+      expect(Webcast::Preferences.find_by({ year: 2015, term_cd: 'B', ccn: 1234 })).to be_nil
     end
 
     it 'should return record with opt_out equal false' do
-      record = Webcast::Preferences.lookup(2015, 'D', 1234)
+      record = Webcast::Preferences.find_by({ year: 2015, term_cd: 'D', ccn: 1234 })
       expect(record).to_not be_nil
+      expect(record.year).to eq 2015
+      expect(record.term_cd).to eq 'D'
+      expect(record.ccn).to eq 1234
       expect(record.opt_out).to be true
     end
   end
+
+  private
+
+  def params(year, term_cd, ccn, opt_out)
+    { year: year, term_cd: term_cd, ccn: ccn, opt_out: opt_out }
+  end
+
 end
