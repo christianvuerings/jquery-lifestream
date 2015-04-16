d15c0681db21d00d8788438f475bed0aa8f09b79
John Crossman <johncrossman@berkeley.edu>
2015-04-16 16:21:13 -0700

    CLC-5130, remove unused lookup

--- app/models/webcast/preferences.rb
+++ app/models/webcast/preferences.rb
@@ -5,16 +5,5 @@ module Webcast
 
     attr_accessible :year, :term_cd, :ccn, :opt_out
 
-    def self.lookup(year, term_cd, ccn)
-      result_set = Preferences.limit(1).where(
-        'year = :year AND term_cd = :term_cd AND ccn = :ccn',
-        {
-          year: year,
-          term_cd: term_cd,
-          ccn: ccn
-        })
-      result_set[0]
-    end
-
   end
 end
