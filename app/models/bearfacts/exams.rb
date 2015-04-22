58428a96a190c801c1f8475d25557ba3a968bc42
Paul Kerschen <kerschen@berkeley.edu>
2015-04-22 15:42:38 -0700

    CLC-5186 Move BearFacts proxies to Mockable framework

--- app/models/bearfacts/exams.rb
+++ app/models/bearfacts/exams.rb
@@ -1,8 +1,12 @@
 module Bearfacts
   class Exams < Proxy
 
-    def get
-      request("/student/#{lookup_student_id}/reg/finalexams", "finalexams")
+    def mock_xml
+      read_file('fixtures', 'xml', "bearfacts_exams_#{@student_id}.xml")
+    end
+
+    def request_path
+      "/student/#{@student_id}/reg/finalexams"
     end
 
   end
--- app/models/bearfacts/profile.rb
+++ app/models/bearfacts/profile.rb
@@ -1,8 +1,12 @@
 module Bearfacts
   class Profile < Proxy
 
-    def get
-      request("/student/#{lookup_student_id}", "profile")
+    def mock_xml
+      read_file('fixtures', 'xml', "bearfacts_profile_#{@student_id}.xml")
+    end
+
+    def request_path
+      "/student/#{@student_id}"
     end
 
   end
