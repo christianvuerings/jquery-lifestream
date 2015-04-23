1d004c74c68be4252e294f42077c0afa4166942f
Paul Farestveit <pfarestveit@berkeley.edu>
2015-04-23 15:08:19 -0700

    CLC-5120: fix view-as Selenium tests

--- spec/ui_selenium/pages/settings_page.rb
+++ spec/ui_selenium/pages/settings_page.rb
@@ -20,15 +20,22 @@ module CalCentralPages
     button(:disconnect_no_button, :xpath => '//button[@data-ng-click="showValidation = false"]')
     button(:connect_button, :xpath => '//button[@data-ng-click="api.user.enableOAuth(service)"]')
 
-    # View-as
-    text_area(:view_as_input, :id => 'cc-settings-act-as-uid')
-    button(:view_as_button, :xpath => '//button[@type="submit"]')
-
-    # UID/SID Lookup
-    text_area(:uid_sid_input, :id => 'cc-settings-id')
-    button(:lookup_button, :xpath => '//button[text()="Look Up"]')
-    td(:uid_result, :xpath => '//td[@data-ng-bind="user.ldap_uid"]')
-    td(:sid_result, :xpath => '//td[@data-ng-bind="user.student_id || \'This user has no SID\'"]')
+    # Student Lookup
+    button(:search_tab, :xpath => '//button[@data-ng-click="admin.switchSelectedOption(selectOption)"][text()="Search"]')
+    button(:saved_tab, :xpath => '//button[@data-ng-click="admin.switchSelectedOption(selectOption)"][text()="Saved"]')
+    button(:recent_tab, :xpath => '//button[@data-ng-click="admin.switchSelectedOption(selectOption)"][text()="Recent"]')
+    text_area(:uid_sid_input, :id => 'cc-widget-student-lookup-search')
+    button(:search_button, :xpath => '//button[@type="submit"]')
+    table(:search_results_table, :xpath => '//div[@data-ng-repeat="tab in admin.tabs"][1]//table')
+    table(:saved_users_table, :xpath => '//div[@data-ng-repeat="tab in admin.tabs"][2]//table')
+    table(:recent_users_table, :xpath => '//div[@data-ng-repeat="tab in admin.tabs"][3]//table')
+    elements(:uid_result, :td, :xpath => '//td[@data-ng-bind="user.ldap_uid || \'none\'"]')
+    button(:uid_link, :xpath => '//button[@data-ng-click="admin.changeIDType(\'UID\')"]')
+    elements(:sid_result, :td, :xpath => '//td[@data-ng-bind="user.student_id || \'none\'"]')
+    button(:sid_link, :xpath => '//button[@data-ng-click="admin.changeIDType(\'SID\')"]')
+    button(:view_as_link, :xpath => '//button[@data-ng-click="admin.actAsUser(user)"]')
+    button(:save_user_toggle, :xpath => '//button[@data-ng-click="admin.toggleSaveState(user)"]')
+    elements(:user_saved, :image, :xpath => '//i[@class="fa fa-star-o"]')
 
     def load_page(driver)
       logger.info('Loading settings page')
@@ -51,17 +58,68 @@ module CalCentralPages
       end
     end
 
-    def view_as_user(id)
-      view_as_input_element.when_visible(timeout=WebDriverUtils.page_load_timeout)
-      self.view_as_input = id
-      view_as_button
-    end
+    # VIEW-AS
 
-    def convert_id(id)
-      uid_sid_input_element.when_visible(timeout=WebDriverUtils.page_load_timeout)
+    def search_for_user(id)
+      search_tab_element.when_visible(timeout=WebDriverUtils.page_load_timeout)
+      search_tab
+      uid_sid_input_element.when_visible(timeout=WebDriverUtils.page_event_timeout)
       self.uid_sid_input = id
-      lookup_button
+      search_button
     end
 
+    def view_as_user(id)
+      search_for_user(id)
+      search_results_table_element.when_present(timeout=WebDriverUtils.page_event_timeout)
+      wait_until(timeout=WebDriverUtils.page_event_timeout) { search_results_table_element[1][0].text == id }
+      view_as_link_element.when_visible(timeout=WebDriverUtils.page_event_timeout)
+      view_as_link
+    end
+
+    def save_first_user_in_list
+      save_user_toggle_element.when_visible(timeout=WebDriverUtils.page_event_timeout)
+      save_user_toggle
+      user_saved_elements[0].when_present(timeout=WebDriverUtils.page_event_timeout)
+    end
+
+    def click_saved_users_tab
+      saved_tab_element.when_visible(timeout=WebDriverUtils.page_load_timeout)
+      saved_tab
+    end
+
+    def all_saved_uids
+      uids = []
+      saved_users_table_element.when_visible(timeout=WebDriverUtils.page_event_timeout)
+      saved_users_table_element.each { |row| uids << row[0].text }
+      uids.drop(1)
+    end
+
+    def all_saved_names
+      names = []
+      saved_users_table_element.when_visible(timeout=WebDriverUtils.page_event_timeout)
+      saved_users_table_element.each { |row| names << row[1].text }
+      names.drop(1)
+    end
+
+    def un_save_all_saved_users
+      click_saved_users_tab
+      while saved_users_table?
+        row_one = save_user_toggle_element
+        row_one.click
+        row_one.when_not_present(timeout=WebDriverUtils.page_event_timeout)
+      end
+    end
+
+    def show_uid
+      if uid_link_element.visible?
+        uid_link
+      end
+    end
+
+    def show_sid
+      if sid_link_element.visible?
+        sid_link
+      end
+    end
   end
 end
