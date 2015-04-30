0ba295d42d07d1147aa591d9dc9096c2f72ac821
Paul Kerschen <kerschen@berkeley.edu>
2015-04-30 10:11:46 -0700

    CLC-5238 Follow-up comment edit

--- config/initializers/disable_webmock_default.rb
+++ config/initializers/disable_webmock_default.rb
@@ -1,6 +1,8 @@
 # Webmock is enabled by default upon load, which causes issues with test utilities,
 # like Selenium. It's therefore disabled here and then enabled when
-# the backend rspec tests run.
+# the backend rspec tests run, and when fake proxies are instantiated in the development
+# environment.
+
 if Settings.application.fake_proxies_enabled
   require 'webmock'
   WebMock.disable!
