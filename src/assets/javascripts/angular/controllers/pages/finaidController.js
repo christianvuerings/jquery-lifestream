40e8dd68edf4cf2900a3c322977800fb1d85edd9
Christian Vuerings <vueringschristian@gmail.com>
2015-05-01 15:42:55 -0700

    SISRP-3682 - Develop FinAid Awards front-end code
    https://jira.berkeley.edu/browse/SISRP-3682

---
+++ src/assets/javascripts/angular/controllers/pages/finaidController.js
@@ -0,0 +1,10 @@
+(function(angular) {
+  'use strict';
+
+  /**
+   * Financial Aid controller
+   */
+  angular.module('calcentral.controllers').controller('FinaidController', function(apiService) {
+    apiService.util.setTitle('Financial Aid');
+  });
+})(window.angular);
