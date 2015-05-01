46afd6a6a83a1ec50479304851b0b27a1a8c114a
Ray Davis <ray@media.berkeley.edu>
2015-05-01 09:21:17 -0700

    CLC-5135 Update the DB schema version Missing line from https://github.com/ets-berkeley-edu/calcentral/pull/3700

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416164519) do
+ActiveRecord::Schema.define(version: 20150430222956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
