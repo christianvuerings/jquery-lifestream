b3bf25f3626d908034552bb4d7204f0c4b3483f4
John Crossman <johncrossman@berkeley.edu>
2015-05-05 15:41:32 -0700

    CLC-5134, add params to filter results to published

--- app/models/canvas/courses.rb
+++ app/models/canvas/courses.rb
@@ -4,7 +4,8 @@ module Canvas
     def official_courses
       self.class.fetch_from_cache(@uid) do
         account_id = Settings.canvas_proxy.official_courses_account_id
-        response = request_uncached("accounts/#{account_id}/courses", '_all_official_course_sites', {:method => :get})
+        params = 'published=true&include=[sections]'
+        response = request_uncached("accounts/#{account_id}/courses?#{params}", '_all_official_course_sites', {:method => :get})
         response ? safe_json(response.body) : nil
       end
     end
