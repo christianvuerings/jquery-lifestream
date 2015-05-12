98265e2ea0476e8b2ce94621e960dfe197fe4f5c
Christian Vuerings <vueringschristian@gmail.com>
2015-05-12 12:09:15 +0100

    CLC-5270 - Update scss lint to 0.38 and fix the gem install
    https://jira.ets.berkeley.edu/jira/browse/CLC-5270
    
    Focus QA Notes:
    * Spinner is still working on previously supported devices
    * No big regressions in color changes

--- script/front-end-test.sh
+++ script/front-end-test.sh
@@ -3,6 +3,6 @@ echo "Node version: $node_version"
 npm config set strict-ssl false
 npm install
 npm run build-production
-gem install scss-lint --version 0.31.0
+gem install scss_lint --version 0.38.0
 scss-lint src/assets/stylesheets/
 [[ $? -ne 0 ]] && echo 'scss-lint failed' && exit 1 || echo 'scss-lint was succesful'
