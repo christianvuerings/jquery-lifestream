c502f13cd87864e92de2a0538ac4d03bf61f030d
Chris Tweney <chris@media.berkeley.edu>
2015-05-20 13:36:52 -0700

    SISRP-3683 Make the CS Awards proxy real

--- app/models/campus_solutions/awards.rb
+++ app/models/campus_solutions/awards.rb
@@ -10,5 +10,24 @@ module CampusSolutions
       'cs_awards.xml'
     end
 
+    # TODO make year dynamic (default to current aid year)
+    def request_options
+      {
+        query: {
+          'year' => 2015
+        }
+      }
+    end
+
+    # TODO ID is hardcoded until we can use ID crosswalk service to convert CalNet ID to CS Student ID
+    def url
+      "#{@settings.base_url}/00000137"
+    end
+
+    def build_feed(response)
+      #HTTParty won't parse automatically because the application/xml header is missing
+      MultiXml.parse(response.parsed_response)
+    end
+
   end
 end
