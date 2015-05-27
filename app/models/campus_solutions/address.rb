18d14abfb4defbc5a954b7c49147b458264f8377
Chris Tweney <chris@media.berkeley.edu>
2015-05-27 09:24:16 -0700

    SISRP-4274 Sketch out way to map CS fields to CC fields, with metadata

--- app/models/campus_solutions/address.rb
+++ app/models/campus_solutions/address.rb
@@ -1,13 +1,20 @@
 module CampusSolutions
   class Address < DirectProxy
 
+    FIELD_MAPPINGS = [
+      FieldMapping.required(:country, :COUNTRY),
+      FieldMapping.required(:address1, :ADDRESS1),
+      FieldMapping.optional(:address2, :ADDRESS2)
+    ]
+
     def xml_filename
       'address.xml'
     end
 
     def build_feed(response)
       feed = {
-        addresses: []
+        addresses: [],
+        fields: FieldMapping.to_hash(FIELD_MAPPINGS)
       }
       return feed if response.parsed_response.blank?
       response.parsed_response['UC_PER_ADDR_GET_RESP']['ADDRESS'].each do |address|
