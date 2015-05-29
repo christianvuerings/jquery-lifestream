08ae3979ab827042ce7b404e95530f374d610d6f
Ray Davis <ray@media.berkeley.edu>
2015-05-29 09:50:34 -0700

    CLC-4579 Rake task to generate TurnItIn report for a term

---
+++ script/report-turnitin-usage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script to generate a CSV report on TurnItIn usage for a term in bCourses.
+
+# Make sure the normal shell environment is in place, since it may not be
+# when running as a cron job.
+source "$HOME/.bash_profile"
+
+cd $( dirname "${BASH_SOURCE[0]}" )/..
+
+LOG=`date +"$PWD/log/report_turnitin_usage_%Y-%m-%d.log"`
+LOGIT="tee -a $LOG"
+
+# Enable rvm and use the correct Ruby version and gem set.
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+source .rvmrc
+
+export RAILS_ENV=production
+export LOGGER_STDOUT=only
+export LOGGER_LEVEL=INFO
+export JRUBY_OPTS="-Xcext.enabled=true -J-client -X-C"
+
+echo | $LOGIT
+echo "------------------------------------------" | $LOGIT
+echo "`date`: About to run the TurnItIn usage reporting script..." | $LOGIT
+
+cd deploy
+
+bundle exec rake canvas:report_turnitin | $LOGIT
