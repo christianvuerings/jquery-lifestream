bd37287c88844c11742cdae0f3876c6c1c9a8a77
Ray Davis <ray@media.berkeley.edu>
2015-07-09 09:53:57 -0700

    CLC-5427 Nix obsolete "app_alerts" feature flag

--- app/controllers/blog_feed_controller.rb
+++ app/controllers/blog_feed_controller.rb
@@ -2,10 +2,8 @@ class BlogFeedController < ApplicationController
 
   def get_blog_info
     result = {}
-    if Settings.features.app_alerts
-      alert_data = EtsBlog::Alerts.new.get_latest
-      result.merge!(:alert => alert_data) unless alert_data.blank?
-    end
+    alert_data = EtsBlog::Alerts.new.get_latest
+    result.merge!(:alert => alert_data) unless alert_data.blank?
     result.merge!( EtsBlog::ReleaseNotes.new.get_latest_release_notes )
     render :json => result
   end
