40865aec93f041cf29964f71d4bd9f9ee28929d3
Winston Kim <winzton.kim@gmail.com>
2015-07-14 15:48:13 -0700

    SISRP-4447 - Write spec to cover PhotoController

--- spec/controllers/photo_controller_spec.rb
+++ spec/controllers/photo_controller_spec.rb
@@ -1,29 +1,34 @@
 require 'spec_helper'
 
 describe PhotoController do
+  let(:user_id) { random_id }
 
   context "when serving a users photo" do
     context "when user is logged in" do
-
+        before do
+          session['user_id'] = random_id
+        end
       context "when user has photo" do
         before do
-          test_photo_object = {:photo => 'photo_binary_content'}
+          test_photo_object = {'photo' => 'photo_binary_content'}
           CampusOracle::Queries.stub(:get_photo).and_return(test_photo_object)
         end
         it "renders users raw image" do
-          allow(controller).to receive(:send_data).with('photo_binary_content', type: 'image/jpeg', disposition: 'inline').and_return(true)
+          expect(controller).to receive(:send_data).with('photo_binary_content', type: 'image/jpeg', disposition: 'inline'){
+            controller.render nothing: true
+          }
           get :my_photo
         end
       end
-
-    end
-
-    context "when user is not logged in" do
-      before { session['user_id'] = nil }
-      it "returns 401 error with no body" do
-        get :my_photo
-        expect(response.status).to eq 401
-        expect(response.body).to eq " "
+      context "when user has no photo" do
+        before do
+          test_photo_object = nil
+          CampusOracle::Queries.stub(:get_photo).and_return(test_photo_object)
+        end
+        it "renders users raw image" do
+          allow(controller).to receive(:send_data).with(nil, type: 'image/jpeg', disposition: 'inline').and_return(true)
+          get :my_photo
+        end
       end
     end
 
