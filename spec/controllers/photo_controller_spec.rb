d91868fed737353b5248d7103ce58de7d063beca
Winston Kim <winzton.kim@gmail.com>
2015-07-16 11:18:52 -0700

    SISRP-4447 - Write spec to cover PhotoController

--- spec/controllers/photo_controller_spec.rb
+++ spec/controllers/photo_controller_spec.rb
@@ -3,31 +3,30 @@ require 'spec_helper'
 describe PhotoController do
   let(:user_id) { random_id }
 
-  context "when serving a users photo" do
-    context "when user is logged in" do
+  context 'when serving a users photo' do
+    context 'when user is logged in' do
         before do
           session['user_id'] = random_id
         end
-      context "when user has photo" do
+      context 'when user has photo' do
         before do
           test_photo_object = {'photo' => 'photo_binary_content'}
-          CampusOracle::Queries.stub(:get_photo).and_return(test_photo_object)
+          allow(CampusOracle::Queries).to receive(:get_photo).and_return(test_photo_object)
         end
-        it "renders users raw image" do
-          expect(controller).to receive(:send_data).with('photo_binary_content', type: 'image/jpeg', disposition: 'inline'){
-            controller.render nothing: true
-          }
+        it 'renders users raw image' do
           get :my_photo
+          expect(response.status).to eq 200
+          expect(response.body).to eq 'photo_binary_content'
         end
       end
-      context "when user has no photo" do
+      context 'when user has no photo' do
         before do
-          test_photo_object = nil
-          CampusOracle::Queries.stub(:get_photo).and_return(test_photo_object)
+          allow(CampusOracle::Queries).to receive(:get_photo).and_return(nil)
         end
-        it "renders users raw image" do
-          allow(controller).to receive(:send_data).with(nil, type: 'image/jpeg', disposition: 'inline').and_return(true)
+        it 'renders users raw image' do
           get :my_photo
+          expect(response.status).to eq 200
+          expect(response.body).to eq ' '
         end
       end
     end
