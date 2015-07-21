49a461c5a0374a7f1ec18021719acb5c8d7d31fd
Christian Vuerings <vueringschristian@gmail.com>
2015-07-21 16:53:48 -0700

    CLC-5573 - Make coloring optional for the amounts
    https://jira.ets.berkeley.edu/jira/browse/CLC-5573

--- src/assets/javascripts/angular/directives/amountDirective.js
+++ src/assets/javascripts/angular/directives/amountDirective.js
@@ -2,6 +2,13 @@
 
 var angular = require('angular');
 
+/**
+ * Show an $ amount in a specific way
+ *
+ * Usage:
+ *   data-cc-amount-directive="amount" // The amount to pass through
+ *   data-cc-amount-directive-color="false" // Default is true, if set to false, we don't add the color class
+ */
 angular.module('calcentral.directives').directive('ccAmountDirective', [function() {
   var isNumber = function(number) {
     return !isNaN(parseFloat(number)) && isFinite(number);
@@ -13,6 +20,10 @@ angular.module('calcentral.directives').directive('ccAmountDirective', [function
 
   return {
     link: function(scope, element, attr) {
+      // Whether to add a color class
+      var directiveColor = scope.$eval(attr.ccAmountDirectiveColor);
+      var isColorEnabled = (typeof directiveColor !== 'undefined') ? directiveColor : true;
+
       scope.$watch(attr.ccAmountDirective, function ccAmountWatchAction(value) {
         // Only do something when it's a number
         if (!isNumber(value)) {
@@ -25,7 +36,9 @@ angular.module('calcentral.directives').directive('ccAmountDirective', [function
           text = '  $ ' + numberWithCommas(value);
         } else {
           text = '- $ ' + numberWithCommas(value).replace('-', '');
-          element.addClass('cc-page-myfinances-green');
+          if (isColorEnabled) {
+            element.addClass('cc-page-myfinances-green');
+          }
         }
         text = text.replace(/\s/g, '\u00A0');
 
