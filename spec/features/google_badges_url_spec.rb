659e52ef06fed6311d06a91b6a34d7561c85ccb9
Paul Kerschen <kerschen@berkeley.edu>
2015-07-24 15:17:04 -0700

    CLC-5577 Fix failing Capybara test for Google Calendar links

--- spec/features/google_badges_url_spec.rb
+++ spec/features/google_badges_url_spec.rb
@@ -6,18 +6,20 @@ feature 'MyBadges urls:' do
   def selenium_check_url_exists(url, truthy_match = nil)
     truthy_match ||= url
     visit url
-    within("div.signin-card") {
-      fill_in "Email", with: Settings.google_proxy.test_user_email
-      fill_in "Passwd", with: Settings.google_proxy.test_user_password
-      click_button "signIn"
+    within('div.signin-card') {
+      fill_in 'Email', with: Settings.google_proxy.test_user_email
+      click_button 'Next'
+      sleep 1
+      fill_in 'Passwd', with: Settings.google_proxy.test_user_password
+      click_button 'signIn'
     }
     visit url
     sleep 5 #waiting for redirect
-    current_url.include?(truthy_match).should be_truthy
-    malformed_link = url[0..-2] + "ohnoes"
+    expect(current_url).to include truthy_match
+    malformed_link = url[0..-2] + 'ohnoes'
     visit malformed_link
     sleep 5 #waiting for redirect
-    current_url.include?(malformed_link).should be_falsey
+    expect(current_url).not_to include malformed_link
   end
 
   before(:all) do
@@ -49,7 +51,7 @@ feature 'MyBadges urls:' do
 
   scenario 'see if munged urls from google_calendar badges resolve', :testext => true do
     #This requires an existing event to be exist from the time listed below.
-    event_listed_after = "2013-05-09T12:42:02-07:00"
+    event_listed_after = '2013-05-09T12:42:02-07:00'
     GoogleApps::Proxy.stub(:access_granted?).and_return(true)
     GoogleApps::EventsRecentItems.stub(:new).and_return(@real_calendar_events)
     results = MyBadges::GoogleCalendar.new(@user_id).fetch_counts(
@@ -57,29 +59,29 @@ feature 'MyBadges urls:' do
         timeMin: event_listed_after,
         updatedMin: nil
       })
-    results[:items].size.should_not == 0
+    expect(results[:items]).not_to be_empty
     first_link = results[:items].first[:link]
     eid = Rack::Utils.parse_query(URI.parse(first_link).query)['eid']
     selenium_check_url_exists(first_link, eid)
   end
 
-  scenario 'see if mungled ruls from google_calendar badges != passed through urls for berkeley.edu tokens' do
-    User::Oauth2Data.stub(:get_google_email).and_return("oski.the.creepy.bear@berkeley.edu")
+  scenario 'see if mungled urls from google_calendar badges != passed through urls for berkeley.edu tokens' do
+    User::Oauth2Data.stub(:get_google_email).and_return('oski.the.creepy.bear@berkeley.edu')
     GoogleApps::Proxy.stub(:access_granted?).and_return(true)
     GoogleApps::EventsRecentItems.stub(:new).and_return(@fake_calendar_events)
-    raw_feed = GoogleApps::EventsRecentItems.new(@uid).recent_items.first.data["items"]
+    raw_feed = GoogleApps::EventsRecentItems.new(@uid).recent_items.first.data['items']
     munged_feed = MyBadges::GoogleCalendar.new(@user_id).fetch_counts
     # find something where title is == summary
     found_matching_item = false
     munged_feed[:items].each do |item|
-      matched_items = raw_feed.select {|raw_item| raw_item["summary"] == item[:title]}
+      matched_items = raw_feed.select {|raw_item| raw_item['summary'] == item[:title]}
       if matched_items.present?
-        matched_items.first["htmlLink"].should_not == item[:link]
+        expect(matched_items.first['htmlLink']).to_not eq item[:link]
         found_matching_item = true
         break
       end
     end
     #makes sure something was tested.
-    found_matching_item.should_not be_falsey
+    expect(found_matching_item).to_not be_falsey
   end
 end
