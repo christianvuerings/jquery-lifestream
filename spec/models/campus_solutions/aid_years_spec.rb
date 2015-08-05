fb398d3056ea49382e479e181664439df0b05502
Christian Vuerings <vueringschristian@gmail.com>
2015-08-05 10:23:29 -0700

    SISRP-5145 - Fix the back-end tests

--- spec/models/campus_solutions/aid_years_spec.rb
+++ spec/models/campus_solutions/aid_years_spec.rb
@@ -7,7 +7,7 @@ describe CampusSolutions::AidYears do
     let(:feed) { fake_proxy.get[:feed] }
 
     it 'returns JSON fixture data by default' do
-      expect(feed['finaidSummary']['finaidYear']).to be
+      expect(feed['finaidSummary']['finaidYears']).to be
     end
     it 'can be overridden to return errors' do
       fake_proxy.set_response(status: 506, body: '')
