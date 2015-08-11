8cc8ee2c0527de6cb679eb99bb1a6451b6920c64
John Crossman <johncrossman@berkeley.edu>
2015-08-11 10:35:00 -0700

    CLC-5639, use be_an Array instead of be_a

--- spec/models/google_apps/drive_spec.rb
+++ spec/models/google_apps/drive_spec.rb
@@ -4,11 +4,11 @@ describe GoogleApps::DriveList do
     drive_list_proxy = GoogleApps::DriveList.new :fake => true
     expect(drive_list_proxy.class.api).to eq 'drive'
     response = drive_list_proxy.drive_list
-    expect(response).to be_a Enumerable
+    expect(response).to be_an Enumerable
     response.each do |response_page|
       expect(response_page.status).to eq 200
       expect(response_page.data['kind']).to eq 'drive#fileList'
-      expect(response_page.data['items']).to be_a Array
+      expect(response_page.data['items']).to be_an Array
     end
   end
 
