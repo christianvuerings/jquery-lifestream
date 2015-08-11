d63846546184bdd2b06be15a271cc0e9e2c7dff1
John Crossman <johncrossman@berkeley.edu>
2015-08-11 10:29:38 -0700

    CLC-5639, rspec 3 sytnax for existing GoogleApps::DriveList code

--- spec/models/google_apps/drive_spec.rb
+++ spec/models/google_apps/drive_spec.rb
@@ -1,20 +1,14 @@
-require "spec_helper"
-
 describe GoogleApps::DriveList do
 
-  begin
-    @random_id = rand(99999).to_s
-  end
-
-  it "Should return a valid list of drive files" do
+  it 'Should return a valid list of drive files' do
     drive_list_proxy = GoogleApps::DriveList.new :fake => true
-    drive_list_proxy.class.api.should == "drive"
+    expect(drive_list_proxy.class.api).to eq 'drive'
     response = drive_list_proxy.drive_list
-    response.kind_of?(Enumerable).should be_truthy
+    expect(response).to be_a Enumerable
     response.each do |response_page|
-      response_page.status.should == 200
-      response_page.data["kind"].should == "drive#fileList"
-      response_page.data["items"].kind_of?(Array).should be_truthy
+      expect(response_page.status).to eq 200
+      expect(response_page.data['kind']).to eq 'drive#fileList'
+      expect(response_page.data['items']).to be_a Array
     end
   end
 
