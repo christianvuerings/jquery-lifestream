c9e2994cd5e3f7173f45c5067396799094792e9c
Chris Tweney <chris@media.berkeley.edu>
2015-08-12 14:31:09 -0700

    SISRP-6245 Refactor back to Hash for performance reasons

--- lib/cache/related_cache_key_tracker.rb
+++ lib/cache/related_cache_key_tracker.rb
@@ -14,20 +14,20 @@ module Cache
         super uid
         related_keys = related_cache_keys uid
         logger.debug "Will now expire these associated keys for uid #{uid}: #{related_keys.inspect}"
-        related_keys.each do |related_key|
+        related_keys.keys.each do |related_key|
           Rails.cache.delete related_key
         end
         Rails.cache.delete(user_key(uid))
       end
 
       def related_cache_keys(uid=nil)
-        Rails.cache.read(user_key(uid)) || Set.new
+        Rails.cache.read(user_key(uid)) || {}
       end
 
       def save_related_cache_key(uid=nil, related_key=nil)
         related_keys = related_cache_keys uid
-        return if related_keys.include?(related_key)
-        related_keys.add related_key
+        return if related_keys[related_key].present?
+        related_keys[related_key] = 1
         logger.debug "Writing related keys for uid #{uid}: #{related_keys.inspect}"
         Rails.cache.write(user_key(uid),
                           related_keys,
