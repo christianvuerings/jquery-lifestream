d42a6d3e0eeddea7901c1aea2deb6ce512c5ca39
John Crossman <johncrossman@berkeley.edu>
2015-08-17 11:17:02 -0700

    CLC-5639, find_folders_by_title and find_items_by_title (more generic)

--- app/models/google_apps/drive_manager.rb
+++ app/models/google_apps/drive_manager.rb
@@ -7,6 +7,35 @@ module GoogleApps
       @credential_store = credential_store
     end
 
+    def find_folders_by_title(title, parent_id = nil)
+      find_items_by_title(title, mime_type: 'application/vnd.google-apps.folder', parent_id: parent_id)
+    end
+
+    def find_items_by_title(title, options = {})
+      client = get_google_api
+      drive_api = client.discovered_api('drive', 'v2')
+      items = []
+      # mime_type, parent_id = nil
+      parent_id = options[:parent_id] || 'root'
+      query = "'#{parent_id}' in parents and title='#{title}' and trashed=false"
+      query.concat " and mimeType='#{options[:mime_type]}'" if options.has_key? :mime_type
+      page_token = nil
+      begin
+        parameters = { :q => query }
+        parameters[:pageToken] = page_token unless page_token.nil?
+        api_response = client.execute(:api_method => drive_api.files.list, :parameters => parameters)
+        if api_response.status == 200
+          files = api_response.data
+          items.concat files.items
+          page_token = files.next_page_token
+        else
+          puts "An error occurred: #{api_response.data['error']['message']}"
+          page_token = nil
+        end
+      end while page_token.to_s != ''
+      items
+    end
+
     def create_folder(title, parent_id)
       client = get_google_api
       drive_api = client.discovered_api('drive', 'v2')
