846003efd309b1945d49bb39292b867052d89e42
Chris Tweney <chris@media.berkeley.edu>
2015-08-26 15:14:00 -0700

    SISRP-6732 Fix testext failures, hopefully

--- spec/models/campus_solutions/aid_years_spec.rb
+++ spec/models/campus_solutions/aid_years_spec.rb
@@ -1,22 +1,6 @@
 require 'spec_helper'
 
 describe CampusSolutions::AidYears do
-  #
-  # context 'mock proxy' do
-  #   let(:fake_proxy) { CampusSolutions::AidYears.new(fake: true) }
-  #   let(:feed) { fake_proxy.get[:feed] }
-  #   it_behaves_like 'a proxy that properly observes the finaid feature flag'
-  #   it 'returns data with the expected structure' do
-  #     expect(feed['finaidSummary']['finaidYears']).to be
-  #   end
-  #   it 'can be overridden to return errors' do
-  #     fake_proxy.set_response(status: 506, body: '')
-  #     response = fake_proxy.get
-  #     expect(response[:errored]).to eq true
-  #   end
-  #
-  # end
-
 
   shared_examples 'a proxy that gets data' do
     subject { proxy.get }
