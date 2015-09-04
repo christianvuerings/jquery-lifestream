b0622a146906fe2d381cbb602f461ecf8097d4ed
Paul Kerschen <kerschen@berkeley.edu>
2015-09-04 15:46:33 -0700

    CLC-5726 Move drive methods from Oec::Task to Oec::RemoteDrive

--- app/models/oec/task.rb
+++ app/models/oec/task.rb
@@ -4,7 +4,6 @@ module Oec
 
     def initialize(opts)
       @log = []
-      uid = Settings.oec.google.uid
       @remote_drive = Oec::RemoteDrive.new
       @term_code = opts.delete :term_code
       @opts = opts
@@ -31,69 +30,56 @@ module Oec
 
     private
 
+    def copy_file(file, dest_folder)
+      return if @opts[:local_write]
+      @remote_drive.check_conflicts_and_copy_file(file, dest_folder,
+        on_success: -> { log :debug, "Copied file '#{file.title}' to remote drive folder '#{dest_folder.title}'" }
+      )
+    end
+
     def create_folder(folder_name, parent=nil)
       return if @opts[:local_write]
-      if @remote_drive.find_folders_by_title(folder_name, folder_id(parent)).any?
-        raise RuntimeError, "Folder '#{folder_name}' with parent '#{folder_title(parent)}' already exists on remote drive"
-      else
-        create_folder_no_existence_check(folder_name, parent)
-      end
-    end
-
-    def export_sheet(worksheet, dest_folder)
-      if @opts[:local_write]
-        worksheet.write_csv
-        log :debug, "Exported worksheet to local file '#{worksheet.output_filename}'"
-      else
-        upload_to_remote_drive(worksheet, worksheet.base_filename.chomp('.csv'), dest_folder)
-      end
-    end
-
-    def find_or_create_folder(folder_name, parent=nil)
-      find_folder(folder_name, parent) || create_folder_no_existence_check(folder_name, parent)
-    end
-
-    def create_folder_no_existence_check(folder_name, parent=nil)
-      unless folder = @remote_drive.create_folder(folder_name, folder_id(parent))
-        raise RuntimeError, "Could not create folder '#{folder_name}' on remote drive"
-      end
-      log :debug, "Created remote folder '#{folder_name}'"
-      folder
-    end
-
-    def copy_file(file, dest_folder)
-      if find_item(file.title, dest_folder)
-        raise RuntimeError, "File '#{file.title}' already exists in remote drive folder '#{dest_folder.title}'; could not copy"
-      end
-      if (@remote_drive.copy_item_to_folder(file, dest_folder.id))
-        log :debug, "Copied file '#{file.title}' to remote drive folder '#{dest_folder.title}'"
-      else
-        raise RuntimeError, "Could not copy file '#{file.title}' to '#{dest_folder.title}'"
-      end
+      @remote_drive.check_conflicts_and_create_folder(folder_name, parent,
+        on_conflict: :error,
+        on_creation: -> { log :debug, "Created remote folder '#{folder_name}'" }
+      )
     end
 
     def datestamp
       DateTime.now.strftime '%F'
     end
 
-    def find_folder(title, parent=nil)
-      @remote_drive.find_folders_by_title(title, folder_id(parent)).first
+    def export_sheet(worksheet, dest_folder)
+      if @opts[:local_write]
+        worksheet.write_csv
+        log :debug, "Exported worksheet to local file #{worksheet.csv_export_path}"
+      else
+        upload_worksheet(worksheet, worksheet.export_name, dest_folder)
+      end
     end
 
-    def find_folders(parent=nil)
-      @remote_drive.find_folders folder_id(parent)
+    def export_sheet_headers(klass, dest_folder)
+      worksheet = klass.new @tmp_path
+      if @opts[:local_write]
+        worksheet.write_csv
+        log :debug, "Exported to header-only local file #{worksheet.csv_export_path}"
+      else
+        @remote_drive.check_conflicts_and_upload(worksheet, klass.export_name, Oec::Worksheet, dest_folder,
+          on_success: -> { log :debug, "Uploaded header-only sheet '#{klass.export_name}' to remote drive folder '#{dest_folder.title}'" })
+      end
     end
 
-    def find_item(title, parent=nil)
-      @remote_drive.find_items_by_title(title, parent_id: folder_id(parent)).first
+    def find_or_create_folder(folder_name, parent=nil)
+      @remote_drive.check_conflicts_and_create_folder(folder_name, parent,
+        on_conflict: :return_existing,
+        on_creation: -> { log :debug, "Created remote folder '#{folder_name}'" }
+      )
     end
 
-    def folder_id(folder)
-      folder ? folder.id : 'root'
-    end
-
-    def folder_title(folder)
-      folder ? folder.title : 'root'
+    def find_or_create_today_subfolder(category_name)
+      return if @opts[:local_write]
+      parent = @remote_drive.find_nested([@term_code, category_name], on_failure: :error)
+      find_or_create_folder(datestamp, parent)
     end
 
     def log(level, message)
@@ -105,40 +91,23 @@ module Oec
       DateTime.now.strftime '%H%M%S'
     end
 
-    def upload_worksheet_headers(klass, dest_folder)
-      worksheet = klass.new(@tmp_path)
-      if @opts[:local_write]
-        worksheet.write_csv
-        log :debug, "Exported to header-only local file #{worksheet.output_filename}"
-      else
-        upload_to_remote_drive(worksheet, klass.name.demodulize.underscore, dest_folder)
-      end
-    end
-
-    def upload_to_remote_drive(worksheet, title, folder)
-      if @remote_drive.find_items_by_title(title, parent_id: folder.id).any?
-        raise RuntimeError, "Item '#{title}' already exists in remote drive folder '#{folder.title}'; could not upload sheet"
-      end
-      if (!@remote_drive.upload_worksheet(title, '', worksheet, folder.id))
-        raise RuntimeError, "Sheet '#{title}' could not be uploaded to remote drive folder '#{folder.title}'"
-      end
-      log :debug, "Uploaded sheet '#{title}' to remote drive folder '#{folder.title}'"
-    end
-
     def upload_file(path, remote_name, type, folder)
-      if @remote_drive.find_items_by_title(remote_name, parent_id: folder.id).any?
-        raise RuntimeError, "Item '#{remote_name}' already exists in remote drive folder '#{folder.title}'; could not upload"
-      end
-      if (!@remote_drive.upload_file(remote_name, '', folder.id, type, path.to_s))
-        raise RuntimeError, "File #{path} could not be uploaded to remote drive folder '#{folder.title}'"
-      end
-      log :debug, "Uploaded file #{path} to remote drive folder '#{folder.title}'"
+      @remote_drive.check_conflicts_and_upload(path, remote_name, type, folder,
+        on_success: -> { log :debug, "Uploaded item #{path} to remote drive folder '#{folder.title}'" })
+    end
+
+    def upload_worksheet(worksheet, title, folder)
+      @remote_drive.check_conflicts_and_upload(worksheet, title, Oec::Worksheet, folder,
+        on_success: -> { log :debug, "Uploaded sheet '#{title}' to remote drive folder '#{folder.title}'" })
     end
 
     def write_log
       log_name = "#{timestamp}_#{self.class.name.demodulize.underscore}.log"
+      log :debug, "Exporting log file '#{log_name}'"
       File.open(@tmp_path.join(log_name), 'wb') { |f| f.puts @log }
-      unless @opts[:local_write]
+      if @opts[:local_write]
+        logger.debug "Wrote log file to path #{@tmp_path.join(log_name)}"
+      else
         if (reports_today = find_or_create_today_subfolder('reports'))
           begin
             upload_file(@tmp_path.join(log_name), log_name, 'text/plain', reports_today)
@@ -151,15 +120,5 @@ module Oec
       logger.error "Could not write log: #{e.message}\n#{e.backtrace.join "\n\t"}"
     end
 
-    def find_or_create_today_subfolder(category_name)
-      return if @opts[:local_write]
-      unless (term_folder = find_folder @term_code) && (folder = find_folder(category_name, term_folder))
-        raise RuntimeError, "Could not locate '#{category_name}' folder on remote drive"
-      end
-      unless (today_folder = find_or_create_folder(datestamp, folder))
-        raise RuntimeError, "Could not find_or_create '#{category_name}' folder dated #{datestamp} on remote drive"
-      end
-      today_folder
-    end
   end
 end
--- spec/models/oec/report_diff_task_spec.rb
+++ spec/models/oec/report_diff_task_spec.rb
@@ -4,12 +4,12 @@ describe Oec::ReportDiffTask do
     let(:term_code) { '2015-D' }
     subject { described_class.new(term_code: term_code) }
 
-    let(:fake_sheets_manager) { double }
+    let(:fake_remote_drive) { double }
     let(:today) { '2015-08-31' }
     let(:now) { '092222' }
     let(:logfile) { "#{now}_report_diff_task.log" }
 
-    before(:each) { allow(Oec::RemoteDrive).to receive(:new).and_return fake_sheets_manager }
+    before(:each) { allow(Oec::RemoteDrive).to receive(:new).and_return fake_remote_drive }
     let(:dept_code) { 'PSTAT' }
     let(:dept_name) { 'STAT' }
     let(:fake_code_mapping) { [Oec::CourseCode.new(dept_name: dept_name, catalog_id: nil, dept_code: dept_name, include_in_oec: true)] }
@@ -17,11 +17,11 @@ describe Oec::ReportDiffTask do
     before {
       allow(Oec::CourseCode).to receive(:by_dept_code).and_return({dept_code => fake_code_mapping})
       expect(subject).to receive(:write_log)
-      expect(fake_sheets_manager).to_not receive(:upload_worksheet)
+      expect(fake_remote_drive).to_not receive(:check_conflicts_and_upload)
     }
 
     it 'should produce no diff when department spreadsheet not found' do
-      expect(fake_sheets_manager).to receive(:find_dept_courses_spreadsheet).with(term_code, dept_code).and_return nil
+      expect(fake_remote_drive).to receive(:find_dept_courses_spreadsheet).with(term_code, dept_code).and_return nil
       subject.run
     end
   end
