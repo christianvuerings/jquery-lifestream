e96114f69be110a93a969340f76bc6000eaea471
Chris Tweney <chris@media.berkeley.edu>
2015-09-16 16:02:52 -0700

    SISRP-7420 Transform more fields from EDO

--- app/models/hub_edos/user_attributes.rb
+++ app/models/hub_edos/user_attributes.rb
@@ -18,7 +18,7 @@ module HubEdos
       edo_feed = Student.new(user_id: @uid).get
       result = {}
       if (feed = edo_feed[:feed])
-        edo = feed['student'] # TODO will have to dynamically switch student/person EDO somehow
+        edo = HashConverter.symbolize feed['student'] # TODO will have to dynamically switch student/person EDO somehow
         extract_ids(edo, result)
         extract_names(edo, result)
         extract_affiliations(edo, result)
@@ -26,6 +26,7 @@ module HubEdos
         extract_education_level(edo, result)
         extract_total_units(edo, result)
         extract_residency(edo, result)
+        extract_special_program_code(edo, result)
         result[:statusCode] = 200
       else
         logger.error "Could not get Student EDO data for uid #{@uid}"
@@ -34,69 +35,82 @@ module HubEdos
     end
 
     def extract_ids(edo, result)
-      edo['identifiers'].each do |id|
-        if id['type'] == 'CalNet UID'
-          result[:ldap_uid] = id['id']
-        elsif id['type'] == 'Student ID'
-          result[:student_id] = id['id']
+      edo[:identifiers].each do |id|
+        if id[:type] == 'CalNet UID'
+          result[:ldap_uid] = id[:id]
+        elsif id[:type] == 'Student ID'
+          result[:student_id] = id[:id]
         end
       end
     end
 
     def extract_names(edo, result)
-      edo['names'].each do |name|
+      edo[:names].each do |name|
         # use preferred name
-        if name['type']['code'] == 'PRF'
-          result[:first_name] = name['givenName']
-          result[:last_name] = name['familyName']
-          result[:person_name] = name['formattedName']
+        if name[:type][:code] == 'PRF'
+          result[:first_name] = name[:givenName]
+          result[:last_name] = name[:familyName]
+          result[:person_name] = name[:formattedName]
           break
         end
       end
     end
 
     def extract_affiliations(edo, result)
-      result[:affiliations] = [] # TODO develop affiliations when we have more examples of different types
-      edo['affiliations'].each do |affiliation|
-        if affiliation['type']['code'] == 'UNDERGRAD' && affiliation['statusCode'] == 'ACT'
+      # TODO affiliations needs more business analysis and transformation.
+      result[:affiliations] = edo[:affiliations]
+      result[:affiliations].each do |affiliation|
+        if affiliation[:type][:code] == 'UNDERGRAD' && affiliation[:statusCode] == 'ACT'
           result[:ug_grad_flag] = 'U'
-        elsif affiliation['type']['code'] == 'GRAD' && affiliation['statusCode'] == 'ACT'
+        elsif affiliation[:type][:code] == 'GRAD' && affiliation[:statusCode] == 'ACT'
           result[:ug_grad_flag] = 'G'
         end
       end
     end
 
     def extract_emails(edo, result)
-      edo['emails'].each do |email|
-        if email['primary'] == true
-          result[:email_address] = email['emailAddress']
+      edo[:emails].each do |email|
+        if email[:primary] == true
+          result[:email_address] = email[:emailAddress]
         end
-        if email['type']['code'] == 'CAMP'
-          result[:official_bmail_address] = email['emailAddress']
+        if email[:type][:code] == 'CAMP'
+          result[:official_bmail_address] = email[:emailAddress]
         end
       end
     end
 
     def extract_education_level(edo, result)
-      result[:educ_level] = edo['currentRegistration']['academicLevel']['level']['description']
+      result[:educ_level] = edo[:currentRegistration][:academicLevel][:level][:description]
     end
 
     def extract_total_units(edo, result)
-      edo['currentRegistration']['termUnits'].each do |term_unit|
-        if term_unit['type']['description'] == 'Total'
-          result[:tot_enroll_unit] = term_unit['unitsEnrolled']
+      edo[:currentRegistration][:termUnits].each do |term_unit|
+        if term_unit[:type][:description] == 'Total'
+          result[:tot_enroll_unit] = term_unit[:unitsEnrolled]
           break
         end
       end
     end
 
     def extract_residency(edo, result)
-      if edo['residency']['official']['code'] == 'RES'
+      if edo[:residency][:official][:code] == 'RES'
         result[:cal_residency_flag] = 'Y'
       else
         result[:cal_residency_flag] = 'N'
       end
     end
+
+    def extract_special_program_code(edo, result)
+      result[:education_abroad] = false
+      # TODO verify business correctness of this conversion based on more examples of study-abroad students
+      edo[:currentRegistration][:specialStudyPrograms].each do |pgm|
+        if pgm[:type][:code] == 'EAP'
+          result[:education_abroad] = true
+          break
+        end
+      end
+    end
+
   end
 end
 
