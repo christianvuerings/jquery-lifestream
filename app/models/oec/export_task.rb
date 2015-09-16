9a6d545f7955d489673c1edc775e8d8cc9ca67f8
Paul Kerschen <kerschen@berkeley.edu>
2015-09-16 16:43:00 -0700

    CLC-5638 Oec: add supervisors and course_supervisors to export sheets

--- app/models/oec/export_task.rb
+++ app/models/oec/export_task.rb
@@ -12,6 +12,13 @@ module Oec
       students = Oec::Students.new
       course_students = Oec::CourseStudents.new
 
+      merged_supervisor_confirmations = @remote_drive.find_nested [@term_code, 'departments', 'merged_supervisor_confirmations']
+      merged_supervisor_confirmations_csv = merged_supervisor_confirmations && @remote_drive.export_csv(merged_supervisor_confirmations)
+      raise RuntimeError, 'No merged supervisor confirmation sheet found' unless merged_supervisor_confirmations_csv
+
+      supervisors = Oec::Supervisors.from_csv(merged_supervisor_confirmations_csv)
+      course_supervisors = Oec::CourseSupervisors.new
+
       ccns = Set.new
       suffixed_ccns = {}
 
@@ -19,6 +26,15 @@ module Oec
         add_checking_conflicts(courses, locked_confirmation_row, %w(COURSE_ID))
         add_checking_conflicts(course_instructors, locked_confirmation_row, %w(LDAP_UID COURSE_ID))
         add_checking_conflicts(instructors, locked_confirmation_row, %w(LDAP_UID))
+
+        supervisors.matching_dept_name(locked_confirmation_row['DEPT_FORM']).each do |supervisor|
+          course_supervisor_row = {
+            'COURSE_ID' => locked_confirmation_row['COURSE_ID'],
+            'LDAP_UID' => supervisor['LDAP_UID'],
+            'DEPT_NAME' => locked_confirmation_row['DEPT_FORM']
+          }
+          add_checking_conflicts(course_supervisors, course_supervisor_row, %w(LDAP_UID COURSE_ID))
+        end
 
         next unless (ccn = locked_confirmation_row['COURSE_ID'].split('-')[2])
         ccn, suffix = ccn.split('_')
@@ -51,18 +67,16 @@ module Oec
       end
 
       exports_today = find_or_create_today_subfolder 'exports'
-      [instructors, course_instructors, courses, students, course_students].each do |sheet|
+      [instructors, course_instructors, courses, students, course_students, supervisors, course_supervisors].each do |sheet|
         export_sheet(sheet, exports_today)
       end
-
-      #TODO Build and export supervisors and course_supervisors sheets
     end
 
     def add_checking_conflicts(sheet, row, key_columns)
       key = key_columns.map { |col| row[col] }.join('-')
       candidate_row = row.slice(*sheet.headers)
       if sheet[key] && (sheet[key] != candidate_row)
-        raise RuntimeError, "Sheet '#{sheet.export_name}' has conflicting values for key '#{key}'; aborting export"
+        raise RuntimeError, "Sheet '#{sheet.export_name}' has conflicting values for key '#{key}'; aborting export\n#{sheet[key]}\n#{candidate_row}"
       else
         sheet[key] ||= candidate_row
       end
