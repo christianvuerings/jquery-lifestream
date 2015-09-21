b53bdea33fdf065315399220c04beee5c2179bf1
Paul Kerschen <kerschen@berkeley.edu>
2015-09-21 15:29:30 -0700

    CLC-5794 OEC: Add SIS_ID column to Instructors and Supervisors sheets

--- app/models/oec/supervisors.rb
+++ app/models/oec/supervisors.rb
@@ -4,6 +4,7 @@ module Oec
     def headers
       %w(
         LDAP_UID
+        SIS_ID
         FIRST_NAME
         LAST_NAME
         EMAIL_ADDRESS
@@ -26,6 +27,7 @@ module Oec
     end
 
     validate('LDAP_UID') { |row| 'Non-numeric' unless row['LDAP_UID'] =~ /\A\d+\Z/ }
+    validate('SIS_ID') { |row| 'Unexpected' unless row['SIS_ID'] == "UID:#{row['LDAP_UID']}" || row['SIS_ID'] =~ /\A\d+\Z/ }
 
   end
 end
