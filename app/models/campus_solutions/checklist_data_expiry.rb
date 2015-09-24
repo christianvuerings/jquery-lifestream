67f7ae8170ad58904e220e5b99bc9155fc38ec5a
Chris Tweney <chris@media.berkeley.edu>
2015-09-24 16:20:03 -0700

    SISRP-6621 Temporarily expire the CampusSolutions::Checklist with no UID, as a bandaid until the dynamic-user code can be merged

--- app/models/campus_solutions/checklist_data_expiry.rb
+++ app/models/campus_solutions/checklist_data_expiry.rb
@@ -4,6 +4,8 @@ module CampusSolutions
       [CampusSolutions::Checklist, MyTasks::Merged].each do |klass|
         klass.expire uid
       end
+      # TODO remove this next line after SISRP-7420 gets merged
+      CampusSolutions::Checklist.expire nil
     end
   end
 end
