faa2443b8d8828a91697a943e6aff0ee6ab91124
Chris Tweney <chris@media.berkeley.edu>
2015-09-24 16:58:19 -0700

    SISRP-6621 Followup - remove bandaid cache fix

--- app/models/campus_solutions/checklist_data_expiry.rb
+++ app/models/campus_solutions/checklist_data_expiry.rb
@@ -4,8 +4,6 @@ module CampusSolutions
       [CampusSolutions::Checklist, MyTasks::Merged].each do |klass|
         klass.expire uid
       end
-      # TODO remove this next line after SISRP-7420 gets merged
-      CampusSolutions::Checklist.expire nil
     end
   end
 end
