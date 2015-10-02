f93f7500412ae09bc2454fcf98a5a7a58fff9fa4
Chris Tweney <chris@media.berkeley.edu>
2015-10-02 14:18:55 -0700

    NOJIRA Update student API fixture and test to be more realistic copy of current API feed

--- spec/models/hub_edos/user_attributes_spec.rb
+++ spec/models/hub_edos/user_attributes_spec.rb
@@ -7,13 +7,11 @@ describe HubEdos::UserAttributes do
   it 'should provide the converted person data structure' do
     expect(subject[:ldap_uid]).to eq '61889'
     expect(subject[:student_id]).to eq 11667051
-    expect(subject[:first_name]).to eq 'Oski'
+    expect(subject[:first_name]).to eq 'Osk'
     expect(subject[:last_name]).to eq 'Bear'
-    expect(subject[:person_name]).to eq 'Oski Bear'
-    expect(subject[:ug_grad_flag]).to eq 'U'
-    expect(subject[:affiliations]).to be
+    expect(subject[:person_name]).to eq 'Osk Bear'
     expect(subject[:email_address]).to eq 'oski@berkeley.edu'
-    expect(subject[:official_bmail_address]).to eq 'oski@berkeley.edu'
+    expect(subject[:official_bmail_address]).to be_nil
     expect(subject[:names]).to be
     expect(subject[:addresses]).to be
   end
