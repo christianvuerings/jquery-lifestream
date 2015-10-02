e13174d314a4b58228a6e0fe60987a30a0eb814a
Chris Tweney <chris@media.berkeley.edu>
2015-10-02 11:11:42 -0700

    SISRP-6620 Merge CS communications into My Activities feed

--- spec/models/my_activities/campus_solutions_messages_spec.rb
+++ spec/models/my_activities/campus_solutions_messages_spec.rb
@@ -8,7 +8,10 @@ describe MyActivities::CampusSolutionsMessages do
     end
     subject { MyActivities::CampusSolutionsMessages.append!(user_id, activities) }
     it 'should be able to process activities from a fake pending messages feed' do
-      p "subj = #{subject}"
+      expect(subject[0][:emitter]).to eq 'Campus Solutions'
+      expect(subject[0][:linkText]).to eq 'Campus Provided Software'
+      expect(subject[0][:date][:epoch]).to eq 1440632189
+      expect(subject[0][:date][:dateString]).to eq '8/26'
     end
   end
 
