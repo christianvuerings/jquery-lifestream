363b110d17cc21b46e3a1e41a2ed1fef9a4bbd3e
Chris Tweney <chris@media.berkeley.edu>
2015-10-06 14:48:52 -0700

    SISRP-8276 Fix activities feed

--- app/models/my_activities/campus_solutions_messages.rb
+++ app/models/my_activities/campus_solutions_messages.rb
@@ -12,19 +12,21 @@ module MyActivities
       results = []
       if feed && feed[:commMessagePendingResponse]
         feed[:commMessagePendingResponse].each do |message|
-          results << {
-            emitter: 'Campus Solutions', # TODO what's the right emitter?
-            id: '',
-            linkText: message[:descr],
-            source: message[:commCategory], # TODO is this right?
-            summary: message[:commCenterDescr],
-            type: 'campus_solutions',
-            title: message[:descr],
-            user_id: uid,
-            date: format_date(strptime_in_time_zone(message[:lastupddttm], "%Y-%m-%d-%H.%M.%S.000000")), # 2015-08-26-16.36.29.000000
-            sourceUrl: message[:url],
-            url: message[:url]
-          }
+          if message[:descr].present?
+            results << {
+              emitter: 'Campus Solutions', # TODO what's the right emitter?
+              id: '',
+              linkText: message[:descr],
+              source: message[:commCategory], # TODO is this right?
+              summary: message[:commCenterDescr],
+              type: 'campus_solutions',
+              title: message[:descr],
+              user_id: uid,
+              date: format_date(strptime_in_time_zone(message[:lastupddttm], "%Y-%m-%d-%H.%M.%S.000000")), # 2015-08-26-16.36.29.000000
+              sourceUrl: message[:url],
+              url: message[:url]
+            }
+          end
         end
       end
       results
