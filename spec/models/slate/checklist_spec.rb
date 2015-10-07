f5f0e4f03905dcfe3963a9659b737a266257c960
Chris Tweney <chris@media.berkeley.edu>
2015-10-07 13:16:56 -0700

    SISRP-8364 Put Slate checklist behind feature flag

--- spec/models/slate/checklist_spec.rb
+++ spec/models/slate/checklist_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 describe Slate::Checklist do
 
-  # it_should_behave_like 'a student data proxy' do
-  #   let(:proxy_class) { Slate::Checklist }
-  #   let(:feed_key) { 'PERSON_CHKLST' }
-  # end
-
   context 'mock proxy' do
+    before do
+      allow(Settings.features).to receive(:slate_checklist).and_return(true)
+    end
+
     let(:oski_uid) { '61889' }
     let(:oski_student_id) { 11667051 }
     let(:fake_proxy) { Slate::Checklist.new(user_id: oski_uid, fake: true) }
@@ -22,7 +21,16 @@ describe Slate::Checklist do
       response = fake_proxy.get
       expect(response[:errored]).to eq true
     end
+  end
 
+  context 'with the checklist feature disabled' do
+    before do
+      allow(Settings.features).to receive(:slate_checklist).and_return(false)
+    end
+    subject { Slate::Checklist.new(user_id: random_id, fake: true).get }
+    it 'should return an empty feed' do
+       expect(subject).to eq({})
+    end
   end
 end
 
