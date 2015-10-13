529098923aef00b3502b3c82f2fa7f20142fe8b2
Chris Tweney <chris@media.berkeley.edu>
2015-10-13 10:19:06 -0700

    SISRP-8853 Remove failing test that's not needed in testext

--- spec/models/hub_edos/student_spec.rb
+++ spec/models/hub_edos/student_spec.rb
@@ -23,7 +23,6 @@ describe HubEdos::Student do
     subject { proxy.get }
 
     it_behaves_like 'a proxy that properly observes the profile feature flag'
-    it_should_behave_like 'a simple proxy that returns errors'
 
     it 'returns data with the expected structure' do
       expect(subject[:feed]['student']).to be
