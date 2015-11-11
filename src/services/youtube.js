08c30167aa9c1454b91620efc2f41e2dda5cf86c
David Cheng <davidlimcheng@gmail.com>
2015-11-10 22:05:39 -0800

    Changed Youtube service to Heroku app

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,75 +1,40 @@
 (function($) {
-$.fn.lifestream.feeds.youtube = function( config, callback ) {
+  "use strict";
 
-  var template = $.extend({},
-    {
-      uploaded: 'uploaded <a href="${video.player.default}" ' +
-        'title="${video.description}">${video.title}</a>',
-      favorited: 'favorited <a href="${video.player.default}" ' +
-        'title="${video.description}">${video.title}</a>'
-    },
-    config.template),
+  $.fn.lifestream.feeds.youtube = function( config, callback ) {
 
-  parseYoutube = function( input, activity ) {
-    var output = [], i = 0, j, item, video, date, templateData;
+    var template = $.extend({},
+      {
+        "uploaded": 'uploaded <a href="https://www.youtube.com/watch?v=${id}">${title}</a>'
+      },
+      config.template);
 
-    if(input.data && input.data.items) {
-      j = input.data.items.length;
-      for( ; i<j; i++) {
-        item = input.data.items[i];
+    var parseYoutube = function(response) {
+      var output = [];
 
-        switch (activity) {
-          case 'favorited':
-            video = item.video;
-            date = item.created;
-            templateData = item;
-            break;
-          case 'uploaded':
-            video = item;
-            date = video.uploaded;
-            templateData = {video: video};
-            break;
-        }
+      if(!response.videos) {return output;}
 
-        // Don't add unavailable items (private, rejected, failed)
-        if (!video.player || !video.player['default']) {
-          continue;
-        }
+      for (var i=0; i<response.videos.length;i++){
+        var video = response.videos[i];
 
         output.push({
-          date: new Date(date),
-          config: config,
-          html: $.tmpl( template[activity], templateData )
+          "date": new Date(video.datePublished),
+          "config": config,
+          "html": $.tmpl(template.uploaded, video)
         });
       }
-    }
+      callback(output);
+    };
 
-    return output;
+    $.ajax({
+      "url": "https://youtuby-1.herokuapp.com/" + config.user,
+      "cache": false
+    }).success(parseYoutube);
+
+    // Expose the template.
+    // We use this to check which templates are available
+    return {
+      "template" : template
+    };
   };
-
-  $.ajax({
-    url: "https://gdata.youtube.com/feeds/api/users/" + config.user +
-      "/favorites?v=2&alt=jsonc",
-    dataType: 'jsonp',
-    success: function( data ) {
-      callback(parseYoutube(data, 'favorited'));
-    }
-  });
-
-  $.ajax({
-    url: "https://gdata.youtube.com/feeds/api/users/" + config.user +
-      "/uploads?v=2&alt=jsonc",
-    dataType: 'jsonp',
-    success: function( data ) {
-      callback(parseYoutube(data, 'uploaded'));
-    }
-  });
-
-  // Expose the template.
-  // We use this to check which templates are available
-  return {
-    "template" : template
-  };
-
-};
-})(jQuery);
+  })(jQuery);
